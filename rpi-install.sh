ba4afd53f305a515b3d20c032f4a03d8828dcb23
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2015-06-07 13:32:39 +0200

    Adding git clone to rpi-install file

--- rpi-install.sh
+++ rpi-install.sh
@@ -29,9 +29,9 @@ wget http://node-arm.herokuapp.com/node_0.10.36_armhf.deb
 sudo dpkg -i node_0.10.36_armhf.deb
 
 # Installing MySQL
-# Prevent MySQL for asking a password, set Password to "raspberry"
-echo "mysql-server mysql-server/root_password raspberry" | debconf-set-selections
-echo "mysql-server mysql-server/root_password_again raspberry" | debconf-set-selection
+# Prevent MySQL for asking a password, set Password to "root"
+echo "mysql-server mysql-server/root_password root" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again root" | debconf-set-selection
 # Install MySQL
 sudo apt-get install -y mysql-server
 
@@ -41,4 +41,13 @@ sudo apt-get install -y libasound2-dev
 # NPM global modules
 sudo npm install -g node-gyp
 sudo npm install -g sails
+sudo npm install -g forever
+
+
+#Cloning Gladys into "gladys" directory
+git clone https://github.com/GladysProject/Gladys.git gladys
+cd gladys
+
+# Installing dependencies
+sudo npm install
 
