abf2f04089c55601fc6cf94c5159280f3397a5c3
time-lords <account@gdnet.fr>
2015-06-19 10:12:58 +0200

    no data userSleep

--- assets/js/app/sleep/sleep.controller.js
+++ assets/js/app/sleep/sleep.controller.js
@@ -36,8 +36,11 @@
       return sleepService.getSleep()
         .then(function(data){
           vm.recommended = data.data.recommended;
+          if(data.data.userSleep.length == 0){
+            return;
+          }
           vm.lastSleep = data.data.userSleep[data.data.userSleep.length-1].duration;
-  	      
+          
           vm.userSleep = data.data.userSleep;
           var line = new Morris.Line({
                 element: 'line-chart',
