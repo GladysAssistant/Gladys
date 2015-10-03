a70d489a4bb463b1352d7779f49064e277633995
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2015-10-03 21:02:45 +0200

    Sync events with GitHub

--- api/services/LifeEventService.js
+++ api/services/LifeEventService.js
@@ -7,6 +7,45 @@
   * @author :: Pierre-Gilles Leymarie
   */
 
+var request = require('request');
+var eventsGithub = 'https://raw.githubusercontent.com/GladysProject/gladys-events/master/events.en.json';
+
+/**
+ * Add events if not exist in the database
+ */
+function addEvent(event, cb){
+	EventType.findOne({name:event.name}, function(err, eventtype){
+		if(err) return cb(err);
+		
+		if(!eventtype){
+			EventType.create(event, function(err, event){
+				if(err) return cb(err);
+				
+				sails.log.info('[Gladys Update] New event created - ' + event.name);
+				cb(null, event);
+			});
+		}else{
+			cb(null);
+		}
+	});		 
+}
+
+/**
+ * Get the Events from GitHub 
+ * (see : https://github.com/GladysProject/gladys-events )
+ */
+function getEventsGithub(cb){
+	request(eventsGithub, function(err, response, body) {
+				if(err) return cb('GetEventsGithub : Fail to load URL : ' + err);
+				try{
+					var events = JSON.parse(body);
+					cb(null, events);
+				}catch(e){
+					cb(e);
+				}
+	});
+}
+
 module.exports = {
 	/**
 	 * Description
@@ -37,6 +76,20 @@ module.exports = {
 		 	}else{
 		 		callback('eventType ' + eventType + ' not found');
 		 	}
+		});
+	},
+	
+	/**
+	 * Sync events with GitHub
+	 */
+	syncEvents: function() {
+		sails.log.info('[Gladys Update] Updating life event lists from Github...');
+		getEventsGithub(function(err, events){
+			if(err) return sails.log.warn('LifeEventService : syncEvents' + err);
+			
+			async.each(events, addEvent, function(){
+				sails.log.info('[Gladys Update] Life event lists updated !');
+			});
 		});
 	}
 };
