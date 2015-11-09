774dbbe575fa023c11b90fe2eb05f2c8975f08c9
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2015-11-09 15:36:24 +0100

    Valid ExampleService

--- api/hooks/example/services/ExampleService.js
+++ api/hooks/example/services/ExampleService.js
@@ -1,14 +1,8 @@
-/**
- * Created by jaumard on 11/05/2015.
- */
+
+
 module.exports = {
-    /**
-     * Can be overrided on ExampleService.js file on your sails server
-     * @param req
-     * @param res
-     */
-    test: function (req, res) {
-        console.log("test method on ExampleService");
-        res.json('ok');
+
+    test: function () {
+        console.log("test method on ExampleService");   
     }
 };
