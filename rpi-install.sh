d80fe793882a03dd8edb8c3863481b311d1185ff
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2015-12-01 23:07:27 +0100

    Update install script with Gladys on NPM

--- rpi-install.sh
+++ rpi-install.sh
@@ -22,12 +22,6 @@ sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get install -y build-essential
 
 # Installing Node.js
-#sudo curl -sL https://deb.nodesource.com/setup | sudo bash - 
-#sudo apt-get install -y nodejs
-
-#wget http://node-arm.herokuapp.com/node_0.10.36_armhf.deb
-#sudo dpkg -i node_0.10.36_armhf.deb
-
 wget https://nodejs.org/dist/v4.2.2/node-v4.2.2-linux-armv7l.tar.xz
 tar -xvf node-v4.2.2-linux-armv7l.tar.xz
 cd node-v4.2.2-linux-armv7l
@@ -44,17 +38,12 @@ sudo apt-get install -y libasound2-dev
 # NPM global modules
 sudo npm install -g pm2
 
-cd /home/pi
-
-sudo apt-get install -y git
-#Cloning Gladys into "gladys" directory
-git clone https://github.com/GladysProject/Gladys.git gladys
-cd gladys
-
-# Installing dependencies
-sudo npm install --unsafe-perm
+# Installing Gladys - One line ! :) 
+# --unsafe-perm is because of node serialport module 
+# which does not correctly install without unsafe perm
+sudo npm install -g gladys --unsafe-perm
 
 # Starting Gladys at startup
 sudo pm2 startup
-sudo pm2 start app.js --name gladys
+sudo pm2 start gladys --name gladys
 sudo pm2 save
