857aeebc952ccfdd807899514bde4749512184c5
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2015-12-31 13:17:49 +0100

    update pm2 start command

--- rpi-2-install.sh
+++ rpi-2-install.sh
@@ -45,5 +45,5 @@ sudo npm install -g gladys --unsafe-perm
 
 # Starting Gladys at startup
 sudo pm2 startup
-sudo pm2 start gladys --name gladys
+sudo pm2 start /usr/local/lib/node_modules/gladys/app.js --name gladys
 sudo pm2 save
