d7e5c43e10e12e8a80e3b8fd3e55ea4660cb9a40
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-01-24 12:55:33 +0100

    First implementation of Device functions

---
+++ api/core/device/index.js
@@ -0,0 +1,7 @@
+
+
+module.exports.create = require('./device.create.js');
+module.exports.delete = require('./device.delete.js');
+module.exports.get = require('./device.get.js');
+module.exports.set = require('./device.set.js');
+module.exports.update = require('./device.update.js');
--- api/core/index.js
+++ api/core/index.js
@@ -1,6 +1,8 @@
 var events = require('events');
 
-var gladys = {test:1};
+var gladys = {};
 gladys = new events.EventEmitter();
+
+gladys.device = require('./device/index.js');
 
 module.exports = gladys;
