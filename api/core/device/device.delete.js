9d017f8b4ce218ee72f211a932f1698c0a65dafe
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-01-24 19:47:11 +0100

    Object param in device.delete instead of integer

--- api/core/device/device.delete.js
+++ api/core/device/device.delete.js
@@ -1,12 +1,12 @@
 module.exports = destroy;
 
-function destroy (id) {
+function destroy (options) {
     return new Promise(function(resolve, reject){
-        Device.destroy({id: id}, function(err, devices){
+        Device.destroy({id: options.id}, function(err, devices){
            if(err) return reject(err);
            
            if(devices.length === 0){
-               reject({err: 404, message: 'Device not found'});
+               reject({status: 404, message: 'Device not found'});
            }else{
                resolve(devices[0]);
            }
