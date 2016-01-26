1166ff0171ab51b1db455bcd54a2cea610b1d4ac
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-01-26 22:28:19 +0100

    get deviceType with service name

--- api/core/device/device.queries.js
+++ api/core/device/device.queries.js
@@ -8,8 +8,10 @@ module.exports = {
     OFFSET ?;
   `,
   getDeviceType: `
-    SELECT id, type, unit, min, max, device FROM devicetype
-    WHERE id = ?;
+    SELECT dt.id, dt.type, dt.unit, dt.min, dt.max, dt.device, d.service 
+    FROM device d
+    JOIN devicetype dt ON (d.id = dt.device)
+    WHERE dt.id = ?;
   `
     
 };
