a261db3905279b528c04fa86fd4a733259834539
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-01-28 20:38:57 +0100

    Device set to exec, added execution of service

--- api/core/device/device.set.js
+++ api/core/device/device.set.js
@@ -3,7 +3,8 @@ module.exports = set;
 var queries = require('./device.queries.js');
 
 /**
- * Create a new DeviceState. Useful 
+ * Set a new state for a device by executing 
+ * the related service.
  */
 function set (param) {
     return new Promise(function(resolve, reject){
@@ -17,13 +18,31 @@ function set (param) {
             if(param.value < types[0].min || param.value > types[0].max){
                 return reject({code: 422, message: 'Incorrect value.'});
             }
+
             
-            DeviceState.create(param, function(err, state){
+            if (typeof global[types[0].service] !== "function"){
+                return reject({code: 500, message: 'Service does not exist'});
+            }
+            
+            // calling service method
+            return global[types[0].service].exec(types[0])
+                .then(function(){
+                    
+                    // creating DeviceState
+                    return createState(param);
+                });     
+       });
+       
+    });
+}
+
+
+function createState (param){
+    return new Promise(function(resolve, reject){
+        DeviceState.create(param, function(err, state){
                 if(err) return reject(err);
                 
                 resolve(state); 
-            });      
-       });
-       
+            }); 
     });
 }
