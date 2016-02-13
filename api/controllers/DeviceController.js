2f3ff3516f1cc385c8aa4a4a6b2683c60a10e986
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-02-13 12:58:19 +0100

    DeviceController (get, create, update, delete)

---
+++ api/controllers/DeviceController.js
@@ -0,0 +1,60 @@
+/** 
+  * Gladys Project
+  * http://gladysproject.com
+  * Software under licence Creative Commons 3.0 France 
+  * http://creativecommons.org/licenses/by-nc-sa/3.0/fr/
+  * You may not use this software for commercial purposes.
+  * @author :: Pierre-Gilles Leymarie
+  */
+  
+module.exports = {
+  
+  
+  /**
+   * Return all devices (with pagination)
+   * @take: number of device to return
+   * @skip: for pagination
+   */
+  index: function(req, res, next) {
+      gladys.device.get(req.query)
+            .then(function(devices){
+                return res.json(devices);
+            })
+            .catch(next);
+  },
+  
+  /**
+   * Create a new device
+   */
+  create: function(req, res, next) {
+      gladys.device.create(req.body)
+            .then(function(device){
+                return res.json(device);
+            })
+            .catch(next);
+  },
+  
+  /**
+   * Update a device
+   */
+  update: function(req, res, next) {
+      req.body.id = req.params.id;
+      gladys.device.update(req.body)
+            .then(function(device){
+                return res.json(device);
+            })
+            .catch(next);
+  },
+  
+  /**
+   * Delete a device
+   */
+  destroy: function(req, res, next) {
+      gladys.device.delete(req.params.id)
+            .then(function(device){
+                return res.json(device);
+            })
+            .catch(next);
+  }
+    
+};
