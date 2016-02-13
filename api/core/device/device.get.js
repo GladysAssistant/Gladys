81e5bcf61293d0779cdc4c2bcdcbca69d04e88f4
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-02-13 12:58:45 +0100

    parseInt options in device.get, because req.query returns object of strings

--- api/core/device/device.get.js
+++ api/core/device/device.get.js
@@ -3,12 +3,12 @@ module.exports = get;
 var queries = require('./device.queries.js');
 
 function get (options) {
-    options = options || {};
-    options.offset = options.offset || 0;
-    options.limit = options.limit || 50;
-    
     return new Promise(function (resolve, reject){
-        Device.query(queries.get, [options.limit, options.offset], function(err, devices){
+        options = options || {};
+        options.skip = parseInt(options.skip) || 0;
+        options.take = parseInt(options.take) || 50;
+    
+        Device.query(queries.get, [options.take, options.skip], function(err, devices){
             if(err) return reject(err);
             
             resolve(devices);
