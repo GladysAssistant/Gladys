58b2ece2c885d352aeba601f8e7b826ba2a438a8
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-02-13 13:00:08 +0100

    update checkToken policie

--- api/policies/checkToken.js
+++ api/policies/checkToken.js
@@ -27,21 +27,23 @@ module.exports = function(req, res, next) {
   // find token in the database
   Token.findOne()
   .where({ value : req.param('token') })
-  .where({ status : true })
+  .where({ active : true })
   .populate('user')
   .exec(function foundToken(err, token) {
-  		if (err) return res.forbidden('Access denied. Error while finding token');
+  		if (err) {
+              sails.log.warn(err);
+              return res.forbidden('Access denied. Error while finding token');
+        }
 
   		if (!token)
 				return res.forbidden('Access denied.');
 		else{
 			sails.log.info("Access with token : " + req.param('token'));
-      req.session.User = token.user;
-      req.session.authenticated = true;
-      req.session.trueHuman = false;
-			return next();
-		}
-
+        	req.session.User = token.user;
+            req.session.authenticated = true;
+            req.session.trueHuman = false;
+            next();
+        } 
   });
 
 
