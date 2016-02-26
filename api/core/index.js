224e3c4d9d1fdd6e6d7fccb4c214508b18363d16
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-02-26 14:22:13 +0100

    expose scheduler in gladys global variable

--- api/core/index.js
+++ api/core/index.js
@@ -6,6 +6,7 @@ gladys = new events.EventEmitter();
 gladys.load = function load () {
     gladys.device = require('./device/index.js');
     gladys.notification = require('./notification/index.js');
+    gladys.scheduler = require('./scheduler/index.js');
 };
 
 module.exports = gladys;
