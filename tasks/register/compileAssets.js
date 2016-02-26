15eedad45c1b87c4805345e9262830bcd8014ec8
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-02-26 18:26:04 +0100

    Remove jst from grunt tasks

--- tasks/register/compileAssets.js
+++ tasks/register/compileAssets.js
@@ -1,7 +1,6 @@
 module.exports = function (grunt) {
 	grunt.registerTask('compileAssets', [
 		'clean:dev',
-		'jst:dev',
 		'less:dev',
 		'copy:dev'
 	]);
--- tasks/register/syncAssets.js
+++ tasks/register/syncAssets.js
@@ -1,6 +1,5 @@
 module.exports = function (grunt) {
 	grunt.registerTask('syncAssets', [
-		'jst:dev',
 		'less:dev',
 		'sync:dev'
 	]);
