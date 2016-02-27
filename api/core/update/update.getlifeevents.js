4e4bf957c47a6b0eb17b1147ddb232541dd5bf89
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-02-27 18:37:27 +0100

    use gladys.utils.request in update lifeevent

--- api/core/update/update.getlifeevents.js
+++ api/core/update/update.getlifeevents.js
@@ -2,21 +2,8 @@ var request = require('request');
 var Promise = require('bluebird');
 
 module.exports = function(){
-    return getEvents()
+    return gladys.utils.request(sails.config.lifeevents.url)
         .then(function(events){
            return Promise.map(events, gladys.lifeEvent.addType); 
         });
 };
-
-function getEvents(){
-   return new Promise(function(resolve, reject){
-        request(sails.config.lifeevents.url, function(err, response, body) {
-           try{
-               var events = JSON.parse(body);
-               resolve(events);
-           } catch(e) {
-               reject(e);
-           }
-        });
-    }); 
-}
