8fa3f9091231b91b77be23254e07e46c944a820b
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-02-27 18:37:11 +0100

    reject if there is an error in gladys.utils.request

--- api/core/utils/utils.request.js
+++ api/core/utils/utils.request.js
@@ -5,6 +5,8 @@ module.exports = function(param){
     // get request, return JS is json, else resolve raw body
     return new Promise(function(resolve, reject){
         request(param, function(err, response, body) {
+           if(err) return reject(err);
+           
            try{
                var json = JSON.parse(body);
                resolve(json);
