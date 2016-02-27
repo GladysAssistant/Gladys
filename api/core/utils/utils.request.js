36c7b5fc1dad09e58c57a89f488e1df24e03c67b
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-02-27 18:36:36 +0100

    gladys.utils.request expose request node module and return a promise

---
+++ api/core/utils/utils.request.js
@@ -0,0 +1,16 @@
+var request = require('request');
+
+module.exports = function(param){
+    
+    // get request, return JS is json, else resolve raw body
+    return new Promise(function(resolve, reject){
+        request(param, function(err, response, body) {
+           try{
+               var json = JSON.parse(body);
+               resolve(json);
+           } catch(e) {
+               resolve(body);
+           }
+        });
+    }); 
+};
