cfc9dc1803f5f313c0acbf70e2cfa948a891c91c
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-02-28 21:40:42 +0100

    TestService for testing purpose

--- api/services/TestService.js
+++ api/services/TestService.js
@@ -1,4 +1,6 @@
 
-module.exports.exec = function(notification){
+module.exports.exec = function(params){
+   sails.log.info('Test Service called with params : ');
+   sails.log.info(params);
    return Promise.resolve(true);
-}
+};
