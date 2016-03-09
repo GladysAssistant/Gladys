8b589f91265a8818691f9d57ef17512730ed954c
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-03-09 17:16:46 +0100

    added box model

---
+++ api/controllers/BoxController.js
@@ -0,0 +1,11 @@
+/**
+ * BoxController
+ *
+ * @description :: Server-side logic for managing boxes
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Box.js
@@ -0,0 +1,41 @@
+/**
+* Box.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+      
+      title: {
+          type: 'string',
+          required: true
+      },
+      
+      x: {
+          type: 'integer',
+          required: true
+      },
+      
+      y: {
+          type: 'integer',
+          required: true
+      },
+      
+      ngcontroller: {
+          type: 'string'
+      },
+      
+      html: {
+          type: 'text'
+      },
+      
+      icon: {
+          type: 'string'
+      }
+
+  }
+};
+
