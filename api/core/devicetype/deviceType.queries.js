d6757961e5764da990bd304c64f9f1ceb6aeb59a
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-03-13 15:01:31 +0100

    deviceType.getAll

--- api/core/devicetype/deviceType.queries.js
+++ api/core/devicetype/deviceType.queries.js
@@ -6,4 +6,10 @@ module.exports = {
     JOIN devicetype dt ON (d.id = dt.device)
     WHERE dt.id = ?;
   `,
+  getAll: `
+    SELECT dt.id, dt.type, dt.tag,  dt.unit, dt.min, dt.max, dt.device, d.service, r.name AS roomName, r.id as roomId 
+    FROM device d
+    JOIN devicetype dt ON (d.id = dt.device)
+    JOIN room r ON (d.room = r.id);
+  `
 };
--- api/core/devicetype/index.js
+++ api/core/devicetype/index.js
@@ -2,3 +2,4 @@
 module.exports.create = require('./deviceType.create.js');
 module.exports.exec = require('./deviceType.exec.js');
 module.exports.save = require('./deviceType.save.js');
+module.exports.getAll = require('./deviceType.getAll.js');
