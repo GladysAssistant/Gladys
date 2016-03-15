7cbb0b251b6adbba7777bf99ccd945d0ef4b55f4
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-03-15 18:38:03 +0100

    angular cacheService for caching weather

---
+++ assets/js/app/cache/cacheService.js
@@ -0,0 +1,71 @@
+/** 
+  * Gladys Project
+  * http://gladysproject.com
+  * Software under licence Creative Commons 3.0 France 
+  * http://creativecommons.org/licenses/by-nc-sa/3.0/fr/
+  * You may not use this software for commercial purposes.
+  * @author :: Pierre-Gilles Leymarie
+  */
+  
+  
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('cacheService', cacheService);
+
+    cacheService.$inject = [];
+
+    function cacheService() {
+        
+        var service = {
+            get: get,
+            set: set
+        };
+
+        return service;
+
+        /**
+         * Get value from localStorage
+         */
+        function get(key) {
+            try{
+                var value = null;
+                if(localStorage){
+                    
+                    // get the item from the localstorage
+                    var record = localStorage.getItem(key);
+                    record = JSON.parse(record);
+                    
+                    // if the expiration date is not passed, retrieve value
+                    if(record.expiration > new Date().getTime()){
+                        value = record.value;
+                    } else {
+                        
+                        // if note, delete value in storage
+                        localStorage.removeItem(key);
+                    }
+                }
+                return value;      
+            } catch(e){
+                console.log(e);
+                return null;
+            }
+        }
+        
+        /**
+         * Set value in the localStorage
+         */
+        function set(key, value, expiration){
+            
+            if(localStorage){
+                var record = {
+                    value: value, 
+                    expiration: new Date().getTime() + expiration
+                };
+                localStorage.setItem(key, JSON.stringify(record));
+            }
+        }
+    }
+})();
