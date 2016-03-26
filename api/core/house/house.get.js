85e968ca6149ded4f16fec2e412b3d4a474cc225
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-03-26 17:58:41 +0100

    parseInt options in house.get.js

--- api/core/house/house.get.js
+++ api/core/house/house.get.js
@@ -3,8 +3,8 @@ var queries = require('./house.queries.js');
 module.exports = function get (options){
     
     options = options || {};
-    options.take = options.take || 50;
-    options.skip = options.skip || 0;
+    options.take = parseInt(options.take) || 50;
+    options.skip = parseInt(options.skip) || 0;
     
     return gladys.utils.sql(queries.get, [options.take, options.skip]);
 };
