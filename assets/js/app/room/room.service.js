7f53c4f8b9585697538732ed3f89c40797b0a496
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-03-26 17:56:48 +0100

    houseCtrl and service, and roomService updated

--- assets/js/app/room/room.service.js
+++ assets/js/app/room/room.service.js
@@ -17,14 +17,30 @@
     roomService.$inject = ['$http'];
 
     function roomService($http) {
+        
         var service = {
-            get: get
+            create: create,
+            get: get,
+            destroy: destroy,
+            update: update
         };
 
         return service;
 
-        function get() {
-            return $http({method: 'GET', url: '/room' });
+        function get(options) {
+            return $http({method: 'GET', url: '/room', params: options });
+        }
+        
+        function create(room){
+            return $http({method: 'POST', url: '/room', data: room });
+        }
+        
+        function destroy(id){
+            return $http({method: 'DELETE', url: '/room/' + id });
+        }
+        
+        function update(id, room){
+            return $http({method: 'PATCH', url: '/room/' + id, data: room });
         }
 
     }
