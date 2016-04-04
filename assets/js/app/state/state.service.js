b16017ba118ca1b9bf607864e11d7f61a965cb43
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-04-04 22:05:37 +0200

    state service front

---
+++ assets/js/app/state/state.service.js
@@ -0,0 +1,37 @@
+/** 
+  * Gladys Project
+  * http://gladysproject.com
+  * Software under licence Creative Commons 3.0 France 
+  * http://creativecommons.org/licenses/by-nc-sa/3.0/fr/
+  * You may not use this software for commercial purposes.
+  * @author :: Pierre-Gilles Leymarie
+  */
+  
+(function () {
+    'use strict';
+
+    angular
+        .module('gladys')
+        .factory('stateService', stateService);
+
+    stateService.$inject = ['$http'];
+
+    function stateService($http) {
+        
+        var service = {
+            get: get,
+            create: create
+        };
+
+        return service;
+
+        function get(){
+            return $http({method: 'GET', url: '/state' });
+        }
+        
+        function create(state){
+            return $http({method: 'POST', url: '/state', data: state });
+        }
+        
+    }
+})();
