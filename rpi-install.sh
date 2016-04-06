773ebca333157c61cb099ce7aa4b8849b9b14e94
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-04-06 15:19:01 +0200

    Update install script

--- rpi-install.sh
+++ rpi-install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ###########################################################
 # Gladys Project
 # http://gladysproject.com
@@ -10,26 +12,33 @@
 
 
 # This install files is for  simplifying the installation of Gladys
-# on a Raspberry Pi 2.
+# on a Raspberry Pi
 # Make sure your firmware version is up to date,
 # You can upgrade with the command "sudo rpi-update"
 
 
+
 # Update & upgrade package
+echo "apt-get install and apt-get upgrade" 
 sudo apt-get update && sudo apt-get -y upgrade
 
 # Installing building tools
 sudo apt-get install -y build-essential
 
 # Installing Node.js
-wget https://nodejs.org/dist/v4.2.2/node-v4.2.2-linux-armv7l.tar.xz
-tar -xvf node-v4.2.2-linux-armv7l.tar.xz
-cd node-v4.2.2-linux-armv7l
+echo "Installing Node.js"
+wget https://nodejs.org/dist/v4.4.2/node-v4.4.2-linux-armv6l.tar.xz
+tar -xvf node-v4.4.2-linux-armv6l.tar.xz
+cd node-v4.4.2-linux-armv6l
 sudo cp -R * /usr/local/
 
 # Install MySQL
+echo "Installing MySQL"
 sudo apt-get install -y mysql-server
+
+
 # Creating database gladys
+echo "Creating MySQL DB"
 mysql -u root -proot -e "create database gladys"
 
 # Dependencies
@@ -38,12 +47,14 @@ sudo apt-get install -y libasound2-dev
 # NPM global modules
 sudo npm install -g pm2
 
-# Installing Gladys - One line ! :) 
-# --unsafe-perm is because of node serialport module 
-# which does not correctly install without unsafe perm
-sudo npm install -g gladys --unsafe-perm
+# Installing Gladys - One line ! :)
+sudo npm install -g gladys
+
+# Init script of Gladys
+cd /usr/local/lib/node_modules/gladys/
+node init.js
 
 # Starting Gladys at startup
-sudo pm2 startup
-sudo pm2 start gladys --name gladys
-sudo pm2 save
+pm2 startup
+pm2 start /usr/local/lib/node_modules/gladys/app.js --name gladys
+pm2 save
