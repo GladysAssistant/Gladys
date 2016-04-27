e83ca8448fad3f97c23098936c5aebecc87ecb97
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-04-27 14:09:14 +0200

    handling device set-device-off in deviceType.command

--- api/core/devicetype/deviceType.command.js
+++ api/core/devicetype/deviceType.command.js
@@ -3,15 +3,21 @@ var Promise = require('bluebird');
 module.exports = function command(options){
   console.log(options); 
   
+  var value;
+  
   switch(options.label){
       case 'set-device-on':
-        if(options.deviceTypes.length){
-            return Promise.map(options.deviceTypes, function(deviceType){
-                return gladys.deviceType.exec({devicetype: deviceType.id, value: 1});
-            });
-        } 
+        value = 1;
+      break;
+      case 'set-device-off': 
+        value = 0;
       break;
   }
   
+   if(options.deviceTypes.length){
+        return Promise.map(options.deviceTypes, function(deviceType){
+            return gladys.deviceType.exec({devicetype: deviceType.id, value: value});
+        });
+   } 
    
 };
