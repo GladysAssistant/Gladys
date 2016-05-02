159a56d81ae876e669fe451855a79d9c6481a9f1
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-05-02 20:45:52 +0200

    gladys.utils.concatArray

--- api/core/scenario/scenario.trigger.js
+++ api/core/scenario/scenario.trigger.js
@@ -36,7 +36,7 @@ function verifyAndStart(launcher, scope) {
             // it's ok, so we start all the actions
             var obj = {
                 launcher: launcher,
-                scope: concatArray(conditions, scope)
+                scope: gladys.utils.concatArray(conditions, scope)
             };
             return gladys.scenario.exec(obj);
         })
@@ -64,19 +64,4 @@ function verifyLauncherCondition(launcher, scope) {
     } catch (e) {
         return Promise.reject(new Error(e));
     }
-}
-
-// take in parameters an array of object. Ex : [{ temperature: 12}, {humidity: 50}]
-// and a object 'scope'. Ex: {user: 1}
-// returns a single object with every property inside.
-// ex : return {temperature: 12, humidity: 50, user:1}
-function concatArray(arr, scope) {
-    arr.forEach(function(item) {
-        for (var prop in item) {
-            if (item.hasOwnProperty(prop) && !scope.hasOwnProperty(prop)) {
-                scope[prop] = item[prop];
-            }
-        }
-    });
-    return scope;
 }
--- api/core/utils/index.js
+++ api/core/utils/index.js
@@ -1,3 +1,4 @@
+module.exports.concatArray = require('./utils.concatArray.js');
 module.exports.sql = require('./utils.sql.js');
 module.exports.sqlUnique = require('./utils.sqlUnique.js');
 module.exports.request = require('./utils.request.js');
