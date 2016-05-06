9c71feeb6be50c12755699b420aead8b346c19b7
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-05-06 14:21:25 +0200

    test launcherParam insertBatch

---
+++ test/unit/api/validator/launcherParamValidator.js
@@ -0,0 +1,17 @@
+module.exports = validate;
+
+var should = require('should');
+
+function validate(launcherParam) {
+	if(launcherParam instanceof Array) {
+		launcherParam.forEach(validatelauncherParam);
+	} else {
+		validatelauncherParam(launcherParam);
+	}
+}
+
+function validatelauncherParam(launcherParam) {
+	launcherParam.should.be.instanceOf(Object);
+	launcherParam.should.have.property('variablename');
+	launcherParam.should.have.property('eventtype');
+}
