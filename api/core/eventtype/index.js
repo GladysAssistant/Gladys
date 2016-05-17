7657877b2ba2271130c3f215c8b94b1c00b26a30
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-05-17 19:11:36 +0200

    eventType getByCode

--- api/core/eventtype/index.js
+++ api/core/eventtype/index.js
@@ -1,3 +1,4 @@
 
 module.exports.create = require('./eventType.create.js');
 module.exports.getByCategory = require('./eventType.getByCategory.js');
+module.exports.getByCode = require('./eventType.getByCode.js');
---
+++ test/unit/api/core/eventtype/eventtype.getByCode.test.js
@@ -0,0 +1,19 @@
+var should = require('should');
+var validateEventType = require('../../validator/eventTypeValidator.js');
+
+describe('EventType', function() {
+
+  describe('getByCode', function() {
+    
+    it('should return eventType by code', function (done) {
+          
+          gladys.eventType.getByCode({code: 'test'})
+                .then(function(result){
+                    validateEventType(result);
+                    done();
+                }).catch(done);
+    });
+    
+  });
+
+});
