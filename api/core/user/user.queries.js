e8439750c562119aa118d4b7277e848b23c25585
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-05-23 18:58:53 +0200

    user getById

--- api/core/user/user.queries.js
+++ api/core/user/user.queries.js
@@ -3,5 +3,6 @@ module.exports = {
   getByEmail: 'SELECT * FROM user WHERE email = ?;',
   get: 'SELECT * FROM user LIMIT ? OFFSET ?;' ,
   getAdmin: 'SELECT * FROM user WHERE role = "admin";',
+  getById: 'SELECT * FROM user WHERE id = ?;',
   delete: 'DELETE FROM user WHERE id = ?;' 
 };
