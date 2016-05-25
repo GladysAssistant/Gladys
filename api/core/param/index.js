2c51abc3248a8310d4dca48d8bcf4ab803479bac
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-05-25 10:41:40 +0200

    param.getValues

--- api/core/param/index.js
+++ api/core/param/index.js
@@ -3,4 +3,5 @@ module.exports.clearCache = require('./param.clearCache.js');
 module.exports.get = require('./param.get.js');
 module.exports.delete = require('./param.delete.js');
 module.exports.getValue = require('./param.getValue.js');
+module.exports.getValues = require('./param.getValues.js');
 module.exports.setValue = require('./param.setValue.js');
