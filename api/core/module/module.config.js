ac1737481724652fbc20db97d90c3111b334184a
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-06-14 21:02:14 +0200

    rename config function to setup (config is a sails.js reserved name)

--- api/core/module/module.config.js
+++ api/core/module/module.config.js
@@ -1,11 +1,11 @@
 
 module.exports = function(module){
    
-   // test if config function exist
-   if (typeof gladys.modules[module.slug].config !== "function") {
+   // test if setup function exist
+   if (typeof gladys.modules[module.slug].setup !== "function") {
      
      return Promise.resolve();
    } else {
-       return gladys.modules[module.slug].config();
+       return gladys.modules[module.slug].setup();
    }
 };
