8e4d8e4e3780932be7f86c1eec4bdae66a50a218
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-06-15 17:48:59 +0200

    DeviceState createByIdentifier tested

--- api/core/devicestate/index.js
+++ api/core/devicestate/index.js
@@ -1,3 +1,4 @@
 
 module.exports.create = require('./deviceState.create.js');
+module.exports.createByIdentifier = require('./deviceState.createByIdentifier.js');
 module.exports.get = require('./deviceState.get.js');
