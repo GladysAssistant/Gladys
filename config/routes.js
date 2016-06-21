e8ef134a25fbed2911c45d0b9c488187768185cc
Jean-PhilippeD <jp.danree@free.fr>
2016-06-21 11:32:58 +0200

    Update routes.js

--- config/routes.js
+++ config/routes.js
@@ -90,6 +90,7 @@ module.exports.routes = {
   'patch /device/:id': 'DeviceController.update',
   'delete /device/:id': 'DeviceController.delete',
   'get /device/:id/devicetype': 'DeviceController.getDeviceTypes',
+  'get /device/:service': 'DeviceController.getDeviceByService',
   
   
   // DeviceState
