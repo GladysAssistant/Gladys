f6d1fe6eb0c359e59eb7eaf25923ff23b2c24f98
Jean-PhilippeD <jp.danree@free.fr>
2016-06-21 11:37:32 +0200

    Update DeviceController.js

--- api/controllers/DeviceController.js
+++ api/controllers/DeviceController.js
@@ -35,17 +35,6 @@ module.exports = {
   },
 
   /**
-   * Return all devices for a specific service
-   */
-  getDeviceByService: function(req, res, next){
-    gladys.device.getByService({service: req.params.service})
-      .then(function(devices){
-          return res.json(devices);
-      }) 
-      .catch(next);
-  },
-  
-  /**
    * Create a new device
    */
   create: function(req, res, next) {
