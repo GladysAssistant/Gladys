4ca7c0e581c2d7eaedd62d4d6b0c5d4c84a9704b
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-09-17 11:06:30 +0200

    Fix user login tests

--- api/core/user/user.queries.js
+++ api/core/user/user.queries.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  getByEmail: 'SELECT id, firstname, lastname, email, birthdate, gender, language, assistantName, preparationTimeAfterWakeUp, role, createdAt, updatedAt FROM user WHERE email = ?;',
+  getByEmail: 'SELECT * FROM user WHERE email = ?;',
   get: 'SELECT id, firstname, lastname, email, birthdate, gender, language, assistantName, preparationTimeAfterWakeUp, role, createdAt, updatedAt FROM user LIMIT ? OFFSET ?;' ,
   getAdmin: 'SELECT id, firstname, lastname, email, birthdate, gender, language, assistantName, preparationTimeAfterWakeUp, role, createdAt, updatedAt FROM user WHERE role = "admin";',
   getById: 'SELECT id, firstname, lastname, email, birthdate, gender, language, assistantName, preparationTimeAfterWakeUp, role, createdAt, updatedAt FROM user WHERE id = ?;',
