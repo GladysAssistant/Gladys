abad101366f53ef9a261a90d21c4d5ea1444369e
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-09-22 13:32:40 +0200

    Update script now copy back installed hooks on update

--- rpi-update.sh
+++ rpi-update.sh
@@ -3,7 +3,7 @@
 GLADYS_TOP_FOLDER="/home/pi/gladys"
 GLADYS_FOLDER="/home/pi/gladys/node_modules/gladys"
 HOOK_FOLDER="$GLADYS_FOLDER/api/hooks"
-CACHE_FOLDER="$GLADYS_FOLDER/cache/*"
+CACHE_FOLDER="$GLADYS_FOLDER/cache"
 TMP_HOOK_FOLDER="/tmp/gladys_hooks"
 TMP_CACHE_FOLDER="/tmp/gladys_cache"
 INIT_SCRIPT="$GLADYS_FOLDER/init.js"
@@ -20,19 +20,11 @@ mkdir $TMP_HOOK_FOLDER
 # Then, we create the temp hook folder
 mkdir $TMP_CACHE_FOLDER
 
-# if the hook folder exist
-if [[ -d "$HOOK_FOLDER" ]]; then
-    
-    # We copy the hooks repository of the old folder
-    cp -r $HOOK_FOLDER $TMP_HOOK_FOLDER
-fi
+# We copy the hooks repository of the old folder
+cp -r /home/pi/gladys/node_modules/gladys/api/hooks/* $TMP_HOOK_FOLDER
 
-# if the cache folder exist
-if [[ -d "$CACHE_FOLDER" ]]; then
-    
-    # We copy the cache folder of the old gladys
-    cp -r $CACHE_FOLDER $TMP_CACHE_FOLDER
-fi
+# We copy the cache folder of the old gladys
+cp -r /home/pi/gladys/node_modules/gladys/cache/* $TMP_CACHE_FOLDER
 
 # stopping gladys (silent is in case gladys is not running)
 # silent remove any errors cause by PM2
@@ -41,6 +33,10 @@ pm2 stop --silent gladys
 cd $GLADYS_TOP_FOLDER
 
 npm update gladys
+
+# we copy back the hook and cache folder
+cp -r /tmp/gladys_hooks/* $HOOK_FOLDER
+cp -r /tmp/gladys_cache/*  $CACHE_FOLDER
 
 # start init script
 node $INIT_SCRIPT
