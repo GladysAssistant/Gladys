f5852c1fcdcb115bb4d0d6ba0532b2404508df44
Jean-PhilippeD <jp.danree@free.fr>
2016-09-25 15:41:16 +0200

    add getbyType

--- api/core/devicetype/deviceType.queries.js
+++ api/core/devicetype/deviceType.queries.js
@@ -1,21 +1,27 @@
 
 module.exports = {
   getDeviceType: `
-    SELECT dt.id, dt.type, dt.unit, dt.min, dt.max, d.identifier, dt.device, d.service
+    SELECT dt.id, dt.type, dt.unit, dt.min, dt.max, d.identifier, dt.device, d.service, dt.identifier as deviceTypeIdentifier
     FROM device d
     JOIN devicetype dt ON (d.id = dt.device)
     WHERE dt.id = ?;
   `,
    getByRoom: `
-    SELECT d.name, dt.id, dt.type, dt.unit, dt.min, dt.max, dt.display, dt.sensor, d.identifier, dt.device, d.service,
-    ds.datetime as lastChanged, ds.value AS lastValue, ds.id AS lastValueId
-    FROM device d
-    JOIN devicetype dt ON (d.id = dt.device)
-    LEFT JOIN devicestate ds ON (ds.devicetype = dt.id)
-    WHERE d.room = ?
-    HAVING lastValueId = 
-        (SELECT id FROM devicestate WHERE devicestate.devicetype = dt.id ORDER BY datetime DESC LIMIT 1)
-        OR lastValueId IS NULL;
+   SELECT d.name, dt.id, dt.type, dt.unit, dt.min, dt.max, dt.display, dt.sensor, d.identifier, dt.device, d.service,
+   ds3.datetime as lastChanged, ds3.value AS lastValue, ds3.id AS lastValueId
+   FROM device d
+   JOIN devicetype dt ON (d.id = dt.device)
+   LEFT JOIN (
+      SELECT ds.devicetype, MAX(id) as id
+      FROM devicestate ds 
+      INNER JOIN (
+        SELECT devicetype, MAX(datetime) as datetime FROM devicestate GROUP BY devicetype
+      ) as dsJoin
+      WHERE dsJoin.devicetype = ds.devicetype AND dsJoin.datetime = ds.datetime
+      GROUP by ds.devicetype
+  ) as deviceStateJoin ON (deviceStateJoin.devicetype = dt.id)
+   LEFT JOIN devicestate ds3 ON deviceStateJoin.id = ds3.id
+   WHERE d.room = ?;
   `,
   getByDevice: `
     SELECT dt.*, ds.datetime as lastChanged, ds.value AS lastValue, ds.id AS lastValueId
@@ -31,5 +37,36 @@ module.exports = {
     FROM device d
     JOIN devicetype dt ON (d.id = dt.device)
     JOIN room r ON (d.room = r.id);
-  `
+  `,
+  getById: `
+    SELECT d.name, dt.id, dt.type, dt.unit, dt.min, dt.max, dt.display, dt.sensor, d.identifier, dt.device, d.service,
+    ds.datetime as lastChanged, ds.value AS lastValue, ds.id AS lastValueId
+    FROM device d
+    JOIN devicetype dt ON (d.id = dt.device)
+    LEFT JOIN devicestate ds ON dt.id = ds.devicetype
+    WHERE dt.id = ?
+    ORDER BY ds.id DESC
+    LIMIT 1;
+  `,
+  getByDeviceAndIdentifier: 'SELECT id FROM devicetype WHERE device = ? AND identifier = ?;',
+  delete : 'DELETE FROM devicetype WHERE id = ?;',
+  deleteDeviceStates: 'DELETE FROM devicestate WHERE devicetype = ?;',
+  getByType: `
+    SELECT d.name, dt.id, dt.type, dt.unit, dt.min, dt.max, dt.display, dt.sensor, d.identifier, dt.device, d.service,
+    ds3.datetime as lastChanged, ds3.value AS lastValue, ds3.id AS lastValueId, r.name as room
+    FROM device d
+    JOIN room r ON (d.room = r.id)
+    JOIN devicetype dt ON (d.id = dt.device)
+    LEFT JOIN (
+      SELECT ds.devicetype, MAX(id) as id
+      FROM devicestate ds
+      INNER JOIN (
+        SELECT devicetype, MAX(datetime) as datetime FROM devicestate GROUP BY devicetype
+      ) as dsJoin
+      WHERE dsJoin.devicetype = ds.devicetype AND dsJoin.datetime = ds.datetime
+      GROUP by ds.devicetype
+    ) as deviceStateJoin ON (deviceStateJoin.devicetype = dt.id)
+    LEFT JOIN devicestate ds3 ON deviceStateJoin.id = ds3.id
+    WHERE dt.type = ?;
+   `,
 };
--- api/core/devicetype/index.js
+++ api/core/devicetype/index.js
@@ -1,8 +1,11 @@
 
 module.exports.create = require('./deviceType.create.js');
 module.exports.command = require('./deviceType.command.js');
+module.exports.delete = require('./deviceType.delete.js');
 module.exports.exec = require('./deviceType.exec.js');
 module.exports.save = require('./deviceType.save.js');
 module.exports.getAll = require('./deviceType.getAll.js');
 module.exports.getByRoom = require('./deviceType.getByRoom.js');
 module.exports.getByDevice = require('./deviceType.getByDevice.js');
+module.exports.getByType = require('./deviceType.getByType.js');
+module.exports.getById = require('./deviceType.getById.js');
