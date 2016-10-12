94b5476a5368d2d2d97c6bb63d537e2ae213bc48
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-10-12 08:40:30 +0200

    add name in getUser request

--- api/core/user/user.queries.js
+++ api/core/user/user.queries.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   getByEmail: 'SELECT * FROM user WHERE email = ?;',
-  get: 'SELECT id, firstname, lastname, email, birthdate, gender, language, assistantName, preparationTimeAfterWakeUp, role, createdAt, updatedAt FROM user;' ,
+  get: 'SELECT id, firstname, lastname, CONCAT(firstname, " ", lastname) as name, email, birthdate, gender, language, assistantName, preparationTimeAfterWakeUp, role, createdAt, updatedAt FROM user;' ,
   getAdmin: 'SELECT id, firstname, lastname, email, birthdate, gender, language, assistantName, preparationTimeAfterWakeUp, role, createdAt, updatedAt FROM user WHERE role = "admin";',
   getById: 'SELECT id, firstname, lastname, email, birthdate, gender, language, assistantName, preparationTimeAfterWakeUp, role, createdAt, updatedAt FROM user WHERE id = ?;',
   delete: 'DELETE FROM user WHERE id = ?;' 
