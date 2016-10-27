55eb1fa771ac226232d832b018401d5f1226583b
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-10-27 08:36:28 +0200

    adapt utils.request to accept scenario

--- api/core/utils/utils.request.js
+++ api/core/utils/utils.request.js
@@ -2,8 +2,22 @@ var request = require('request');
 
 module.exports = function(param) {
 
+    // handle scenarios
+    if(param.params) param = param.params;
+
     // get request, return JS is json, else resolve raw body
     return new Promise(function(resolve, reject) {
+
+        // we parse the JSON body from scenario 
+        if(param.json === 'true' && param.body && typeof param.body === 'string' ) {
+            try{
+                param.body = JSON.parse(param.body);
+                param.json = true;
+            }  catch(e) {
+                return reject(e);
+            }
+        }
+
         request(param, function(err, response, body) {
             if (err) return reject(err);
 
