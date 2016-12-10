24484be39a7bd5623c6c3eff0935c8ccd616878b
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-12-10 16:45:22 +0100

    send mode id into scope in mode change

--- api/core/mode/mode.change.js
+++ api/core/mode/mode.change.js
@@ -1,13 +1,22 @@
+var queries = require('./mode.queries.js');
 
 module.exports = function(params){
 
   if(!params.house || !params.mode){
       return Promise.reject(new Error('Missing parameters'));
   }
+
+  // get mode id
+  return gladys.utils.sqlUnique(queries.getByCode, [params.mode])
+    .then((mode) => {
         
-  return gladys.event.create({
-      code: 'house-mode-changed',
-      house: params.house,
-      value: params.mode
-  });  
+        return gladys.event.create({
+            code: 'house-mode-changed',
+            house: params.house,
+            value: params.mode,
+            scope: {
+                mode: mode.id
+            }
+        });  
+    });
 };
