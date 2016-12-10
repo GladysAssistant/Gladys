327256206c4f4451d99474af23e2cc340fe61fe9
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-12-10 18:07:01 +0100

    add unit test to GET /devicetype/room

---
+++ test/unit/api/controllers/DeviceType/deviceType.getByRoom.test.js
@@ -0,0 +1,29 @@
+var request = require('supertest');
+var validateDeviceType = require('../../validator/deviceTypeValidator.js');
+
+describe('DeviceTypeController', function() {
+
+  describe('getByRoom', function() {
+    
+    it('should get all devicetype by room', function (done) {
+        
+     	request(sails.hooks.http.app)
+        .get('/devicetype/room?token=test')
+        .expect(200)
+        .end(function(err, res) {
+            if(err) return done(err);
+
+            res.body.should.be.instanceOf(Array);
+            res.body.forEach(function(room) {
+                validateDeviceType(room.deviceTypes);
+            });
+            
+            done();
+        });
+
+    });
+    
+  });
+
+
+});
