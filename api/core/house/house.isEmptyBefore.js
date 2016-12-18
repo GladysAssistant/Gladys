f9d32ae872a69af85dbf9c58e49ee7bd4e399ac6
Jean-PhilippeD <jp.danree@free.fr>
2016-12-18 14:41:20 +0100

    Update house.isEmptyBefore.js

--- api/core/house/house.isEmptyBefore.js
+++ api/core/house/house.isEmptyBefore.js
@@ -7,7 +7,7 @@
 
           // If users is empty, nobody is at home, return true
           if(!users.length) return true;
-          // If user is provided, is this user at home after event, is so, it means nobody was at home before the user came back, return true
+          // If user is provided and this user is at home after event, it means nobody was at home before the user came back, return true
           if( options.user &&  users.length == 1 && users[0].id == options.user) return true;
           return false;
       });
