48ed4870696535382e397a126299f54080664bd8
Jean-PhilippeD <jp.danree@free.fr>
2017-01-27 21:56:21 +0100

    rename state to getState

--- api/core/sun/index.js
+++ api/core/sun/index.js
@@ -1,5 +1,5 @@
 
 module.exports.init = require('./sun.init.js');
-module.exports.state = require('./sun.state.js');
+module.exports.getState = require('./sun.getState.js');
 module.exports.isItNight = require('./sun.isItNight.js');
 module.exports.isItDay = require('./sun.isItDay.js');
