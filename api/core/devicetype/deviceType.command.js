584c826086c643d83071e70f7bbb7a788ebac32b
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2017-01-30 08:20:39 +0100

    deviceType.command now able to turn on lights in specific room of the house

--- api/core/devicetype/deviceType.command.js
+++ api/core/devicetype/deviceType.command.js
@@ -14,10 +14,27 @@ module.exports = function command(options){
       break;
   }
   
-   if(options.deviceTypes.length){
+   if(options.deviceTypes && options.deviceTypes.length){
         return Promise.map(options.deviceTypes, function(deviceType){
             return gladys.deviceType.exec({devicetype: deviceType.id, value: value});
         });
-   } 
+   } else if(options.rooms && options.rooms.length) {
+        
+        // foreach room found
+        return Promise.map(options.rooms, function(room) {
+
+            // get all binary lights deviceType in the room
+            return gladys.deviceType.getByCategory({category: 'light', type: 'binary', room: room.id})
+              .then((deviceTypes) => {
+
+                 sails.log.info(`DeviceType.command : Found ${deviceTypes.length} deviceType in room ${room.name}`);
+                  
+                  // then, foreach deviceTypes found, turn it on/off
+                  return Promise.map(deviceTypes, function(deviceType) {
+                      return gladys.deviceType.exec({devicetype: deviceType.id, value: value});
+                  });
+              })
+        });
+   }
    
 };
