b12b66c234be62256da0b1a6e29633f252510b05
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2017-04-23 16:00:30 +0200

    expose gladys.brain.answer()

--- api/core/brain/index.js
+++ api/core/brain/index.js
@@ -1,7 +1,7 @@
 
+module.exports.answer = require('./brain.answer.js');
 module.exports.classify = require('./brain.classify.js');
 module.exports.command = require('./brain.command.js');
 module.exports.save = require('./brain.save.js');
 module.exports.trainNew = require('./brain.trainNew.js');
 module.exports.load = require('./brain.load.js');
-
