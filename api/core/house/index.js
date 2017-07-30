81d7e5cb1f4e4345115865b2bd3692ea568edb38
pjap93 <pjap9392@gmail.com>
2017-07-30 18:07:45 +0200

    Update index.js
    Ajout de la condition si Maison non vide

--- api/core/house/index.js
+++ api/core/house/index.js
@@ -7,6 +7,7 @@ module.exports.getById = require('./house.getById.js');
 module.exports.getUsers = require('./house.getUsers.js');
 module.exports.getAll = require('./house.getAll.js');
 module.exports.isEmpty = require('./house.isEmpty.js');
+module.exports.isNotEmpty = require('./house.isNotEmpty.js');
 module.exports.isUserAtHome = require('./house.isUserAtHome.js');
 module.exports.update = require('./house.update.js');
 module.exports.userSeen = require('./house.userSeen.js');
