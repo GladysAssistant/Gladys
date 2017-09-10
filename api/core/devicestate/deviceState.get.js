47017245ebd98402c7eb5027cfd6ca11da070642
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2017-09-10 15:36:51 +0200

    Fix #214 : Get /devicestate now return all devicestate !

--- api/core/devicestate/deviceState.get.js
+++ api/core/devicestate/deviceState.get.js
@@ -3,6 +3,10 @@ var queries = require('./deviceState.queries.js');
 module.exports = function get(options){
     options.take = parseInt(options.take) || 25;
     options.skip = parseInt(options.skip) || 0;
-    
-    return gladys.utils.sql(queries.get, [options.devicetype, options.take, options.skip]);
+
+    if(options.devicetype) {
+        return gladys.utils.sql(queries.getByDeviceType, [options.devicetype, options.take, options.skip]);
+    }  else {
+        return gladys.utils.sql(queries.get, [options.take, options.skip]);
+    }
 };
