beefc8aa3c30469b812a5fde5d4d05ada2ebd242
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2017-09-10 16:47:41 +0200

    add house.isNotEmpty function !

---
+++ api/core/house/house.isNotEmpty.js
@@ -0,0 +1,13 @@
+
+module.exports = function(options){
+    
+    // we get users in house
+    return gladys.house.getUsers(options)  
+      .then(function(users){
+          
+          // if there are user inside, return true
+          if(users.length) return true;
+          
+          return false;
+      });
+  };
