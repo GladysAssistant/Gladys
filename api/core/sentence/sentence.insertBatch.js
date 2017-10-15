5289f7ca999d2b88499e29fe58dcf6fff2c364fa
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2017-10-15 16:10:19 +0200

    don't clean sentence table anymore before inserting new ones

--- api/core/sentence/sentence.insertBatch.js
+++ api/core/sentence/sentence.insertBatch.js
@@ -3,20 +3,15 @@ const queries = require('./sentence.queries.js');
 
 module.exports = function insertBatch(elements) {
 
-    // first, we clean the sentences table
-    return gladys.utils.sql(queries.cleanSentencesTable, [])
-        .then(() => {
-
-            // then, foreach sentence we insert it in the DB
-            return Promise.map(elements, function(element) {
+    // then, foreach sentence we insert it in the DB
+    return Promise.map(elements, function(element) {
         
-                return Promise.map(element.sentences, function(sentence){
-                    sentence.service = element.service;
-                    sentence.label = element.label;
-                    sentence.language = element.language;
+        return Promise.map(element.sentences, function(sentence){
+            sentence.service = element.service;
+            sentence.label = element.label;
+            sentence.language = element.language;
 
-                    return gladys.sentence.create(sentence);
-                });
-            });
+            return gladys.sentence.create(sentence);
         });
+    });
 };
