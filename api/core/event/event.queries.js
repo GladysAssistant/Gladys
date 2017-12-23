26be60d96ca334f478089e1b2f2d926b82d8b111
Jean-PhilippeD <jp.danree@free.fr>
2017-12-23 14:31:13 +0100

    event purge

--- api/core/event/event.queries.js
+++ api/core/event/event.queries.js
@@ -4,5 +4,11 @@ module.exports = {
         JOIN eventtype ON (event.eventtype = eventtype.id)
         WHERE user = ? ORDER BY datetime DESC LIMIT ? OFFSET ?;`,
     getByCode: 'SELECT * FROM eventtype WHERE code = ?;',
-    getById: 'SELECT * FROM eventtype WHERE id = ?;'
+    getById: 'SELECT * FROM eventtype WHERE id = ?;',
+    purge: `
+        DELETE FROM event
+        WHERE value = ?
+        AND eventtype = ?
+        AND datetime < NOW() - INTERVAL ? DAY;
+    `
 };
