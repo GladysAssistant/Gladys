a61a3e8e02b949ee4e7a45186129840b84d30b4b
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-01-27 11:02:48 +0100

    event.purge() : add checks on options parameters

--- test/unit/api/core/event/event.purge.test.js
+++ test/unit/api/core/event/event.purge.test.js
@@ -3,41 +3,45 @@ var validateEvent = require('../../validator/eventValidator.js');
 
 describe('Event', function() {
 
-  describe('purge', function() {
-    
-    it('should return 3 events', function (done) {
-     	
-        var event = {
-            params: {
-            	eventtype: 3,
-            	value: 1,
-            	user: 1,
-            	datetime: new Date()
-	    }
-        };
+    describe('purge', function() {
 
-        gladys.event.create(event)
-        .then(function(result){
+        it('should return 3 events', function(done) {
 
-           var options = {
-             eventtype : 3,
-             days : 1,
-             value: 1
-           }
+            var event = {
+                params: {
+                    eventtype: 3,
+                    value: 1,
+                    user: 1,
+                    datetime: new Date()
+                }
+            };
 
-           return gladys.event.purge(options);
-        })
-        .then(function() {
+            gladys.event.create(event)
+                .then(function(result) {
 
-           return gladys.event.get({user: {id: 1}});
-        })
-        .then(function(result){
-           validateEvent(result);
-           result.should.be.instanceof(Array);
-           result.should.have.length(3);
-           done();
-        })
-        .catch(done)
+                    var options = {
+                        eventtype: 3,
+                        days: 1,
+                        value: 1
+                    }
+
+                    return gladys.event.purge(options);
+                })
+                .then(function() {
+
+                    return gladys.event.get({
+                        user: {
+                            id: 1
+                        }
+                    });
+                })
+                .then(function(result) {
+                    validateEvent(result);
+                    result.should.be.instanceof(Array);
+                    result.should.have.length(3);
+                    done();
+                })
+                .catch(done)
+        });
     });
-  });
 });
