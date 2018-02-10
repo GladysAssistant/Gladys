0132849b7d1ec0c19467cc591fcb737820e075e9
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-02-10 14:49:17 +0100

    New task.checkDbVersion to execute db tasks at each migration

--- api/core/task/index.js
+++ api/core/task/index.js
@@ -1,2 +1,5 @@
 
 module.exports.init = require('./task.init.js');
+module.exports.checkDbVersion = require('./task.checkDbVersion.js');
+module.exports.dbMigration = require('./task.dbMigration.js');
+module.exports.updateDbVersion = require('./task.updateDbVersion.js');
