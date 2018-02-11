014f44fe09824ac8410be9de225e9150235c76fc
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-02-11 19:18:35 +0100

    Translate boxType title in UI

--- api/controllers/BoxTypeController.js
+++ api/controllers/BoxTypeController.js
@@ -8,7 +8,13 @@ module.exports = {
    */
   index: function(req, res, next){
       gladys.boxType.getAll()
-        .then(function(types){
+        .then((types) => {
+            
+            // translate box title
+            types.forEach((type) => {
+              type.title = req.__(`box-${type.title}-title`);
+            });
+
             return res.json(types);
         })
         .catch(next);
