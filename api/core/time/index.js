bb2dec6b23980dced60cb669ee569db4e5275105
pjap93 <pjap9392@gmail.com>
2018-02-28 11:00:35 +0100

    Ajout de fonctions time
    Ajout fonctions pour les conditions des scénarios.

--- api/core/time/index.js
+++ api/core/time/index.js
@@ -1,3 +1,8 @@
 
 module.exports.get = require('./time.get.js');
 module.exports.command = require('./time.command.js');
+module.exports.getTime = require('./time.getTime.js');
+module.exports.isItAM = require('./time.isItAM.js');
+module.exports.isItPM = require('./time.isItPM.js');
+module.exports.isItNight = require('./time.isItNight.js');
+module.exports.isItEvening = require('./time.isItEvening.js');
