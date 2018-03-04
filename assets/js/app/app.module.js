7d8dbac6f0baa222b7c2cd89c7fab09b08e1ff5e
Alexandre Trovato <alexandre.trovato@gmail.com>
2018-03-04 10:58:04 +0100

    Limit app.module.js changes

--- assets/js/app/app.module.js
+++ assets/js/app/app.module.js
@@ -11,5 +11,5 @@
     'use strict';
 
     angular
-        .module('gladys', ['ng.deviceDetector', 'infinite-scroll', 'angularMoment', 'ui-notification', 'smoothScroll', 'chart.js', 'pascalprecht.translate', 'ui.bootstrap-slider', 'ng.deviceDetector', 'mwl.calendar', 'ui.color-picker']);
+        .module('gladys', ['ng.deviceDetector', 'infinite-scroll', 'angularMoment', 'ui-notification', 'smoothScroll', 'chart.js', 'pascalprecht.translate', 'ui.bootstrap-slider', 'ng.deviceDetector', 'mwl.calendar']);
 })();
