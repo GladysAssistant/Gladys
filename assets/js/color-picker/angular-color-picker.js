5f597e211e38bd10689eae468a3ba9333928673e
Alexandre Trovato <alexandre.trovato@gmail.com>
2018-03-10 20:04:29 +0100

    Upload color picker to v3.8.0

---
+++ assets/js/color-picker/angular-color-picker.js
@@ -0,0 +1,23 @@
+(function () {
+    angular.module('ui.color-picker', []).directive('colorPicker', function() {
+        return {
+            restrict: 'E',
+            replace: true,
+            template: '<div class="colorpicker-input" style="width:100%; height:1.5em"></div>',
+            require: 'ngModel',
+            scope: {
+                ngModel: '='
+            },
+            link: function ($scope, element, attrs, ngModelCtrl, $compile) {
+                var picker = new CP(element[0]);
+                element.css('background-color', '#' + ngModelCtrl.$modelValue.toString(16));
+
+                picker.on('stop', function (newValue) {
+                    var intValue = parseInt(newValue, 16);
+                    ngModelCtrl.$setViewValue(intValue);
+                    element.css('background-color', '#' + newValue);
+                });
+            }
+        };    
+    });
+})();
