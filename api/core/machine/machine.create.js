dfff497788a6997333b3f766f5e7b5aadb11f97f
LeptitGeek <mxflash1996@gmail.com>
2018-03-15 14:14:32 +0100

    Add comments for machine documentation

--- api/core/machine/machine.create.js
+++ api/core/machine/machine.create.js
@@ -1,5 +1,32 @@
 var uuid = require('uuid');
 
+/**
+ * @public
+ * @description This function create an machine
+ * @name gladys.machine.create
+ * @param {Object} machine
+ * @param {String} machine.name The name of the machine
+ * @param {House} machine.house The id of house of the machine
+ * @param {String} machine.host The host of the machine
+ * @param {boolean} machine.me If this machine is the created machine
+ * @returns {Machine} machine
+ * @example
+ * var machine = {
+ *      name: "Machine one",
+ *      house: 1,
+ *      host: "ip address",
+ *      me: true,
+ * }
+ * 
+ * gladys.machine.create(machine)
+ *      .then(function(machine){
+ *          // machine created
+ *      })
+ *      .catch(function(err){
+ *          // something bad happened ! :/
+ *      });
+ */
+
 module.exports = function create(machine){
    machine.uuid = machine.uuid || uuid.v4();
    return Machine.create(machine); 
