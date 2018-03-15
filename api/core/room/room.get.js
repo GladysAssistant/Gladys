d34b4a2a737b64a05b5dcd815de4f49bd342f1b1
LeptitGeek <mxflash1996@gmail.com>
2018-03-15 18:08:02 +0100

    Add comments for room documentation

--- api/core/room/room.get.js
+++ api/core/room/room.get.js
@@ -1,5 +1,24 @@
 var queries = require('./room.queries.js');
 
+/**
+ * @public
+ * @description This function return all rooms
+ * @name gladys.room.get
+ * @param {Object} options
+ * @param {integer} options.take The number of rooms to return (optional)
+ * @param {integer} options.skip The number of rooms to skip (optional)
+ * @returns {Array<rooms>} rooms
+ * @example
+ * var options = {
+ *      take: 50,
+ *      skip: 0
+ * }
+ * gladys.room.get(options)
+ *      .then(function(rooms){
+ *          // do something
+ *      })
+ */
+
 module.exports = function(options){
     
   options = options || {};
--- api/core/room/room.update.js
+++ api/core/room/room.update.js
@@ -1,5 +1,27 @@
 var Promise = require('bluebird');
 
+/**
+ * @public
+ * @description This function update an room
+ * @name gladys.room.update
+ * @param {Object} room
+ * @param {integer} room.id The id of the room
+ * @param {String} room.name The name of the room
+ * @param {House} room.house The id of the house of the room
+ * @returns {Room} room
+ * @example
+ * var room = {
+ *      id: 1,
+ *      name: "living room updated",
+ *      house: 1,
+ * }
+ * 
+ * gladys.room.update(room)
+ *      .then(function(room){
+ *          // room updated
+ *      })
+ */
+
 module.exports = function update(room){
     var id = room.id;
     delete room.id;
