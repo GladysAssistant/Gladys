1c02612d7a00e6373815392d8c0d0fd777af7e17
LeptitGeek <mxflash1996@gmail.com>
2018-03-15 13:10:11 +0100

    Add comments for eventdocumentation

--- api/core/event/event.get.js
+++ api/core/event/event.get.js
@@ -1,5 +1,24 @@
 var queries = require('./event.queries.js');
 
+/**
+ * @public
+ * @description This function return all events
+ * @name gladys.event.get
+ * @param {Object} options
+ * @param {integer} options.take The number of events to return (optional)
+ * @param {integer} options.skip The number of events to skip (optional)
+ * @returns {Array<events>} events
+ * @example
+ * var options = {
+ *      take: 50,
+ *      skip: 0
+ * }
+ * gladys.event.get(options)
+ *      .then(function(devices){
+ *          // do something
+ *      })
+ */
+
 module.exports = function(options){
     options = options || {};
     options.skip = parseInt(options.skip) || 0;
