dea15185502e9fc25da9974bf1bd188dc1f918ca
LeptitGeek <mxflash1996@gmail.com>
2018-03-15 19:19:11 +0100

    Add comments for deviceType documentation

--- api/core/devicetype/deviceType.delete.js
+++ api/core/devicetype/deviceType.delete.js
@@ -1,5 +1,26 @@
 var queries = require('./deviceType.queries.js');
 
+/**
+ * @public
+ * @description This function delete an deviceType
+ * @name gladys.deviceType.delete
+ * @param {Object} devicType
+ * @param {integer} devicType.id The id of the deviceType
+ * @returns {DeviceType} deviceType
+ * @example
+ * var devicType = {
+ *      id: 1
+ * };
+ * 
+ * gladys.deviceType.delete(devicType)
+ *      .then(function(devicType){
+ *         // deviceType deleted ! 
+ *      })
+ *      .catch(function(err){
+ *          // something bad happened ! :/
+ *      });
+ */
+
 module.exports = function (deviceType){
   
   // delete all deviceState from this deviceType
