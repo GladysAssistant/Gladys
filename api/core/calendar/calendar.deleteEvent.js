b26a01b4e09352102d2ac5489acb52567dad95df
LeptitGeek <mxflash1996@gmail.com>
2018-03-15 08:32:46 +0100

    add comments for documentation of calendar

--- api/core/calendar/calendar.deleteEvent.js
+++ api/core/calendar/calendar.deleteEvent.js
@@ -1,5 +1,26 @@
 var queries = require('./calendar.queries.js');
 
+/**
+ * @public
+ * @description This function delete an event
+ * @name gladys.calendar.deleteEvent
+ * @param {Object} event
+ * @param {integer} event.id The id of the event
+ * @returns {event} event
+ * @example
+ * var event = {
+ *      id: 1
+ * };
+ *
+ * gladys.calendar.deleteEvent()
+ *      .then(function(event){
+ *         //event deleted ! 
+ *      })
+ *      .catch(function(err){
+ *          // something bad happened ! :/
+ *      });
+ */
+
 module.exports = function(calendarEvent) {
     return gladys.utils.sql(queries.deleteEvent, [calendarEvent.id]);
 };
