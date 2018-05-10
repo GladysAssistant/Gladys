eddbab16de78259f2eb5cab3d51455b8059f076c
pjap93 <pjap9392@gmail.com>
2018-05-10 22:49:36 +0200

    Update time.isItRightTime.test.js

--- test/unit/api/core/time/time.isItRightTime.test.js
+++ test/unit/api/core/time/time.isItRightTime.test.js
@@ -3,7 +3,11 @@ var should = require('should');
 describe('Time', function() {
   describe('isItRightTime', function() {
     it('should return true or false', function (done) {
-        gladys.time.isItRightTime({start: '10:00:00', end: '15:00:00'})
+      var options = {
+        start: '10:00:00',
+        end: '15:00:00'
+      };
+        gladys.time.isItRightTime(options)
             .then((result) => {
                 result.should.be.instanceOf(Boolean);
                 done();
