25f59284ad6fdd21bdd7c68da248c26a81556436
pjap93 <pjap9392@gmail.com>
2018-05-13 19:41:36 +0200

    Update script.get.js
    limitation of the display only on the 50 removed

--- api/core/script/script.get.js
+++ api/core/script/script.get.js
@@ -4,8 +4,6 @@ module.exports = function(options) {
 
     // default params
     options = options || {};
-    options.skip = parseInt(options.skip) || 0;
-    options.take = parseInt(options.take) || 50;
 
-    return gladys.utils.sql(queries.getScripts, [options.user.id, options.take, options.skip]);
+    return gladys.utils.sql(queries.getScripts, [options.user.id]);
 };
