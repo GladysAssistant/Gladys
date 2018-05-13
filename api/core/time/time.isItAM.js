ca4547bf1656caf5ed141c78d95e1394eabda166
pjap93 <pjap9392@gmail.com>
2018-05-14 00:19:20 +0200

    Update time.isItAM.js

--- api/core/time/time.isItAM.js
+++ api/core/time/time.isItAM.js
@@ -1,3 +1,26 @@
+ **
+ * @public
+ * @description This function return true or false is it the a.m.
+ * @name gladys.time.isItAM
+ * @param {Object} house
+ * @param {integer} house.house The id of the house
+ * @returns {House} house
+ * 
+ * @example
+ * var house = {
+ *	house: '1'
+ * };
+ * 
+ * gladys.time.isItAM(house)
+ *      .then(function(isItAM){
+ *          if(isItAM){
+ *              //The time in the selected house is in the night.
+ *          }else{
+ *              //The time in the selected house is not in the night.
+ *          }
+ *      });
+ */
+
 module.exports = function isItAM(options){
     return gladys.time.getMomentOfTheDay(options)
       .then((result) => {
