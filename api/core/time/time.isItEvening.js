305e3fab1bbbaf552d98c5aacd89085d089aa229
pjap93 <pjap9392@gmail.com>
2018-05-14 17:51:46 +0200

    Update time.isItEvening.js

--- api/core/time/time.isItEvening.js
+++ api/core/time/time.isItEvening.js
@@ -1,3 +1,27 @@
+
+/**
+ * @public
+ * @description This function return true or false, test is evening.
+ * @name gladys.time.isItEvening
+ * @param {Object} house
+ * @param {integer} house.house The id of the house
+ * @returns {House} house
+ * 
+ * @example
+ * var house = {
+ *	house: '1'
+ * };
+ * 
+ * gladys.time.isItEvening(house)
+ *      .then(function(isItEvening){
+ *          if(isItEvening){
+ *              //The time in the selected house is evening.
+ *          }else{
+ *              //The time in the selected house is not evening.
+ *          }
+ *      });
+ */
+
 module.exports = function isItEvening(options) {
     return gladys.time.getMomentOfTheDay(options)
       .then((result) => {
