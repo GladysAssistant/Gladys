7ce774f7db15510f9ff898b29dfaefe7e935bdee
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-06-02 11:02:23 +0200

    If notification needs to ne sent on another machine, forward it

--- api/core/notification/notification.create.js
+++ api/core/notification/notification.create.js
@@ -69,7 +69,11 @@ function create(options) {
 function startService(notification, type, user) {
 
     var notify = null;
-    if(gladys[type.service] && typeof gladys[type.service].notify === "function"){
+
+    if(type.machine && type.machine.length) {
+        gladys.emit('notification-notify', {notification, user, machine_id: type.machine});
+        return Promise.reject(new Error('ok'));
+    } else if(gladys[type.service] && typeof gladys[type.service].notify === "function"){
         notify = gladys[type.service].notify;
     } else if (gladys.modules[type.service] || typeof gladys.modules[type.service].notify === "function") {
         notify = gladys.modules[type.service].notify;
