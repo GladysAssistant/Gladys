2f650c7069d8ea1991b03c18d708b1dadfe94273
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-06-03 11:34:02 +0200

    add machineName to get module

--- api/core/module/module.queries.js
+++ api/core/module/module.queries.js
@@ -1,6 +1,9 @@
 
 module.exports = {
-  get: 'SELECT * FROM module',
+  get: `SELECT module.*, machine.name as machineName
+        FROM module
+        LEFT JOIN machine ON machine.uuid = module.machine;
+  `,
   getFreshInstalledModule: 'SELECT * FROM module WHERE status = 1;' ,
   updateStatusToInstalled: 'UPDATE module SET status = 0 WHERE status = 1;',
   getById: 'SELECT * FROM module WHERE id = ?;',
