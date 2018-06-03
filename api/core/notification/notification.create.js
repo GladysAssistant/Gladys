f931082d9ea1b9e74745dad3fa30e7619295ce5a
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-06-03 16:40:10 +0200

    add more logs in notification.create to know when notification is sent remotely

--- api/core/notification/notification.create.js
+++ api/core/notification/notification.create.js
@@ -71,6 +71,7 @@ function startService(notification, type, user) {
     var notify = null;
 
     if(type.machine && type.machine.length) {
+        sails.log.debug(`Notification module is not located on this machine, sending message on machine ${type.machine}`);
         gladys.emit('notification-notify', {notification, user, machine_id: type.machine, module_slug: type.service});
         return Promise.reject(new Error('ok'));
     } else if(gladys[type.service] && typeof gladys[type.service].notify === "function"){
