dbcfefeab5825b0c71be20dada8cef8c756a13c9
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-06-09 15:56:29 +0200

    return deviceType name instead of device name

--- api/core/devicetype/deviceType.queries.js
+++ api/core/devicetype/deviceType.queries.js
@@ -37,7 +37,7 @@ module.exports = {
     AND dt.identifier = ?;
   `,
   getAll: `
-    SELECT CONCAT(d.name, " - ", dt.type) AS name, d.service, d.protocol, dt.id, dt.type, dt.tag,  dt.unit, dt.min, dt.max, dt.device, r.name AS roomName, r.id as roomId 
+    SELECT dt.name, d.service, d.protocol, dt.id, dt.type, dt.tag,  dt.unit, dt.min, dt.max, dt.device, r.name AS roomName, r.id as roomId, d.name as deviceName
     FROM device d
     JOIN devicetype dt ON (d.id = dt.device)
     JOIN room r ON (d.room = r.id);
