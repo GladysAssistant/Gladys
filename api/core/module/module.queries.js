e8b494779ff78563ec9b596505061d9d8f59ca20
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-06-09 13:38:57 +0200

    gladys.module.getById()

--- api/core/module/module.queries.js
+++ api/core/module/module.queries.js
@@ -4,6 +4,12 @@ module.exports = {
         FROM module
         LEFT JOIN machine ON machine.uuid = module.machine;
   `,
+  getById: `
+    SELECT module.*, machine.name as machineName
+    FROM module
+    LEFT JOIN machine ON machine.uuid = module.machine
+    WHERE module.id = ?;
+  `,
   getFreshInstalledModule: 'SELECT * FROM module WHERE status = 1;' ,
   updateStatusToInstalled: 'UPDATE module SET status = 0 WHERE status = 1;',
   getById: 'SELECT * FROM module WHERE id = ?;',
