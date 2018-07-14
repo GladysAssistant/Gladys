34ef4250df06dd02894c7f5edd9fb8c09d90d30c
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-07-14 10:18:47 +0200

    Remove old license header on each file

--- api/controllers/StateTypeController.js
+++ api/controllers/StateTypeController.js
@@ -1,18 +1,3 @@
-/** 
-  * Gladys Project
-  * http://gladysproject.com
-  * Software under licence Creative Commons 3.0 France 
-  * http://creativecommons.org/licenses/by-nc-sa/3.0/fr/
-  * You may not use this software for commercial purposes.
-  * @author :: Pierre-Gilles Leymarie
-  */
-  
-/**
- * StateTypeController
- *
- * @description :: Server-side logic for managing Statetypes
- * @help        :: See http://links.sailsjs.org/docs/controllers
- */
 
 module.exports = {
     
