fcb59ea9b2562bfc77fd06e210a26239552dc353
pjap93 <pjap9392@gmail.com>
2018-07-21 21:01:28 +0200

    Sort sentences by most recent first in brain view

--- api/core/sentence/sentence.queries.js
+++ api/core/sentence/sentence.queries.js
@@ -1,13 +1,13 @@
 
 module.exports = {
-  getAll: 'SELECT * FROM sentence WHERE (status = "official" OR status = "approved");',
+  getAll: 'SELECT * FROM sentence WHERE (status = "official" OR status = "approved") ORDER BY id DESC;',
   getByUuid: 'SELECT * FROM sentence WHERE uuid = ?;',
   getByText: 'SELECT * FROM sentence WHERE lower(text) = lower(?);',
   get: 'SELECT * FROM sentence LIMIT ? OFFSET ?;',
-  getOffical: 'SELECT * FROM sentence WHERE status="official" LIMIT ? OFFSET ?;',
-  getApproved: 'SELECT * FROM sentence WHERE status="approved" LIMIT ? OFFSET ?;',
-  getRejected: 'SELECT * FROM sentence WHERE status="rejected" LIMIT ? OFFSET ?;',
-  getPending: 'SELECT * FROM sentence WHERE status="pending" LIMIT ? OFFSET ?;',
+  getOffical: 'SELECT * FROM sentence WHERE status="official" ORDER BY id DESC LIMIT ? OFFSET ?;',
+  getApproved: 'SELECT * FROM sentence WHERE status="approved" ORDER BY id DESC LIMIT ? OFFSET ?;',
+  getRejected: 'SELECT * FROM sentence WHERE status="rejected" ORDER BY id DESC LIMIT ? OFFSET ?;',
+  getPending: 'SELECT * FROM sentence WHERE status="pending" ORDER BY id DESC LIMIT ? OFFSET ?;',
   getLabels: 'SELECT DISTINCT label, service from sentence;',
   cleanSentencesTable: 'DELETE FROM sentence;'
 };
