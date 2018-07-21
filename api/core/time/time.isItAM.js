c54cd88fb722255da8afcf728b8ec306c9c46d32
pjap93 <pjap9392@gmail.com>
2018-07-21 21:07:38 +0200

    Added JS documentation on gladys time functions (#354)

--- api/core/time/time.isItAM.js
+++ api/core/time/time.isItAM.js
@@ -1,3 +1,26 @@
+/**
+ * @public
+ * @description This function return true or false, test if it's morning.
+ * @name gladys.time.isItAM
+ * @param {Object} house
+ * @param {integer} house.house The id of the house
+ * @returns {House} house
+ * 
+ * @example
+ * var house = {
+ *	house: '1'
+ * };
+ * 
+ * gladys.time.isItAM(house)
+ *      .then(function(isItAM){
+ *          if(isItAM){
+ *              //The time in the selected house is morning.
+ *          }else{
+ *              //The time in the selected house is not morning.
+ *          }
+ *      });
+ */
+
 module.exports = function isItAM(options){
     return gladys.time.getMomentOfTheDay(options)
       .then((result) => {
--- api/core/time/time.isItNight.js
+++ api/core/time/time.isItNight.js
@@ -1,3 +1,27 @@
+/**
+ * @public
+ * @description This function return true or false, test if it's night.
+ * @name gladys.time.isItNight
+ * @param {Object} house
+ * @param {integer} house.house The id of the house
+ * @returns {House} house
+ * 
+ * @example
+ * var house = {
+ *	house: '1'
+ * };
+ * 
+ * gladys.time.isItNight(house)
+ *      .then(function(isItNight){
+ *          if(isItNight){
+ *              //The time in the selected house is night.
+ *          }else{
+ *              //The time in the selected house is not night.
+ *          }
+ *      });
+ */
+
+
 module.exports = function isItNight(options){
     return gladys.time.getMomentOfTheDay(options)
       .then((result) => {
