6efe89961018431a6bc10464ce0950ad7f28c98d
Mathieu Andrade <26581253+MathieuAndrade@users.noreply.github.com>
2018-07-22 10:56:44 +0200

    Correct typos in doc of the function event.get

--- api/core/event/event.get.js
+++ api/core/event/event.get.js
@@ -7,14 +7,18 @@ var queries = require('./event.queries.js');
  * @param {Object} options
  * @param {integer} options.take The number of events to return (optional)
  * @param {integer} options.skip The number of events to skip (optional)
+ * @param {Object} options.user
+ * @param {Object} user
+ * @param {integer} user.id Th id of user 
  * @returns {Array<events>} events
  * @example
  * var options = {
+ *      user = {id: 1},
  *      take: 50,
  *      skip: 0
  * }
  * gladys.event.get(options)
- *      .then(function(devices){
+ *      .then(function(events){
  *          // do something
  *      })
  */
