140806ea199ccc87aac61cf59a6505f9e7f84019
Mathieu Andrade <26581253+MathieuAndrade@users.noreply.github.com>
2018-07-22 14:49:20 +0200

    Modify Events functions (#380)
    * Modify function event.purge for clear all table and not just one eventtype
    
    * Add function event.getByEventtype and test
    
    * Correct typos in doc of the function event.get
    
    * Add doc for new function event.getByEventtype
    
    * Rename function "getByEventtype" on "getByEventType"
    
    * Fix

--- api/core/event/index.js
+++ api/core/event/index.js
@@ -2,5 +2,6 @@
 module.exports.create = require('./event.create.js');
 module.exports.command = require('./event.command.js');
 module.exports.get = require('./event.get.js');
+module.exports.getByEventType = require('./event.getByEventtype.js');
 module.exports.update = require('./event.update.js');
 module.exports.purge = require('./event.purge.js');
