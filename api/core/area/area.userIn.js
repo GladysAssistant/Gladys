e8e49877fe75108ac6217cbd3f488b19001507a6
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-09-10 17:32:23 +0800

    Fix #412: Completely rewrote the area detection algorithm

---
+++ api/core/area/area.userIn.js
@@ -0,0 +1,56 @@
+const Promise = require('bluebird');
+const queries = require('./area.queries.js');
+
+/**
+ * @public
+ * @description This function return all areas the user is currently in
+ * @name gladys.area.userIn
+ * @param {Object} user
+ * @param {String} user.id The id of the user
+ * @returns [{area}] areas
+ * @example
+ * var user = {
+ *    id: 1
+ * };
+ *
+ * gladys.area.userIn(user)
+ *      .then((areas) => {
+ *         // areas is an array of areas where the user is in currently
+ *      })
+ *      .catch((err) => {
+ *          // something bad happened ! :/
+ *      });
+ */
+
+module.exports = function userIn(user) {
+
+    // Getting all areas + events linked to areas
+    // The goal here is to rebuild the history of entering/leaving areas
+    return Promise.all([
+        gladys.utils.sql(queries.get, [user.id]), 
+        gladys.utils.sql(queries.getLastAreaEventPerArea, [user.id])
+    ])
+    .spread((areas, events) => {
+
+        var areasWhereTheUserIsStillIn = [];
+
+        areas.forEach((area) => {
+            var found = false;
+            var i = 0;
+
+            // we see if the last event seen about this area was an "enter-area" event, 
+            // "left-area", or no event at all
+            while(!found && i < events.length) {
+                if(parseInt(events[i].value) === area.id) {
+                    found = true;
+                    if(events[i].code === 'enter-area') {
+                        areasWhereTheUserIsStillIn.push(area);
+                    }
+                }
+                i++;
+            }
+        });
+
+        return areasWhereTheUserIsStillIn;
+    });
+};
--- api/core/area/index.js
+++ api/core/area/index.js
@@ -4,4 +4,5 @@ module.exports.changeArea = require('./area.changeArea.js');
 module.exports.create = require('./area.create.js');
 module.exports.get = require('./area.get.js');
 module.exports.update = require('./area.update.js');
+module.exports.userIn = require('./area.userIn.js');
 module.exports.delete = require('./area.delete.js');
