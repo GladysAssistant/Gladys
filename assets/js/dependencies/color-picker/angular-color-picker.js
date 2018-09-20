3eb09820bbf90a79aeb51aaea11402e79b5cd004
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-09-20 16:56:48 +0800

    Fix #300: Add new colorpicker for deviceTypes (#404)

---
+++ assets/js/dependencies/color-picker/angular-color-picker.js
@@ -0,0 +1,26 @@
+(function () {
+    angular.module('ui.color-picker', []).directive('colorPicker', function() {
+        return {
+            restrict: 'E',
+            replace: true,
+            template: '<div class="colorpicker-input" style="width:100%; height:20px; border: #adadad; border-width: 1px; border-style: solid; border-radius: 20px;"></div>',
+            require: 'ngModel',
+            scope: {
+                ngModel: '='
+            },
+            link: function ($scope, element, attrs, ngModelCtrl, $compile) {
+                var picker = new CP(element[0]);
+		var modelValue = $scope.ngModel;
+		if (modelValue) {
+                    element.css('background-color', '#' + (modelValue).toString(16));
+		}
+
+                picker.on('stop', function (newValue) {
+                    var intValue = parseInt(newValue, 16);
+                    ngModelCtrl.$setViewValue(intValue);
+                    element.css('background-color', '#' + newValue);
+                });
+            }
+        };
+    });
+})();
