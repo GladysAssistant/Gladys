996fe13c49dc0bcd92b0fe6b1ea9d2f9ec227281
piznel <piznel@hotmail.com>
2018-09-22 15:30:08 +0200

    Change of name of Angular directive

--- assets/js/dependencies/googleColorPicker.js
+++ assets/js/dependencies/googleColorPicker.js
@@ -1,4 +1,4 @@
-angular.module('tb-color-picker', [])
+angular.module('ui.google-color-picker', [])
     .run(['$templateCache', function ($templateCache) {
         $templateCache.put('color-picker.tp1.html', '<div class="google-color-picker">' +
             '<div class="google-selected-color" ng-style="{\'background-color\': color}"></div>' +
