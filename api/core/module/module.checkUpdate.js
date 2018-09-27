bbb2d56765aab464ded822023d7084592915498b
Vincent Besseau <vbesseau44@gmail.com>
2018-09-27 16:41:03 +0200

    Adding checking module update on gladys

---
+++ api/core/module/module.checkUpdate.js
@@ -0,0 +1,42 @@
+
+module.exports = function(){
+    return gladys.utils.request({
+        method: 'GET',
+        url: 'https://developer.gladysproject.com/api/v1/modules'
+    })
+    .then(function(modulesOnStore){
+        gladys.module.get()
+        .then((modules)=>{
+            modules.forEach(function(module) {
+                modulesOnStore.forEach(moduleOnStore => {
+                    if(moduleOnStore.name === module.name){
+                        if(moduleOnStore.version > module.version) {
+                            // we get all admins
+                            return gladys.user.getAdmin()
+                            .then(function(admins){
+                                // foreach admin
+                                admins.forEach(function(admin){
+                                    
+                                    // we create a new notification
+                                    var notification = {
+                                        title: sails.__({ phrase: 'notification-update-available-title', locale: admin.language.substr(0,2)}) + ` ${moduleOnStore.version}`,
+                                        text: ` ${moduleOnStore.name} ` + sails.__({ phrase: 'notification-update-module-available-text', locale: admin.language.substr(0,2)}) + ` ${moduleOnStore.version}`,
+                                        icon: sails.config.update.icon,
+                                        iconColor: sails.config.update.iconColor,
+                                        link: sails.config.update.link,
+                                        priority: 0,
+                                        user: admin.id,
+                                    };
+                                    
+                                    // and send it only if we hadn't done it before
+                                    return gladys.notification.createIfNotExist(notification);
+                                });
+                            });
+                        }
+                    }
+                });
+            })
+        })
+    })
+    
+};
