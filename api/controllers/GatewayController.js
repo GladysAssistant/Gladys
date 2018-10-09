8d9d3962a467255c7a8ded6d4c51478abbf3f5c8
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-10-09 16:10:54 +0800

    gladys gateway : GET /gateway/status

--- api/controllers/GatewayController.js
+++ api/controllers/GatewayController.js
@@ -3,6 +3,25 @@ module.exports = {
   login: function(req, res, next) {
     gladys.gateway.login(req.body.email, req.body.password, req.body.two_factor_code)
       .then(() => res.json())
-      .catch(next);
+      .catch((err) => {
+        if(err && err.response && err.response.status) {
+          res.status(err.response.status).json(err.response.data);
+        } else if( err instanceof Error && err.message === '2FA_NOT_ENABLED') {
+          res.status(403).json({
+            status: 403,
+            code: '2FA_NOT_ENABLED',
+            message: '2FA should be enabled to login on Gladys'
+          });
+        } else {
+          sails.log.error(err);
+          next(err);
+        }
+      });
+  },
+
+  isConnected: function(req, res, next) {
+    res.json({
+      connected: gladys.gateway.isConnected()
+    });
   }
 };
--- api/core/gateway/index.js
+++ api/core/gateway/index.js
@@ -1,2 +1,3 @@
 module.exports.login = require('./gateway.login.js');
 module.exports.init = require('./gateway.init.js');
+module.exports.isConnected = require('./gateway.isConnected.js');
