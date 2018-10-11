bac37c6adebf2720849f0d13ca1f24855ac3108c
Vincent Besseau <vbesseau44@gmail.com>
2018-10-11 21:46:42 +0200

    Checking version by semver dependency

--- api/core/module/module.checkUpdate.js
+++ api/core/module/module.checkUpdate.js
@@ -1,42 +1,42 @@
 
+const semver = require('semver')
+var Promise = require('bluebird');
+
 module.exports = function(){
-    return gladys.utils.request({
-        method: 'GET',
-        url: 'https://developer.gladysproject.com/api/v1/modules'
-    })
-    .then(function(modulesOnStore){
-        gladys.module.get()
-        .then((modules)=>{
-            modules.forEach(function(module) {
-                modulesOnStore.forEach(moduleOnStore => {
-                    if(moduleOnStore.name === module.name){
-                        if(moduleOnStore.version > module.version) {
-                            // we get all admins
-                            return gladys.user.getAdmin()
-                            .then(function(admins){
-                                // foreach admin
-                                admins.forEach(function(admin){
-                                    
-                                    // we create a new notification
-                                    var notification = {
-                                        title: sails.__({ phrase: 'notification-update-available-title', locale: admin.language.substr(0,2)}) + ` ${moduleOnStore.version}`,
-                                        text: `${moduleOnStore.name} ` + sails.__({ phrase: 'notification-update-module-available-text', locale: admin.language.substr(0,2)}) + ` ${moduleOnStore.version}`,
-                                        icon: 'fa fa-cloud-download',
-                                        iconColor: '',
-                                        link: '/dashboard/module',
-                                        priority: 0,
-                                        user: admin.id,
-                                    };
-                                    
-                                    // and send it only if we hadn't done it before
-                                    return gladys.notification.createIfNotExist(notification);
-                                });
-                            });
-                        }
-                    }
-                });
+    return gladys.module.get()
+    .then(function(modulesInstalled) {
+        Promise.map(modulesInstalled, function(moduleInstalled) {
+            return gladys.utils.request({
+                method: 'GET',
+                url: 'https://developer.gladysproject.com/api/v1/modules/'+moduleInstalled.slug
             })
-        })
-    })
-    
+            .then(function(moduleOnStore) {
+                if(semver.valid(moduleOnStore.version) && semver.valid(moduleInstalled.version)){
+                    if(semver.gt(moduleOnStore.version, moduleInstalled.version) === true) {
+                        // we get all admins
+                        return gladys.user.getAdmin()
+                        .then(function(admins){
+                            // foreach admin
+                            Promise.map(admins, function(admin){
+                                
+                                // we create a new notification
+                                var notification = {
+                                    title: sails.__({ phrase: 'notification-update-available-title', locale: admin.language.substr(0,2)}) + ` ${moduleOnStore.version}`,
+                                    text: `${moduleInstalled.name} ` + sails.__({ phrase: 'notification-update-module-available-text', locale: admin.language.substr(0,2)}) + ` ${moduleOnStore.version}`,
+                                    icon: 'fa fa-cloud-download',
+                                    iconColor: 'blue',
+                                    link: '/dashboard/module',
+                                    priority: 0,
+                                    user: admin.id,
+                                };
+                                
+                                // and send it only if we hadn't done it before
+                                return gladys.notification.createIfNotExist(notification);
+                            });
+                        });
+                    }   
+                }
+            })
+        },{concurrency:1})
+    })    
 };
