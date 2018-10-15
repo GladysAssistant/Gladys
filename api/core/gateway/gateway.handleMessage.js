cf919020ca97c6d5206c35b2e5fdc70b285a72ab
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-10-15 13:59:56 +0800

    fix linting

--- api/core/gateway/gateway.handleMessage.js
+++ api/core/gateway/gateway.handleMessage.js
@@ -1,35 +1,34 @@
 const callApi = require('./utils').callApi;
 const importControllers = require('./utils').importControllers;
-const DeviceTypeController = require('../../controllers/DeviceTypeController.js');
 
 var controllers = importControllers();
-var idRouteRegex =  /\/\d+\//g;
+var idRouteRegex = /\/\d+\//g;
 
 module.exports = function(data, cb) {
-  switch(data.type) {
-    case 'gladys-api-call':
-      var route = (data.options.method + ' ' + data.options.url).toLowerCase();
-      
-      var id = route.match(idRouteRegex);
-      route = route.replace(idRouteRegex, '/:id/');
+  switch (data.type) {
+  case 'gladys-api-call':
+    var route = (data.options.method + ' ' + data.options.url).toLowerCase();
 
-      var params = {};
-      
-      if(id && id.length > 0) {
-        params.id = id[0].replace(/\//g, '');
-      }
+    var id = route.match(idRouteRegex);
+    route = route.replace(idRouteRegex, '/:id/');
 
-      var controller = controllers[route];
-      if(!controller) {
-        return cb({error_code: 'ROUTE_NOT_FOUND', error_message: `Route: ${route} not found`});
-      }
+    var params = {};
 
-      callApi(data.sender_id, controller, params, data.options.query, data.options.data, cb);
+    if (id && id.length > 0) {
+      params.id = id[0].replace(/\//g, '');
+    }
+
+    var controller = controllers[route];
+    if (!controller) {
+      return cb({ error_code: 'ROUTE_NOT_FOUND', error_message: `Route: ${route} not found` });
+    }
+
+    callApi(data.sender_id, controller, params, data.options.query, data.options.data, cb);
     break;
 
-    default: 
-      sails.log.warn(`Gladys Gateway: Unknown message type ${data.type}`);
-      cb(new Error('INVALID_MESSAGE_TYPE'));
+  default:
+    sails.log.warn(`Gladys Gateway: Unknown message type ${data.type}`);
+    cb(new Error('INVALID_MESSAGE_TYPE'));
     break;
-  } 
+  }
 };
