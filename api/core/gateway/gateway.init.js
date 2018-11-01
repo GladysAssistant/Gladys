06d5fb732a69acfc014a16600be93b3feab829b1
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-11-01 17:28:51 +0800

    Gladys Gateway is now more resilient to network failure.

--- api/core/gateway/gateway.init.js
+++ api/core/gateway/gateway.init.js
@@ -3,12 +3,20 @@ const handleNewMessage = require('./gateway.handleMessage');
 const shared = require('./shared.js');
 
 module.exports = function init() {
+
+  sails.log.info(`Gladys Gateway: Init Procedure : Getting keys from database`);
+
   return gladys.param.getValues(['GLADYS_GATEWAY_REFRESH_TOKEN', 'GLADYS_GATEWAY_RSA_PRIVATE_KEY', 'GLADYS_GATEWAY_ECDSA_PRIVATE_KEY'])
     .spread((refreshToken, rsaPrivateKey, ecdsPrivateKey) => {
+
+      sails.log.info(`Gladys Gateway: Init Procedure : Keys retrieved with success! Connecting in websockets...`);
+
       return gladysGatewayClient.instanceConnect(refreshToken, JSON.parse(rsaPrivateKey), JSON.parse(ecdsPrivateKey), handleNewMessage);
     })
     .then(() => {
-      sails.log.info(`Gladys Gateway: Connected with success!`);
+      
+      sails.log.info(`Gladys Gateway: Init Procedure : Connected with success!`);
+      
       shared.isConnected = true;
 
       // add listener
@@ -16,6 +24,7 @@ module.exports = function init() {
     })
     .catch((err) => {
       sails.log.info(`Gladys Gateway: Not connected.`);
+      
       shared.isConnected = false;
     });
 };
