a591f505aa8418470240b1ed5ce7781faac72d9f
piznel <piznel@hotmail.com>
2018-11-11 01:24:06 +0100

    init commit

--- assets/js/app/module/module.service.js
+++ assets/js/app/module/module.service.js
@@ -1,43 +1,48 @@
 
 (function () {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('gladys')
-        .factory('moduleService', moduleService);
+  angular
+    .module('gladys')
+    .factory('moduleService', moduleService);
 
-    moduleService.$inject = ['$http'];
+  moduleService.$inject = ['$http'];
 
-    function moduleService($http) {
+  function moduleService($http) {
         
-        var service = {
-            get: get,
-            install: install,
-            config: config,
-            uninstall: uninstall,
-            upgrade: upgrade
-        };
+    var service = {
+      get: get,
+      install: install,
+      config: config,
+      uninstall: uninstall,
+      upgrade: upgrade,
+      getMethods: getMethods
+    };
 
-        return service;
+    return service;
 
-        function get() {
-            return $http({method: 'GET', url: '/module'});
-        }
-        
-        function install(module){
-            return $http({method: 'POST', url: '/module/install', data: module});
-        }
-        
-        function config(slug){
-            return $http({method: 'POST', url: '/module/' + slug + '/config'});
-        }
-        
-        function uninstall(id){
-            return $http({method: 'DELETE', url: '/module/' + id });
-        }
-
-        function upgrade(id, version){
-            return $http({method: 'POST', url: '/module/' + id + '/upgrade', data: {version: version}});
-        }
+    function get() {
+      return $http({method: 'GET', url: '/module'});
     }
+        
+    function install(module){
+      return $http({method: 'POST', url: '/module/install', data: module});
+    }
+        
+    function config(slug){
+      return $http({method: 'POST', url: '/module/' + slug + '/config'});
+    }
+        
+    function uninstall(id){
+      return $http({method: 'DELETE', url: '/module/' + id });
+    }
+
+    function upgrade(id, version){
+      return $http({method: 'POST', url: '/module/' + id + '/upgrade', data: {version: version}});
+    }
+
+    function getMethods(params) {
+      return $http({method: 'POST', url: '/module/getMethods', data: params});
+    }
+  }
 })();
