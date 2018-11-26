6cb3faf52e400751c395fc9170dcdca67b6ec6f9
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-11-26 10:29:47 +0800

    Closes #410 : Add function gladys.deviceType.getByTag(options);

--- api/core/devicetype/deviceType.queries.js
+++ api/core/devicetype/deviceType.queries.js
@@ -52,14 +52,14 @@ module.exports = {
     JOIN devicetype dt ON (d.id = dt.device)
     LEFT JOIN room ON d.room = room.id 
     WHERE dt.id = ?;
-  `, 
+  `,
   getByType: `
     SELECT dt.*, dt.lastValueDatetime as lastChanged, room.name as roomName
     FROM device d
     JOIN devicetype dt ON (d.id = dt.device)
     LEFT JOIN room ON d.room = room.id 
     WHERE dt.type = ?;
-  `, 
+  `,
 
   getDeviceTypeByCategory:
   `
@@ -69,7 +69,13 @@ module.exports = {
     WHERE category = ?
     AND (room = ? OR ? IS NULL)
     AND (type = ? OR ? IS NULL);
-  `
+  `,
 
-  
+  getDeviceTypeByTags:
+    `
+    SELECT devicetype.* 
+    FROM devicetype 
+    JOIN device ON devicetype.device = device.id 
+    WHERE devicetype.tag IN ( ? );
+  `
 };
