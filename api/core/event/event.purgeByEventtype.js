e3bdef8e59aa1f1796b8e8da13271f102b5d19af
Jean-PhilippeD <jp.danree@free.fr>
2018-11-26 04:09:32 +0100

    Add gladys.event.purgeByEventType function (#442)
    * add event purge by type
    
    * eslint

---
+++ api/core/event/event.purgeByEventtype.js
@@ -0,0 +1,28 @@
+var queries = require('./event.queries.js');
+
+/**
+ * @public
+ * @description This function purge all events on a specific eventtype code and older than days 
+ * @name gladys.event.purgeByEventType
+ * @param {Object} options
+ * @param {integer} options.eventtype The code of eventtype we want to purge
+ * @param {integer} options.days The days we want to keep for this kind of event (optionnal)
+ * @returns {event} event
+ * @example
+ * var options = {
+ *      days: 2,
+ *      eventtype: 'devicetype-new-value'
+ * }
+ * gladys.event.purgeByEventType(options)
+ *      .then(function(events){
+ *          // do something
+ *      })
+ */
+
+
+module.exports = function purge(options) {
+
+  options.days = parseInt(options.days) || 10;
+  return gladys.utils.sql(queries.purgeByEventType, [options.days, options.eventtype]);
+};
+
--- test/unit/api/core/event/event.purgeByEventtype.test.js
+++ test/unit/api/core/event/event.purgeByEventtype.test.js
@@ -2,8 +2,8 @@
 var validateEvent = require('../../validator/eventValidator.js');
 
 describe('Event', function() {
-  describe('purge', function() {
-    it('should return 1 event', function(done) {
+  describe('purgeByEventType', function() {
+    it('should return 6 events', function(done) {
       var event = {
         params: {
           eventtype: 3,
@@ -17,10 +17,11 @@ describe('Event', function() {
         .create(event)
         .then(function(result) {
           var options = {
-            days: 1
+            days: 1,
+            eventtype: 'devicetype-new-value'
           };
 
-          return gladys.event.purge(options);
+          return gladys.event.purgeByEventType(options);
         })
         .then(function() {
           return gladys.event.get({
@@ -32,7 +33,7 @@ describe('Event', function() {
         .then(function(result) {
           validateEvent(result);
           result.should.be.instanceof(Array);
-          result.should.have.length(1);
+          result.should.have.length(6);
           done();
         })
         .catch(done);
