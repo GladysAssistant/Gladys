447cd26bf2148469d48384564c2e47b1e7c497a3
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2019-01-07 14:56:56 +0100

    Switch from @gladysproject to @gladysassistant npm organization

--- api/core/gateway/shared.js
+++ api/core/gateway/shared.js
@@ -1,6 +1,6 @@
 const WebCrypto = require('node-webcrypto-ossl');
 const cryptoLib = new WebCrypto();
-const gladysGatewayJs = require('@gladysproject/gladys-gateway-js');
+const gladysGatewayJs = require('@gladysassistant/gladys-gateway-js');
 
 module.exports = {
   gladysGatewayClient: gladysGatewayJs({ cryptoLib, serverUrl: sails.config.gladysGateway.apiUrl, logger: sails.log }),
