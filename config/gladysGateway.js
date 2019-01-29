29cdb8cbaaefe80db6d255bc4d57ee9b9eb30b80
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2019-01-29 09:13:21 +0700

    Bugfix Gladys Gateway server url

--- config/gladysGateway.js
+++ config/gladysGateway.js
@@ -1,4 +1,4 @@
 
 module.exports.gladysGateway = {
-  apiUrl: process.env.GlADYS_GATEWAY_API_URL || 'http://localhost:3000' || 'https://api.gladysgateway.com'
+  apiUrl: process.env.GlADYS_GATEWAY_API_URL || 'https://api.gladysgateway.com'
 };
