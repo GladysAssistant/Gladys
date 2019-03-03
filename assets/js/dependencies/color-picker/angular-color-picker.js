f45bf326a480443dd6e3bbd9f659ac44bb9d6e95
Mathieu Andrade <26581253+MathieuAndrade@users.noreply.github.com>
2019-03-03 04:29:13 +0100

    Update color picker (#478)
    - Update color picker lib
    - Update angular directive

--- assets/js/dependencies/color-picker/angular-color-picker.js
+++ assets/js/dependencies/color-picker/angular-color-picker.js
@@ -6,20 +6,36 @@
             template: '<div class="colorpicker-input" style="width:43px; height:20px; border: #adadad; border-width: 1px; border-style: solid; border-radius: 20px;"></div>',
             require: 'ngModel',
             scope: {
-                ngModel: '='
+                ngModel: '=',
+                intValue: '='
             },
             link: function ($scope, element, attrs, ngModelCtrl, $compile) {
                 var picker = new CP(element[0]);
-		var modelValue = $scope.ngModel;
-		if (modelValue) {
-                    element.css('background-color', '#' + (modelValue).toString(16));
-		}
-
-                picker.on('stop', function (newValue) {
-                    var intValue = parseInt(newValue, 16);
-                    ngModelCtrl.$setViewValue(intValue);
-                    element.css('background-color', '#' + newValue);
+                
+                $scope.$watch('ngModel', function(newValue, oldValue) {
+                    if(Number.isInteger(newValue)){
+                        picker.set('#' + (newValue).toString(16))
+                        picker.source.style.backgroundColor = '#' + (newValue).toString(16)
+                    }else {
+                        picker.set(newValue)
+                        picker.source.style.backgroundColor = newValue
+                    }
+                    
                 });
+
+                picker.on('stop', function(newColor) {
+                    var intValue = $scope.intValue
+
+                    if(intValue != undefined && intValue != null && intValue != false){
+                        ngModelCtrl.$setViewValue(parseInt(newColor, 16));
+                    }else {
+                        ngModelCtrl.$setViewValue('#' + newColor);
+                    }
+                })
+
+                picker.on('change', function(newColor) {
+                    picker.source.style.backgroundColor = '#' + newColor
+                })
             }
         };
     });
