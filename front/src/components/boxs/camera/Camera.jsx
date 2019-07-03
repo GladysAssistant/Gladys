889b0ea2851b1a53bf0454a032a5dd78e602dc4a
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2019-07-03 17:57:48 +0200

    Fix camera loading state

--- front/src/components/boxs/camera/Camera.jsx
+++ front/src/components/boxs/camera/Camera.jsx
@@ -2,13 +2,13 @@ import { Component } from 'preact';
 import { connect } from 'unistore/preact';
 import { Text } from 'preact-i18n';
 import actions from '../../../actions/dashboard/boxes/camera';
-import { DASHBOARD_BOX_STATUS_KEY, DASHBOARD_BOX_DATA_KEY } from '../../../utils/consts';
+import { DASHBOARD_BOX_STATUS_KEY, DASHBOARD_BOX_DATA_KEY, RequestStatus } from '../../../utils/consts';
 import get from 'get-value';
 
 const CameraBox = ({ children, ...props }) => (
   <div class="card">
     {props.image && <img class="card-img-top" src={`data:${props.image}`} alt={props.roomName} />}
-    {!props.image && (
+    {!props.image && props.boxStatus !== RequestStatus.Getting && (
       <div>
         <p class="alert alert-danger">
           <i class="fe fe-bell" />
@@ -19,6 +19,12 @@ const CameraBox = ({ children, ...props }) => (
       </div>
     )}
     <div class="card-body d-flex flex-column">
+      {props.boxStatus === RequestStatus.Getting && (
+        <div class="dimmer active">
+          <div class="dimmer-content" style={{ height: '100px' }} />
+          <div class="loader" />
+        </div>
+      )}
       <h4>{props.box && props.box.name}</h4>
     </div>
   </div>
