855753ab3900a08a43eac11d7cd33be0c338e4ea
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2019-07-03 11:39:43 +0200

    Add more tests

---
+++ server/test/controllers/gatway.test.js
@@ -0,0 +1,31 @@
+const { expect } = require('chai');
+
+const { EVENTS } = require('../../utils/constants');
+
+describe('Gladys Gateway call', () => {
+  it('should call API route threw Gladys Gateway', (done) => {
+    const user = {
+      id: '0cd30aef-9c4e-4a23-88e3-3547971296e5',
+      firstname: 'John',
+      lastname: 'Doe',
+      selector: 'john',
+      email: 'demo@demo.com',
+      language: 'en',
+    };
+    // @ts-ignore
+    global.TEST_GLADYS_INSTANCE.event.emit(
+      EVENTS.GATEWAY.NEW_MESSAGE_API_CALL,
+      user,
+      'GET',
+      '/api/v1/me',
+      {},
+      {},
+      (data) => {
+        expect(data).to.have.property('id', '0cd30aef-9c4e-4a23-88e3-3547971296e5');
+        expect(data).to.have.property('firstname');
+        expect(data).to.have.property('selector', 'john');
+        done();
+      },
+    );
+  });
+});
