d765dcf45f5d05cf6ccf659e382fe0c018156775
damalgos <thibaut.mery@gmail.com>
2019-07-09 20:06:08 +0200

    update test xiaomi service

--- server/test/services/xiaomi/lib/xiaomiManaget.test.js
+++
@@ -1,9 +0,0 @@
-const { expect } = require('chai');
-const { assert } = require('sinon');
-const EventEmitter = require('events');
-
-const event = new EventEmitter();
-const XiaomiManager = require('../../../../services/xiaomi/lib');
-
-describe('xiaomiManager commands', () => {
-  const zwaveManager = new XiaomiManager(ZwaveMock, event, 'de051f90-f34a-4fd5-be2e-e502339ec9cb');
