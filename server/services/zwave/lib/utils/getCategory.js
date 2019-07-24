cf62f50796ca538276fc02954fcfff9e4d00c58d
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2019-07-24 14:44:59 +0200

    Add more z-wave compatibilities (#512)
    * Add more z-wave categories
    
    * Add zwave mapping
    
    * Fix switch power
    
    * Add icons and i18n to new categories
    
    * Add product ids for keypad
    
    * Remove unknown features from zwave view
    
    * Add more detailed translation/icons
    
    * Improve translation
    
    * More precise productid/producttype
    
    * Fix node issue
    
    * Add detailed icons
    
    * Apply change to device page
    
    * Add power and show inactive nodes
    
    * add light sensor / sismic translation
    
    * Put zwave with product id first
    
    * Add product type to smoke sensor
    
    * Fix device zwave view
    
    * Add bindValue function to map states
    
    * Fix linting
    
    * Fix demo mode for zwave

--- server/services/zwave/lib/utils/getCategory.js
+++ server/services/zwave/lib/utils/getCategory.js
@@ -2,27 +2,32 @@ const { CATEGORIES, UNKNOWN_CATEGORY, UNKNOWN_TYPE } = require('../constants');
 
 /**
  * @description Get a ZWave value and return a category in Gladys
+ * @param {Object} node - The node object.
  * @param {Object} value - Value object.
  * @returns {Object} Return the category in Gladys.
  * @example
  * const { category, type } = getCategory({
+ *  productid: '',
+ *  producttype: ''
+ * }, {
  *  class_id: 49,
  *  index: 1,
  * });
  */
-function getCategory(value) {
+function getCategory(node, value) {
   let found = false;
   let categoryFound = null;
   let i = 0;
-  const categories = Object.keys(CATEGORIES);
-  while (!found && i < categories.length) {
-    const category = CATEGORIES[categories[i]];
+  while (!found && i < CATEGORIES.length) {
+    const category = CATEGORIES[i];
     const validComClass = category.COMMAND_CLASSES ? category.COMMAND_CLASSES.includes(value.class_id) : true;
     const validIndex = category.INDEXES ? category.INDEXES.includes(value.index) : true;
-    found = validComClass && validIndex;
+    const validProductId = category.PRODUCT_IDS ? category.PRODUCT_IDS.includes(node.productid) : true;
+    const validProductType = category.PRODUCT_TYPES ? category.PRODUCT_TYPES.includes(node.producttype) : true;
+    found = validComClass && validIndex && validProductId && validProductType;
     if (found) {
       categoryFound = {
-        category: categories[i],
+        category: category.CATEGORY,
         type: category.TYPE,
       };
     }
