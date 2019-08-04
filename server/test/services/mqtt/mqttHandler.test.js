42fb3ef87253c34910b2c41374be73c4c71ba241
atrovato <alexandre.trovato@gmail.com>
2019-08-04 10:31:29 +0200

    MQTT service: fix test

--- server/test/services/mqtt/mqttHandler.test.js
+++ server/test/services/mqtt/mqttHandler.test.js
@@ -1,6 +1,6 @@
 const { assert, fake } = require('sinon');
 const { EVENTS } = require('../../../utils/constants');
-const { MockedMqttClient, event } = require('./mocks.test');
+const { MockedMqttClient } = require('./mocks.test');
 
 const gladys = {
   variable: {
@@ -15,12 +15,10 @@ const MqttHandler = require('../../../services/mqtt/lib');
 
 describe('MqttHandler', () => {
   const mqttHandler = new MqttHandler(gladys, MockedMqttClient, 'faea9c35-759a-44d5-bcc9-2af1de37b8b4');
-  it('should call connect function', () => {
-    mqttHandler.connect();
-    assert.called(MockedMqttClient.internalConnect);
-    event.emit('connect');
-    event.emit('error', 'this is an error');
-    event.emit('message', 'test', 'test');
+  it('should call connect function', async () => {
+    await mqttHandler.connect();
+    assert.callCount(gladys.variable.getValue, 3);
+    assert.calledOnce(MockedMqttClient.internalConnect);
   });
   it('should create device', () => {
     mqttHandler.handleNewMessage('gladys/master/device/create', '{}');
