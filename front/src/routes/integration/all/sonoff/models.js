713efcf9fa0cb23cf0d971dde5aec81eac3c4099
atrovato <alexandre.trovato@gmail.com>
2019-08-13 09:54:19 +0200

    sonoff : scan for devices

--- front/src/routes/integration/all/sonoff/models.js
+++ front/src/routes/integration/all/sonoff/models.js
@@ -1,44 +1,9 @@
-import { DEVICE_FEATURE_CATEGORIES, DEVICE_FEATURE_TYPES } from '../../../../../../server/utils/constants';
+import { featureConverter } from '../../../../../../server/services/mqtt/lib/handler/sonoff/featureConverter';
 
 const Models = {
-  basic: [
-    {
-      category: DEVICE_FEATURE_CATEGORIES.SWITCH,
-      type: DEVICE_FEATURE_TYPES.SWITCH.BINARY,
-      read_only: false,
-      has_feedback: true,
-      min: 0,
-      max: 1
-    }
-  ],
-  pow: [
-    {
-      category: DEVICE_FEATURE_CATEGORIES.SWITCH,
-      type: DEVICE_FEATURE_TYPES.SWITCH.BINARY,
-      read_only: false,
-      has_feedback: true,
-      min: 0,
-      max: 1
-    },
-    {
-      category: DEVICE_FEATURE_CATEGORIES.SWITCH,
-      type: DEVICE_FEATURE_TYPES.SWITCH.POWER,
-      read_only: true,
-      has_feedback: false,
-      min: 0,
-      max: Number.MAX_VALUE
-    }
-  ],
-  s26: [
-    {
-      category: DEVICE_FEATURE_CATEGORIES.SWITCH,
-      type: DEVICE_FEATURE_TYPES.SWITCH.BINARY,
-      read_only: false,
-      has_feedback: true,
-      min: 0,
-      max: 1
-    }
-  ]
+  basic: featureConverter(1),
+  pow: featureConverter(6),
+  s26: featureConverter(8)
 };
 
 export default Models;
