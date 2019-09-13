a2dddd3bd49ba317481d31ac4a42ef3db6d2410d
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2019-09-13 11:12:12 +0200

    Improve Gladys Gateway integration (#536)
    * Add delete function to gateway
    
    * Only accept authorized gateway keys

--- server/lib/gateway/gateway.handleNewMessage.js
+++ server/lib/gateway/gateway.handleNewMessage.js
@@ -1,5 +1,6 @@
 const get = require('get-value');
 
+const { NotFoundError } = require('../../utils/coreErrors');
 const { EVENTS } = require('../../utils/constants');
 
 /**
@@ -7,12 +8,31 @@ const { EVENTS } = require('../../utils/constants');
  * @param {Object} data - Gateway message.
  * @param {Object} rawMessage - Message with metadata.
  * @param {Function} cb - Callback.
+ * @returns {Promise} Resolve when finished.
  * @example
  * handleNewMessage({
  *  type: 'gladys-api-call',
  * });
  */
 async function handleNewMessage(data, rawMessage, cb) {
+  // first, we verify that the user has the right to control the instance
+  const usersKeys = JSON.parse(await this.variable.getValue('GLADYS_GATEWAY_USERS_KEYS'));
+  const rsaPublicKey = await this.gladysGatewayClient.generateFingerprint(rawMessage.rsaPublicKeyRaw);
+  const ecdsaPublicKey = await this.gladysGatewayClient.generateFingerprint(rawMessage.ecdsaPublicKeyRaw);
+
+  const found = usersKeys.find(
+    (user) => user.rsa_public_key === rsaPublicKey && user.ecdsa_public_key === ecdsaPublicKey,
+  );
+
+  if (!found || !found.accepted) {
+    cb({
+      status: 403,
+      error: 'USER_NOT_ACCEPTED_LOCALLY',
+      message: 'User not allowed to control this Gladys instance',
+    });
+    return;
+  }
+
   if (!rawMessage.local_user_id && get(data, 'options.url') !== '/api/v1/user') {
     cb({
       status: 400,
@@ -21,16 +41,27 @@ async function handleNewMessage(data, rawMessage, cb) {
     return;
   }
   if (data.type === 'gladys-api-call') {
-    const user = rawMessage.local_user_id ? await this.user.getById(rawMessage.local_user_id) : null;
-    this.event.emit(
-      EVENTS.GATEWAY.NEW_MESSAGE_API_CALL,
-      user,
-      data.options.method,
-      data.options.url,
-      data.options.query,
-      data.options.data,
-      cb,
-    );
+    try {
+      const user = rawMessage.local_user_id ? await this.user.getById(rawMessage.local_user_id) : null;
+      this.event.emit(
+        EVENTS.GATEWAY.NEW_MESSAGE_API_CALL,
+        user,
+        data.options.method,
+        data.options.url,
+        data.options.query,
+        data.options.data,
+        cb,
+      );
+    } catch (e) {
+      if (e instanceof NotFoundError && e.message === 'User not found') {
+        cb({
+          status: 404,
+          error: 'LINKED_USER_NOT_FOUND',
+        });
+      } else {
+        cb(e);
+      }
+    }
   }
 }
 
