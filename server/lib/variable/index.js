52e028a9acb3406b910c16acd72957014bb19f4a
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2019-09-13 10:12:40 +0200

    Add variable.destroy function

--- server/lib/variable/index.js
+++ server/lib/variable/index.js
@@ -1,8 +1,10 @@
+const { destroy } = require('./variable.destroy');
 const { getValue } = require('./variable.getValue');
 const { setValue } = require('./variable.setValue');
 
-const Variable = function Variable() {};
+const Variable = function Variable() { };
 
+Variable.prototype.destroy = destroy;
 Variable.prototype.setValue = setValue;
 Variable.prototype.getValue = getValue;
 
