702e10eb91873d7db212a318738e879955688910
atrovato <alexandre.trovato@gmail.com>
2019-10-15 11:25:53 +0200

    Sonoff: improve manual/auto device creation

--- front/src/routes/integration/all/sonoff/device-page/EmptyState.jsx
+++ front/src/routes/integration/all/sonoff/device-page/EmptyState.jsx
@@ -7,19 +7,20 @@ const EmptyState = ({ children, ...props }) => (
   <div class="col-md-12">
     <div class={cx('text-center', style.emptyStateDivBox)}>
       <Text id="integration.sonoff.device.noDeviceFound" />
-      <div class="row justify-content-center mt-4">
-        <div>
-          <Link href="/dashboard/integration/device/sonoff/discover">
-            <button class="btn btn-outline-primary ml-2">
-              <Text id="integration.sonoff.discoverTab" /> <i class="fe fe-radio" />
-            </button>
-          </Link>
-        </div>
-        <div>
-          <button onClick={props.addDevice} class="btn btn-outline-primary ml-2">
-            <Text id="integration.sonoff.device.newButton" /> <i class="fe fe-plus" />
+
+      <div class="mt-5">
+        <Text id="integration.sonoff.discoverDeviceDescr" />
+        <Link href="/dashboard/integration/device/sonoff/discover">
+          <button class="btn btn-outline-primary ml-2">
+            <Text id="integration.sonoff.discoverTab" /> <i class="fe fe-radio" />
           </button>
-        </div>
+        </Link>
+      </div>
+      <div class=" mt-5">
+        <Text id="integration.sonoff.prepareDeviceDescr" />
+        <button onClick={props.addDevice} class="btn btn-outline-primary ml-2">
+          <Text id="integration.sonoff.device.newButton" /> <i class="fe fe-plus" />
+        </button>
       </div>
     </div>
   </div>
