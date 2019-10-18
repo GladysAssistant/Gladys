b35f267bc6125fa46e5e83691b4dbde969c332bc
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2019-10-18 14:09:10 +0200

    Disable scene UI in Gladys beta

--- front/src/routes/scene/index.js
+++ front/src/routes/scene/index.js
@@ -1,8 +1,9 @@
 import { Component } from 'preact';
 import { connect } from 'unistore/preact';
-import ScenePage from './ScenePage';
+// import ScenePage from './ScenePage';
+import ComingSoon from './ComingSoon';
 import actions from '../../actions/scene';
-import { RequestStatus } from '../../utils/consts';
+// import { RequestStatus } from '../../utils/consts';
 
 @connect(
   'scenes,currentUrl,scenesGetStatus',
@@ -14,8 +15,9 @@ class Scene extends Component {
   }
 
   render(props, {}) {
-    const loading = props.scenesGetStatus === RequestStatus.Getting;
-    return <ScenePage {...props} loading={loading} />;
+    // const loading = props.scenesGetStatus === RequestStatus.Getting;
+    // return <ScenePage {...props} loading={loading} />;
+    return <ComingSoon />;
   }
 }
 
