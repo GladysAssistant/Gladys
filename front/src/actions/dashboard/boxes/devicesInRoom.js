12d05734085de496cfd7736be1e9e87ea4e3e95e
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2019-10-22 16:16:00 +0200

    Fix #567 : Device in room general light status is refreshed when device state changes

--- front/src/actions/dashboard/boxes/devicesInRoom.js
+++ front/src/actions/dashboard/boxes/devicesInRoom.js
@@ -9,23 +9,22 @@ const BOX_KEY = 'DevicesInRoom';
 
 const getLightStatus = room => {
   let roomLightStatus = 0;
-  const binaryLightDevice = room.devices.find(device => {
-    return (
-      device.features.find(feature => {
-        // if it's a light
-        const isLight =
-          feature.category === DEVICE_FEATURE_CATEGORIES.LIGHT && feature.type === DEVICE_FEATURE_TYPES.LIGHT.BINARY;
-        // if it's a light and it's turned on, we consider that the light
-        // is on in the room
-        if (isLight && feature.last_value === 1) {
-          roomLightStatus = 1;
-        }
-        return isLight;
-      }) !== undefined
-    );
+  let hasBinaryLightDeviceFeature = false;
+  room.devices.forEach(device => {
+    device.features.forEach(feature => {
+      // if it's a light
+      const isLight =
+        feature.category === DEVICE_FEATURE_CATEGORIES.LIGHT && feature.type === DEVICE_FEATURE_TYPES.LIGHT.BINARY;
+      // if it's a light and it's turned on, we consider that the light
+      // is on in the room
+      if (isLight && feature.last_value === 1) {
+        roomLightStatus = 1;
+      }
+      if (isLight) {
+        hasBinaryLightDeviceFeature = true;
+      }
+    });
   });
-  // if yes, it will display the "ON/OFF" button for the room
-  const hasBinaryLightDeviceFeature = binaryLightDevice !== undefined;
   return {
     roomLightStatus,
     hasBinaryLightDeviceFeature
@@ -96,8 +95,11 @@ function createActions(store) {
           }
         }
       });
+      const { hasBinaryLightDeviceFeature, roomLightStatus } = getLightStatus(newData.room);
       boxActions.mergeBoxData(state, BOX_KEY, x, y, {
-        room: newData.room
+        room: newData.room,
+        hasBinaryLightDeviceFeature,
+        roomLightStatus
       });
     },
     deviceFeatureWebsocketEvent(state, x, y, payload) {
