ea93de7aa2072778bca11ac28c0d4de0c48623cd
renaiku <corbeau.lenaic@gmail.com>
2019-11-16 16:16:46 +0100

    kc2

--- server/services/magic-devices/lib/commands/magicdevices.setValue.js
+++ server/services/magic-devices/lib/commands/magicdevices.setValue.js
@@ -1,7 +1,6 @@
 const logger = require('../../../../utils/logger');
 const { hslToRgb } = require('../../../../utils/units');
 const { getDeviceParam } = require('../../../../utils/device');
-const { generateGatewayKey } = require('../utils/generateGatewayKey');
 
 const SERVER_PORT = 9898;
 
@@ -11,49 +10,11 @@ const SERVER_PORT = 9898;
  * @param {Object} deviceFeature - The feature to control.
  * @param {Object} state - The new state.
  * @example
- * xiaomi.setValue();
+ * magicDevices.setValue();
  */
 function setValue(device, deviceFeature, state) {
-  logger.debug(`Xiaomi.setValue : Setting value of device ${device.external_id}`);
-  let msg;
-  const rgb = hslToRgb(214, 1, 0.5);
-  const rgbNumber = 0xff000000 + (rgb[0] << 16) + (rgb[1] << 8) + rgb[2]; // eslint-disable-line no-bitwise
-  const sid = device.external_id.split(':')[1];
-  const ip = this.gatewayIpBySensorSid.get(sid);
-  const token = this.gatewayTokenByIp.get(ip);
-  const password = getDeviceParam(this.sensors[sid], 'GATEWAY_PASSWORD');
-  if (!ip || !token || !password) {
-    throw new Error('Missing information to connect to the Gateway');
-  }
-  const key = generateGatewayKey(token, password);
-  switch (device.model) {
-    case 'xiaomi-gateway':
-      msg = {
-        cmd: 'write',
-        model: 'gateway',
-        sid,
-        data: JSON.stringify({
-          rgb: rgbNumber,
-          key,
-        }),
-      };
-      break;
-    case 'xiaomi-duplex-wired-switch':
-      msg = {
-        cmd: 'write',
-        model: this.sensorModelBySensorSid.get(sid),
-        sid,
-        data: JSON.stringify({
-          channel_0: 'off',
-          key,
-        }),
-      };
-      break;
-    default:
-      throw new Error('Device not handled');
-  }
-  const msgStr = JSON.stringify(msg);
-  this.socket.send(msgStr, 0, msgStr.length, SERVER_PORT, ip, (cb) => {});
+  logger.debug(`MagicDevices.setValue : Setting value of device ${device.external_id}`);
+  // this.socket.send(msgStr, 0, msgStr.length, SERVER_PORT, ip, (cb) => {});
 }
 
 module.exports = {
