af0fadd0866dd6dbe806525d741d17da58feb295
VonOx <vkulak62@gmail.com>
2020-01-02 20:32:55 +0100

    Test build

--- .circleci/load_env.sh
+++ .circleci/load_env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo 'export GITHUB_REPO=gladysassistant/Gladys' >>$BASH_ENV
+echo 'export GITHUB_REPO=vonox/Gladys' >>$BASH_ENV
 echo 'export IMAGE=gladys' >>$BASH_ENV
 echo 'export REGISTRY=gladysassistant' >>$BASH_ENV
 
