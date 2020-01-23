0d2d2d52a60b04d4c8774cc4fa216a7f293286cb
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2020-01-23 10:30:34 +0100

    Use react-select in choose trigger card

--- front/src/routes/scene/edit-scene/triggers/ChooseTriggerTypeCard.jsx
+++ front/src/routes/scene/edit-scene/triggers/ChooseTriggerTypeCard.jsx
@@ -1,33 +1,42 @@
 import { Component } from 'preact';
 import { connect } from 'unistore/preact';
 const TRIGGER_LIST = ['device.new-state'];
+import Select from 'react-select';
 
 @connect('httpClient', {})
 class ChooseTriggerType extends Component {
   state = {
     currentTrigger: null
   };
-  handleChange = e => {
-    this.setState({
-      currentTrigger: e.target.value
-    });
-  };
-  changeBoxType = () => {
-    if (this.state.currentTrigger && this.state.currentTrigger.length) {
-      this.props.updateTriggerProperty(this.props.index, 'type', this.state.currentTrigger);
+  handleChange = selectedOption => {
+    if (selectedOption) {
+      this.setState({
+        currentTrigger: selectedOption
+      });
+    } else {
+      this.setState({
+        currentTrigger: null
+      });
     }
   };
-  render(props, {}) {
+  changeBoxType = () => {
+    if (this.state.currentTrigger) {
+      this.props.updateTriggerProperty(this.props.index, 'type', this.state.currentTrigger.value);
+    }
+  };
+  render(props, { currentTrigger }) {
     return (
       <div>
         <div class="form-group">
           <label class="form-label">Select a trigger type</label>
-          <select class="custom-select" onChange={this.handleChange}>
-            <option value="">-------</option>
-            {TRIGGER_LIST.map(trigger => (
-              <option value={trigger}>{trigger}</option>
-            ))}
-          </select>
+          <Select
+            value={currentTrigger}
+            options={TRIGGER_LIST.map(trigger => ({
+              value: trigger,
+              label: trigger
+            }))}
+            onChange={this.handleChange}
+          />
         </div>
         <div class="form-group">
           <button onClick={this.changeBoxType} class="btn btn-success">
