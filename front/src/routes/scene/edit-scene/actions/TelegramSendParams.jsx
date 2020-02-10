4a13ef405a289d5dd2c8627f38f86a5508bc8f1e
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2020-02-10 09:56:04 +0100

    Telegram card in scene view

--- front/src/routes/scene/edit-scene/actions/TelegramSendParams.jsx
+++ front/src/routes/scene/edit-scene/actions/TelegramSendParams.jsx
@@ -1,40 +1,47 @@
 import Select from 'react-select';
+import { Component } from 'preact';
+import { connect } from 'unistore/preact';
+import { Text } from 'preact-i18n';
 
-const TelegramSendParams = ({ children, ...props }) => (
-  <div>
-    <div class="form-group">
-      <label class="form-label">
-        User <span class="form-required">*</span>
-      </label>
-      <Select
-        options={
-          props.sceneParamsData &&
-          props.sceneParamsData.users.map(user => ({
-            value: user.selector,
-            label: user.firstname
-          }))
-        }
-      />
-    </div>
-    <div class="form-group">
-      <label class="form-label">
-        Telegram message <span class="form-required">*</span>
-      </label>
-      <textarea class="form-control" value={props.action.text} placeholder="Text" />
-    </div>
-    <div class="form-group">
-      <label class="form-label">Join file</label>
-      <Select
-        isClearable
-        options={[
-          {
-            value: 'camera',
-            label: `Kitchen's camera`
-          }
-        ]}
-      />
-    </div>
-  </div>
-);
+@connect('', {})
+class TelegramSendParams extends Component {
+  handleChangeDuration = e => {
+    this.props.updateActionProperty(this.props.columnIndex, this.props.index, 'value', e.target.value);
+  };
+  handleChangeUnit = e => {
+    this.props.updateActionProperty(this.props.columnIndex, this.props.index, 'unit', e.target.value);
+  };
+  componentDidMount() {
+    if (!this.props.unit) {
+      this.props.updateActionProperty(this.props.columnIndex, this.props.index, 'unit', 'seconds');
+    }
+  }
+  render(props, {}) {
+    return (
+      <div>
+        <div class="form-group">
+          <label class="form-label">
+            User <span class="form-required">*</span>
+          </label>
+          <Select
+            options={
+              props.sceneParamsData &&
+              props.sceneParamsData.users.map(user => ({
+                value: user.selector,
+                label: user.firstname
+              }))
+            }
+          />
+        </div>
+        <div class="form-group">
+          <label class="form-label">
+            Telegram message <span class="form-required">*</span>
+          </label>
+          <textarea class="form-control" value={props.action.text} placeholder="Text" />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default TelegramSendParams;
