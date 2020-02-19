11233fa18db6f2804130e9a4490b0e5f44102207
Bertrand d'Aure <bertrandda@laposte.net>
2020-02-19 23:39:35 +0100

    Fix prettier

--- server/test/services/caldav/index.test.js
+++ server/test/services/caldav/index.test.js
@@ -6,16 +6,16 @@ const CaldavService = require('../../../services/caldav/index');
 describe('CaldavService', () => {
   const gladys = {
     user: {
-      get: stub().resolves([{ id: '9de05cca-85bd-4218-a715-c2fa8e934408' }])
+      get: stub().resolves([{ id: '9de05cca-85bd-4218-a715-c2fa8e934408' }]),
     },
     service: {
       getLocalServiceByName: stub().resolves({
-        id: '6d1bd783-ab5c-4d90-8551-6bc5fcd02212'
-      })
+        id: '6d1bd783-ab5c-4d90-8551-6bc5fcd02212',
+      }),
     },
     variable: {
-      getValue: stub().resolves(null)
-    }
+      getValue: stub().resolves(null),
+    },
   };
   const caldavService = CaldavService(gladys);
 
@@ -37,7 +37,8 @@ describe('CaldavService', () => {
     await caldavService.syncAllUsers();
     expect(gladys.user.get.callCount).to.equal(1);
     expect(gladys.service.getLocalServiceByName.args).to.eql([['caldav']]);
-    expect(gladys.variable.getValue.args).to.eql([['CALDAV_URL', '6d1bd783-ab5c-4d90-8551-6bc5fcd02212', '9de05cca-85bd-4218-a715-c2fa8e934408']]);
-
+    expect(gladys.variable.getValue.args).to.eql([
+      ['CALDAV_URL', '6d1bd783-ab5c-4d90-8551-6bc5fcd02212', '9de05cca-85bd-4218-a715-c2fa8e934408'],
+    ]);
   });
 });
