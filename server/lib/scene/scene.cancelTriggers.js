ad8b9e7739faf87082561816986993b96451358a
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2020-03-02 18:36:30 +0100

    Add tests on cancelTriggers function

--- server/lib/scene/scene.cancelTriggers.js
+++ server/lib/scene/scene.cancelTriggers.js
@@ -9,9 +9,11 @@ function cancelTriggers(sceneSelector) {
     this.scenes[sceneSelector].triggers.forEach((trigger) => {
       if (trigger.nodeScheduleJob) {
         trigger.nodeScheduleJob.cancel();
+        delete trigger.nodeScheduleJob;
       }
       if (trigger.jsInterval) {
         clearInterval(trigger.jsInterval);
+        delete trigger.jsInterval;
       }
     });
   }
