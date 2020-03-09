549f8e673b94d803554d79c42f6d5c45f2af61c6
VonOx <vkulak62@gmail.com>
2020-03-09 14:43:59 +0100

    Manifest use now tag release on latest

--- .circleci/load_env.sh
+++ .circleci/load_env.sh
@@ -4,7 +4,7 @@ echo 'export GITHUB_REPO=vonox/Gladys' >>$BASH_ENV
 echo 'export IMAGE=gladys' >>$BASH_ENV
 echo 'export REGISTRY=vonox' >>$BASH_ENV
 
-echo 'export VERSION=4.0.0-beta' >>$BASH_ENV
+echo 'export VERSION=${CIRCLE_TAG}' >>$BASH_ENV
 echo 'export IMAGE_ID="${REGISTRY}/${IMAGE}:${VERSION}-${TAG}"' >>$BASH_ENV
 echo 'export DIR=`pwd`' >>$BASH_ENV
 echo 'export QEMU_VERSION="v4.2.0-2"' >>$BASH_ENV
