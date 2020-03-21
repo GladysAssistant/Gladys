0a5316ceef6589d2f04a69c3d7b0f6bdbe5057b7
Mathis Tondenier <40740136+mTondenier@users.noreply.github.com>
2020-03-21 12:56:25 +0100

    tests fixes

--- server/test/services/rflink/lib/devicesToTest.test.js
+++ server/test/services/rflink/lib/devicesToTest.test.js
@@ -24,7 +24,29 @@ const DEVICES = [
       },
     ],
   },
-  {},
+  {
+    service_id: 'a810b8db-6d04-4697-bed3-c4b72c996279',
+    name: `Prise `,
+    selector: `rflink:86aa70:10`,
+    external_id: `rflink:86aa70:10`,
+    model: 'Tristate',
+    should_poll: false,
+    features: [
+      {
+        name: 'switch',
+        selector: `rflink:86aa70:switch:10`,
+        external_id: `rflink:86aa70:switch:10`,
+        rfcode: 'CMD',
+        category: DEVICE_FEATURE_CATEGORIES.SWITCH,
+        type: DEVICE_FEATURE_TYPES.SENSOR.BINARY,
+        read_only: false,
+        keep_history: true,
+        has_feedback: false,
+        min: 0,
+        max: 1,
+      },
+    ],
+  },
 ];
 
 module.exports = DEVICES;
