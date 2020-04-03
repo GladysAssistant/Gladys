127bd5df2336fa0f0322d9f6e4df86a2eeaa4186
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2020-04-03 11:27:27 +0200

    Resolves #685 : Implement new MQTT API (#686)
    * Handle new MQTT API, device to Gladys
    
    * Fix mqtt tests
    
    * Remove exact match
    
    * Throw error if device feature external_id is not present
    
    * Add setValue function
    
    * Add ability to edit read-only + display MQTT topic + copy to clipboard
    
    * value sent to mqtt topic should be a string
    
    * Make read-only text more explicit

---
+++ server/test/services/mqtt/lib/setValue.test.js
@@ -0,0 +1,44 @@
+const sinon = require('sinon');
+const assertChai = require('chai').assert;
+
+const { assert, fake } = sinon;
+const { MockedMqttClient, mqttApi } = require('../mocks.test');
+
+const gladys = {
+  variable: {
+    getValue: fake.resolves('result'),
+  },
+  event: {
+    emit: fake.returns(null),
+  },
+};
+
+const MqttHandler = require('../../../../services/mqtt/lib');
+
+describe('mqtt.setValue', () => {
+  const mqttHandler = new MqttHandler(gladys, MockedMqttClient, 'faea9c35-759a-44d5-bcc9-2af1de37b8b4');
+
+  beforeEach(async () => {
+    sinon.reset();
+  });
+
+  it('should not publish, as not connected', async () => {
+    const promise = mqttHandler.setValue({ external_id: 'my-device' }, { external_id: 'my-device-light' }, 1);
+
+    await assertChai.isRejected(promise);
+    assert.notCalled(mqttApi.publish);
+  });
+
+  it('should publish message', async () => {
+    await mqttHandler.connect();
+    await mqttHandler.setValue({ external_id: 'my-device' }, { external_id: 'my-device-light' }, 1);
+
+    assert.calledWith(
+      mqttApi.publish,
+      'gladys/device/my-device/feature/my-device-light/state',
+      '1',
+      undefined,
+      sinon.match.func,
+    );
+  });
+});
