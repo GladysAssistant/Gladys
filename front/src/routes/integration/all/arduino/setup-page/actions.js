89b1c70b48b79b1c4f7d77be00616a5b4bfe960d
billonalex <billonalex21@gmail.com>
2020-05-05 17:59:49 +0200

    correction model

--- front/src/routes/integration/all/arduino/setup-page/actions.js
+++ front/src/routes/integration/all/arduino/setup-page/actions.js
@@ -154,9 +154,6 @@ const actions = store => {
         await state.httpClient.post('/api/v1/service/arduino/variable/ARDUINO_PATH', {
           value: state.arduinoPath
         });
-        await state.httpClient.post('/api/v1/service/arduino/variable/ARDUINO_MODEL', {
-          value: state.arduinoModel
-        });
         await state.httpClient.post('/api/v1/service/arduino/connect');
         store.setState({
           connectArduinoStatus: RequestStatus.Success,
