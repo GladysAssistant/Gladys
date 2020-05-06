7755fa97a4dad2f3a3d478b6715668f87a24a5fe
billonalex <billonalex21@gmail.com>
2020-05-06 23:29:00 +0200

    test device

--- front/src/routes/integration/all/arduino/device-page/Device.jsx
+++ front/src/routes/integration/all/arduino/device-page/Device.jsx
@@ -4,6 +4,8 @@ import cx from 'classnames';
 import { RequestStatus } from '../../../../../utils/consts';
 import ArduinoDeviceForm from './DeviceForm';
 import { Link } from 'preact-router/match';
+import { DEVICE_SUBSERVICE_LIST } from '../../../../../../../server/utils/constants';
+import get from 'get-value';
 
 class ArduinoDeviceBox extends Component {
   saveDevice = async () => {
@@ -26,6 +28,22 @@ class ArduinoDeviceBox extends Component {
     this.setState({ loading: false });
   };
 
+  updateName = e => {
+    this.props.updateDeviceProperty(this.props.deviceIndex, 'name', e.target.value);
+  };
+
+  updateDataPin = e => {
+    this.props.updateDeviceProperty(this.props.deviceIndex, 'dataPin', e.target.value);
+  };
+
+  updateRoom = e => {
+    this.props.updateDeviceProperty(this.props.deviceIndex, 'room_id', e.target.value);
+  };
+
+  updateExternalId = e => {
+    this.props.updateDeviceProperty(this.props.deviceIndex, 'external_id', e.target.value);
+  };
+
   render(props, { loading }) {
     return (
       <div class="col-md-6">
@@ -39,8 +57,99 @@ class ArduinoDeviceBox extends Component {
             <div class="loader" />
             <div class="dimmer-content">
               <div class="card-body">
-                <ArduinoDeviceForm {...props} />
+                {/*<ArduinoDeviceForm {...props} />*/}
+                <div>
+                  <div class="form-group">
+                    <label class="form-label" for="deviceName">
+                      <Text id="integration.arduino.device.nameLabel" />
+                    </label>
+                    <Localizer>
+                      <input
+                        id="deviceName"
+                        type="text"
+                        value={props.device.name}
+                        onInput={this.updateName}
+                        class="form-control"
+                        placeholder={<Text id="integration.arduino.device.nameLabel" />}
+                      />
+                    </Localizer>
+                  </div>
 
+                  <div class="form-group">
+                    <label class="form-label" for="room">
+                      <Text id="integration.arduino.device.roomLabel" />
+                    </label>
+                    <select onChange={this.updateRoom} class="form-control" id="room">
+                      <option value="">
+                        <Text id="global.emptySelectOption" />
+                      </option>
+                      {props.houses &&
+                        props.houses.map(house => (
+                          <optgroup label={house.name}>
+                            {house.rooms.map(room => (
+                              <option selected={room.id === props.device.room_id} value={room.id}>
+                                {room.name}
+                              </option>
+                            ))}
+                          </optgroup>
+                        ))}
+                    </select>
+                  </div>
+
+                  <div class="form-group">
+                    <label class="form-label" for="subservice">
+                      <Text id="integration.arduino.device.subserviceLabel" />
+                    </label>
+                    <select class="form-control" id="subservice">
+                      <option value={DEVICE_SUBSERVICE_LIST.EMIT_433}>
+                        <Text id="integration.arduino.subservice.emit433" />
+                      </option>
+                      <option value={DEVICE_SUBSERVICE_LIST.EMIT_433_CHACON}>
+                        <Text id="integration.arduino.subservice.emit433Chacon" />
+                      </option>
+                      <option value={DEVICE_SUBSERVICE_LIST.EMIT_IR}>
+                        <Text id="integration.arduino.subservice.emitIR" />
+                      </option>
+                    </select>
+                  </div>
+
+                  <div class="form-group">
+                    <label class="form-label" for="dataPin">
+                      <Text id="integration.arduino.device.dataPinLabel" />
+                    </label>
+                    <Localizer>
+                      <input
+                        id="dataPin"
+                        type="text"
+                        value={props.device.dataPin}
+                        onInput={this.updateDataPin}
+                        class="form-control"
+                        placeholder={<Text id="integration.arduino.device.dataPinLabel" />}
+                      />
+                    </Localizer>
+                  </div>
+
+                  <div class="form-group">
+                    <label class="form-label">
+                      <Text id="integration.arduino.device.featuresLabel" />
+                    </label>
+                    <div class="tags">
+                      {props.device &&
+                        props.device.features &&
+                        props.device.features.map(feature => (
+                          <span class="tag">
+                            <Text id={`deviceFeatureCategory.${feature.category}.${feature.type}`} />
+                            <div class="tag-addon">
+                              <i class={`fe fe-${get(DeviceFeatureCategoriesIcon, `${feature.category}.${feature.type}`)}`} />
+                            </div>
+                          </span>
+                        ))}
+                      {(!props.device.features || props.device.features.length === 0) && (
+                        <Text id="integration.arduino.device.noFeatures" />
+                      )}
+                    </div>
+                  </div>
+                </div>
                 <div class="form-group">
                   <button onClick={this.saveDevice} class="btn btn-success mr-2">
                     <Text id="integration.arduino.device.saveButton" />
