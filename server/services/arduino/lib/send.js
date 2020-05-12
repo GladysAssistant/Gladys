d4d1e2cfcb0f5fea51933188f4e9fe03cca2e4fa
billonalex <billonalex21@gmail.com>
2020-05-12 23:00:50 +0200

    Update send

--- server/services/arduino/lib/send.js
+++ server/services/arduino/lib/send.js
@@ -9,20 +9,15 @@ const SerialPort = require('serialport');
  */
 async function send(path, message) {
   try {
-    //const arduinoDevice = await
-    //var message = '{"function_name": "emit_433","parameters": {"code": "4464676","bit_length": "24","data_pin": "4"}}%';
-    logger.warn(`Path: ${path}`);
-    logger.warn(`Message: ${message}`);
-    const port = new SerialPort(path, function (err) {
-      if (err) {
-        return logger.warn('Error: ', err.message);
-      }
-    });
+    
+    const textToSend = JSON.stringify(message) + "%";
+
+    const port = new SerialPort(path, {baudRate: 9600, lock: false});
 
     if (!port.isOpen) {
       port.on('open', function () {
         logger.warn('port opened');
-        port.write(message + "%");
+        port.write(textToSend);
       });
     }
 
