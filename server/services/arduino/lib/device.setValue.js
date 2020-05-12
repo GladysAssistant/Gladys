ee38744876749dd74a5ac09d02632a9e01018b9b
billonalex <billonalex21@gmail.com>
2020-05-12 19:26:48 +0200

    Creation message

--- server/services/arduino/lib/device.setValue.js
+++ server/services/arduino/lib/device.setValue.js
@@ -1,4 +1,4 @@
-const { DEVICE_FEATURE_TYPES } = require('../../../utils/constants');
+const { DEVICE_FEATURE_TYPES, DEVICE_FUNCTION } = require('../../../utils/constants');
 
 const logger = require('../../../utils/logger');
 const { NotFoundError } = require('../../../utils/coreErrors');
@@ -15,15 +15,34 @@ async function setValue(device, deviceFeature, value) {
 
   const arduinoSelectorIndex = device.params.findIndex((param) => param.name === 'ARDUINO_LINKED');
   const arduinoSelector = device.params[arduinoSelectorIndex].value;
-
   const arduino = await this.gladys.device.getBySelector(arduinoSelector);
-
   const pathIndex = arduino.params.findIndex((param) => param.name === 'ARDUINO_PATH');
   const path = arduino.params[pathIndex].value;
-  logger.debug(path);
 
-  //Récupérer l'Arduino rattaché au device
-  //En récupérer son path
+  const functionName = device.params.find((param) => param.name === 'FUNCTION').value;
+
+  var message = {
+    function: functionName,
+    parameters: {
+      data_pin: device.params.find((param) => param.name === 'DATA_PIN').value,
+    },
+  };
+
+  switch (functionName) {
+    case DEVICE_FUNCTION.EMIT_433:
+      message.parameters['code'] = device.params.find((param) => param.name === 'CODE').value;
+      break;
+    case DEVICE_FUNCTION.EMIT_IR:
+      message.parameters['code'] = device.params.find((param) => param.name === 'CODE').value;
+      break;
+    default:
+      break;
+  }
+
+  logger.debug(message);
+
+  //Récupérer l'Arduino rattaché au device                                                    --OK
+  //En récupérer son path                                                                     --OK
   //Créer le JSON, message qui sera à transmettre à l'Arduino
   //Appeler sendValue avec le path et le JSON
 
