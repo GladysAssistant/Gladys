389394b62f22d0a1c09513b9767f69f985199bad
billonalex <billonalex21@gmail.com>
2020-05-14 16:40:08 +0200

    Test upload

--- server/services/arduino/lib/setup.js
+++ server/services/arduino/lib/setup.js
@@ -17,7 +17,7 @@ async function setup(device) {
       path: arduinoPath,
     });
 
-    avrgirl.flash('arduino-code/arduino-code.ino.standard.hex', function (error) {
+    avrgirl.flash('arduino-code.ino.standard.hex', function (error) {
       if (error) {
         logger.warn(error);
       } else {
