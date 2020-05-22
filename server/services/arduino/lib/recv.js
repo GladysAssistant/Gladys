112838b776fae99a1febf7eaa3dd2627d058e9d6
billonalex <billonalex21@gmail.com>
2020-05-23 01:31:49 +0200

    Correction

--- server/services/arduino/lib/recv.js
+++ server/services/arduino/lib/recv.js
@@ -38,7 +38,7 @@ async function recv(device) {
         //gladys.device.setParam(device, 'CODE', data.toString('utf8'));
         if (IsJsonString(data.toString('utf8'))) {
           var messageJSON = JSON.parse(data.toString('utf8'));
-          gladys.device.setParam(device, 'CODE', messageJSON.parameters.value);
+          //gladys.device.setParam(device, 'CODE', messageJSON.parameters.value);
           gladys.device.setValue({ id: device.id }, device.features[0], messageJSON.parameters.value);
         }
       });
