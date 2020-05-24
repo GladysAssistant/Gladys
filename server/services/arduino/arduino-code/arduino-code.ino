860122f8bd62c5d25a7048b3a146aaf05bb86bf9
billonalex <billonalex21@gmail.com>
2020-05-24 04:17:04 +0200

    Update arduino code

--- server/services/arduino/arduino-code/arduino-code.ino
+++ server/services/arduino/arduino-code/arduino-code.ino
@@ -1,14 +1,22 @@
 #include <ArduinoJson.h>
 #include <RCSwitch.h>
 #include <IRremote.h>
+#include <dht.h>
+
+dht DHT;
+
+IRsend ir_send;                                                  // Crée une instance pour controler les led (pin 3 par défaut)
+RCSwitch mySwitch = RCSwitch();                                  // Crée une instance pour la réception 433 MHz (pin 2)
+
+decode_results results;                                          // Variable contenant le résultat des réceptions IR
 
 bool recv433 = true;
 bool recvIR = false;
+bool dhtEnabled = false;
+
+int dht_pin;
 
 const unsigned int THIGH = 220, TSHORT = 350, TLONG = 1400;       // Temps des états (nécessaire à l'envoi de signaux Chacon)
-
-//const int VCC_EMIT_PIN = 48;
-//const int GND_EMIT_PIN = 46;
 
 // Serial buffer
 String command = "";
@@ -16,21 +24,13 @@ String command = "";
 // End of command marker
 char endMarker = '%';
 
-//IRrecv irrecv_led(DATA_RECV_PIN_IR_LED);
-//IRrecv irrecv_tv(DATA_RECV_PIN_IR_TV);
-
-IRsend ir_send;                                                  // Crée une instance pour controler les led (pin 3 par défaut)
-RCSwitch mySwitch = RCSwitch();                                  // Crée une instance pour la réception 433 MHz (pin 2)
-
-decode_results results;                                         // Variable contenant le résultat des réceptions IR
-
 /*
    Functions that will be called by Gladys
 */
 
 void emit_ir(unsigned long code, int bit_length, int data_pin) {    // Fonction à appeler pour envoyer un code IR au ruban LED
   ir_send.changePin(data_pin);
-  ir_send.sendNEC(code, bit_length);                                // code télécommande et nombre de bits
+  ir_send.sendNEC(code, bit_length);
 }
 
 void emit_433(long code, int bit_length, int data_pin) {            // Fonction à appeler pour envoyer un code en 433 MHz
@@ -50,29 +50,10 @@ void recv_433(bool isEnabled, int data_pin) {
   recv433 = isEnabled;
 }
 
-/*
-  void detectIR_LED(){                                              // Fonction à appeler dans void loop() pour permettre la détection de signaux IR du ruban LED
-    if (irrecv_led.decode(&results)) {
-      Serial.print("{\"action\":\"received\",\"value\":");
-      Serial.print(results.value, HEX);
-      Serial.println("}");
-      irrecv_led.resume(); // Receive the next value
-    }
-    delay(100);
-  }
-
-  void detectChacon(int data_pin){                                  // Fonction à appeler dans void loop() pour permettre la détection de signaux Chacon
-    unsigned long sender = listenSignalDIO(data_pin);
-
-    if(sender != 0){
-      Serial.print("{\"action\":\"received\",\"value\":");
-      Serial.print(sender);
-      Serial.println("}");
-      delay(200);
-    }
-  }
-
-*/
+void recv_dht(bool enabled, int data_pin){
+  dhtEnabled = enabled;
+  dht_pin = data_pin;
+}
 
 /*
    Fonction permettant d'envoyer un signal radio Chacon
@@ -152,65 +133,25 @@ void serialEvent() {
   }
 }
 
-/*
-  This function is used to detect 433 MHz signals from Chacon outlets
-*/
-/*
-  unsigned long listenSignalDIO(int data_pin){
-        int i = 0;                          //Variable du compteur boucle
-        unsigned long t = 0;                //Variable de temps entre 2 front descendant
-        byte prevBit = 0;                   //avant dernière "bit" 433 reçue
-        byte bit = 0;                       //dernière "bit" 433 reçue
-        unsigned long sender = 0;           //code DIO au format unsigned long - ne gère pas le bit on-off, donne 2 codes différents
-        t = pulseIn(data_pin, LOW, 1000000);//réception des bits
-        if(t > 2550 && t < 2800) {           //on lance le déchiffrage quand on reçoit un bit correspondant au deuxieme cadenas du signal DIO
-          while(i < 64)
-              {
-              t = pulseIn(data_pin, LOW, 1000000);  //reception des bits
-              if(t > 280 && t < 340)                //si environ 310 + ou - 30 c'est un bit à 0
-                  {   bit = 0;
-                  }
-                  else if(t > 1300 && t < 1400) //si environ 1350 + ou moins 50 c'est un bit à 1
-                        {   bit = 1;
-                        }
-                        else    //sinon la série est érronnée, on sort de la fonction
-                        break;
-             if(i % 2 == 1) // on ne traite que les deuxièmes bits (codage manchester)
-                  {
-                  if((prevBit ^ bit) == 0) //si faux = codage manchester KO, on sort
-                       break;
-                  sender <<= 1;       //on stock chaque bit dans l'unsigned long
-                  sender |= prevBit;
-                  }
-             prevBit = bit;  //on enregistre le bit précédent
-              ++i;
-            }
-        }
-        if(i == 64){     // si on a 64 bit (i=64) on a un code DIO détecté
-         return sender;
-        }
-     return 0; //sinon pas de code DIO
-  }
-*/
 void setup() {
 
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
 
-  //pinMode(GND_EMIT_PIN, OUTPUT);
-  //pinMode(VCC_EMIT_PIN, OUTPUT);
-  //digitalWrite(VCC_EMIT_PIN, HIGH);
-  //digitalWrite(GND_EMIT_PIN, LOW);
-
   recv_433(true,0);
 
-  // Start the IR receivers
-  //irrecv_led.enableIRIn();
 }
 
 void loop() {
-  //detectChacon();
-  //detectIR_LED();
+  if(dhtEnabled){                                                  // Partie réception température/humidité via DHT11
+    int chk = DHT.read11(dht_pin);
+
+    Serial.print("{\"function_name\":\"recv_dht\",\"parameters\":{\"temperature\":");
+    Serial.print(DHT.temperature);
+    Serial.print(", \"humidity\":");
+    Serial.print(DHT.humidity);
+    Serial.println("}}");
+  }
 
   if (recv433) {                                                   // Partie réception 433 MHz
     if (mySwitch.available()) {
@@ -226,22 +167,4 @@ void loop() {
       mySwitch.resetAvailable();
     }
   }
-
-  /*unsigned long sender = 0;
-
-    if (sender != 0) {
-    Serial.print("{\"action\":\"received\",\"value\":");
-    Serial.print(sender);
-    Serial.println("}");
-    delay(200);
-    }*/
-
-  /*                                                             // Fonction à appeler dans void loop() pour permettre la détection de signaux IR de la TV
-    if (irrecv_tv.decode(&results)) {
-      Serial.print("{\"action\":\"received\",\"value\":");
-      Serial.print(results.value, HEX);
-      Serial.println("}");
-      irrecv_tv.resume(); // Receive the next value
-      delay(100);
-    }*/
 }
