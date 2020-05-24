4c46df0008513586f3d616b03c64d3afb1340014
billonalex <billonalex21@gmail.com>
2020-05-24 02:04:25 +0200

    New test

--- server/services/arduino/lib/poll.js
+++ server/services/arduino/lib/poll.js
@@ -11,6 +11,12 @@ const { recv } = require('./recv');
 async function poll(device) {
   try {
     logger.warn("I'm actually polling !!!");
+
+    const arduino = await this.gladys.device.getBySelector(
+      device.params.find((param) => param.name === 'ARDUINO_LINKED').value
+    );
+    const arduinoPath = arduino.params.find((param) => param.name === 'ARDUINO_PATH').value;
+    logger.warn(arduinoPath);
     //recv(device);
   } catch (e) {
     logger.warn('Unable to poll device');
