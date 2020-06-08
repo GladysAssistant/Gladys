f7a4b0143bce7c691d7996dd24497be1b77bc5fc
billonalex <billonalex21@gmail.com>
2020-06-08 13:37:28 +0200

    [Fix] => Setup tests

--- server/services/arduino/lib/setup.js
+++ server/services/arduino/lib/setup.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const { getDeviceParam } = require('../../../utils/device');
-const logger = require('../../../utils/logger');
 
 /**
  * @description Setup the Arduino and copy the code in it.
@@ -21,15 +20,12 @@ async function setup(device) {
 
     avrgirl.flash(path.resolve(`services/arduino/arduino-code/`, `${model}/arduino-code.ino.hex`), (error) => {
       if (error) {
-        logger.warn(error);
         return new Error(error);
       }
       return null;
     });
     return { success: true };
   } catch (e) {
-    logger.warn('Unable to flash the card');
-    logger.debug(e);
     return { success: false };
   }
 }
