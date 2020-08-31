96dab20a7d74bcd5a3164536e583c3d9d7d32f8f
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2020-08-31 11:11:54 +0200

    Remove .only

--- server/test/lib/scene/scene.executeSingleAction.test.js
+++ server/test/lib/scene/scene.executeSingleAction.test.js
@@ -24,7 +24,7 @@ const switchDevice = {
   features: [deviceFeatureSwitchBinary],
 };
 
-describe.only('scene.executeSingleAction', () => {
+describe('scene.executeSingleAction', () => {
   it('should execute one action', async () => {
     const device = {
       setValue: fake.resolves(null),
