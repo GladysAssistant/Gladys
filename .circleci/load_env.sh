23bc7e571f37a43c6763a885f7f049b7c4000bb8
VonOx <vkulak62@gmail.com>
2020-08-31 08:39:59 +0200

    Update QEMU

--- .circleci/load_env.sh
+++ .circleci/load_env.sh
@@ -6,6 +6,6 @@ echo 'export REGISTRY=gladysassistant' >>$BASH_ENV
 
 echo 'export IMAGE_ID="${REGISTRY}/${IMAGE}:${VERSION}-${TAG}"' >>$BASH_ENV
 echo 'export DIR=`pwd`' >>$BASH_ENV
-echo 'export QEMU_VERSION="v4.2.0-2"' >>$BASH_ENV
+echo 'export QEMU_VERSION="v5.1.0-2"' >>$BASH_ENV
 
 . $BASH_ENV
