c3038120ca583bdde9bed10e12cd633262412737
Nicolas Geissel <nicolas.geissel@gmail.com>
2020-09-02 22:31:03 +0200

    Listen to MQTT to change user position

--- server/services/index.js
+++ server/services/index.js
@@ -6,6 +6,6 @@ module.exports['philips-hue'] = require('./philips-hue');
 module.exports['rtsp-camera'] = require('./rtsp-camera');
 module.exports.telegram = require('./telegram');
 module.exports.usb = require('./usb');
-module.exports.xiaomi = require('./xiaomi');
+//module.exports.xiaomi = require('./xiaomi');
 module.exports.zwave = require('./zwave');
 module.exports.tasmota = require('./tasmota');
