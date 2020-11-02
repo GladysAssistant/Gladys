1bca2cadb181d9d0d2510cd4b3d9d16a2f8d7fe8
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2020-11-02 11:35:26 +0200

    Improve getLatestGladysVersion

--- server/lib/gateway/gateway.getLatestGladysVersion.js
+++ server/lib/gateway/gateway.getLatestGladysVersion.js
@@ -1,3 +1,5 @@
+const db = require('../../models');
+
 /**
  * @description Return latest version of Gladys.
  * @returns {Promise} Resolve with latest version of Gladys.
@@ -7,11 +9,13 @@
 async function getLatestGladysVersion() {
   const systemInfos = await this.system.getInfos();
   const clientId = await this.variable.getValue('GLADYS_INSTANCE_CLIENT_ID');
+  const deviceStateCount = await db.DeviceFeatureState.count();
   const params = {
     system: systemInfos.platform,
     node_version: systemInfos.nodejs_version,
     is_docker: systemInfos.is_docker,
     client_id: clientId,
+    device_state_count: deviceStateCount,
   };
   const latestGladysVersion = await this.gladysGatewayClient.getLatestGladysVersion(systemInfos.gladys_version, params);
   this.system.saveLatestGladysVersion(latestGladysVersion.name);
