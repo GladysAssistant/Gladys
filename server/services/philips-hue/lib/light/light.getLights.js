bb92a34328a79ce272c88cfbf840a4dc144449fc
VonOx <vkulak62@gmail.com>
2020-11-18 18:00:55 +0100

    Add LCL001

--- server/services/philips-hue/lib/light/light.getLights.js
+++ server/services/philips-hue/lib/light/light.getLights.js
@@ -41,6 +41,7 @@ async function getLights() {
         case 'LLC020': // Hue go
         case 'LCT012': // Hue White and Color Ambiance Candle E12
         case 'LCT003': // Hue White and Color Ambiance Spot GU10
+        case 'LCL001': // Hue Lightstrip Plus
           lightsToReturn.push(getPhilipsHueColorTemperatureLight(philipsHueLight, serialNumber, this.serviceId));
           break;
         case 'LLC001': // LivingColor lamp 2nd generation
