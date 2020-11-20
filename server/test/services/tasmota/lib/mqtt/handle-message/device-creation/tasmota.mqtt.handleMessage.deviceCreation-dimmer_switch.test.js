fa909902a18efcee382db8ee1242f257f22349b2
Alexandre Trovato <1839717+atrovato@users.noreply.github.com>
2020-11-20 16:06:30 +0100

    Tasmota: Handle HTTP devices (#694)
    * Tasmota-HTTP: front
    
    * Tasmota-HTTP: rename MQTT specific
    
    * Tasmota-HTTP: add rest controller
    
    * Tasmota-HTTP: add to discovered devices
    
    * Tasmota HTTP: discover + setValue + poll
    
    * Tasmota HTTP: username - password
    
    * Tasmota: split http/mqtt protocols + rename files
    
    * Tasmota: fix lang
    
    * Tasmota: http url based on device externalid.
    
    * Tasmota: http front integration
    
    * Tasmota: replace interface by protocol
    
    * Tasmota: no MQTT check on device page
    
    Co-authored-by: Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>

---
+++ server/test/services/tasmota/lib/mqtt/handle-message/device-creation/tasmota.mqtt.handleMessage.deviceCreation-dimmer_switch.test.js
@@ -0,0 +1,152 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const { fake, assert } = sinon;
+const TasmotaHandler = require('../../../../../../../services/tasmota/lib');
+const {
+  DEVICE_FEATURE_CATEGORIES,
+  DEVICE_FEATURE_TYPES,
+  EVENTS,
+  WEBSOCKET_MESSAGE_TYPES,
+} = require('../../../../../../../utils/constants');
+
+const messages = require('../../../device-creation/dimmer_switch.json');
+
+const mqttService = {
+  device: {
+    publish: fake.returns(null),
+  },
+};
+const gladys = {
+  event: {
+    emit: fake.returns(null),
+  },
+  stateManager: {
+    get: fake.returns(null),
+  },
+};
+const serviceId = 'service-uuid-random';
+
+describe('Tasmota - MQTT - create device with switch Dimmer feature', () => {
+  const tasmota = new TasmotaHandler(gladys, serviceId);
+  const tasmotaHandler = tasmota.protocols.mqtt;
+  tasmotaHandler.mqttService = mqttService;
+
+  beforeEach(() => {
+    sinon.reset();
+  });
+
+  it('decode STATUS message', () => {
+    tasmotaHandler.handleMessage('stat/tasmota-device-topic/STATUS', JSON.stringify(messages.STATUS));
+
+    expect(tasmotaHandler.discoveredDevices).to.deep.eq({});
+    expect(tasmotaHandler.pendingDevices).to.deep.eq({
+      'tasmota-device-topic': {
+        name: 'Tasmota',
+        params: [
+          {
+            name: 'protocol',
+            value: 'mqtt',
+          },
+        ],
+        model: 1,
+        external_id: 'tasmota:tasmota-device-topic',
+        selector: 'tasmota-tasmota-device-topic',
+        service_id: serviceId,
+        should_poll: false,
+        features: [],
+      },
+    });
+
+    assert.notCalled(gladys.event.emit);
+    assert.notCalled(gladys.stateManager.get);
+    assert.calledWith(mqttService.device.publish, 'cmnd/tasmota-device-topic/STATUS', '11');
+  });
+
+  it('decode STATUS11 message', () => {
+    tasmotaHandler.handleMessage('stat/tasmota-device-topic/STATUS11', JSON.stringify(messages.STATUS11));
+
+    expect(tasmotaHandler.discoveredDevices).to.deep.eq({});
+    expect(tasmotaHandler.pendingDevices).to.deep.eq({
+      'tasmota-device-topic': {
+        name: 'Tasmota',
+        params: [
+          {
+            name: 'protocol',
+            value: 'mqtt',
+          },
+        ],
+        model: 1,
+        external_id: 'tasmota:tasmota-device-topic',
+        selector: 'tasmota-tasmota-device-topic',
+        service_id: serviceId,
+        should_poll: false,
+        features: [
+          {
+            category: DEVICE_FEATURE_CATEGORIES.SWITCH,
+            type: DEVICE_FEATURE_TYPES.SWITCH.DIMMER,
+            external_id: 'tasmota:tasmota-device-topic:Dimmer',
+            selector: 'tasmota-tasmota-device-topic-dimmer',
+            name: 'Dimmer',
+            read_only: false,
+            has_feedback: true,
+            min: 0,
+            max: 100,
+            last_value: 14,
+          },
+        ],
+      },
+    });
+
+    assert.calledWith(gladys.event.emit, EVENTS.DEVICE.NEW_STATE, {
+      device_feature_external_id: 'tasmota:tasmota-device-topic:Dimmer',
+      state: 14,
+    });
+    assert.notCalled(gladys.stateManager.get);
+    assert.calledWith(mqttService.device.publish, 'cmnd/tasmota-device-topic/STATUS', '8');
+  });
+
+  it('decode STATUS8 message', () => {
+    tasmotaHandler.handleMessage('stat/tasmota-device-topic/STATUS8', JSON.stringify(messages.STATUS8));
+
+    const expectedDevice = {
+      name: 'Tasmota',
+      params: [
+        {
+          name: 'protocol',
+          value: 'mqtt',
+        },
+      ],
+      model: 1,
+      external_id: 'tasmota:tasmota-device-topic',
+      selector: 'tasmota-tasmota-device-topic',
+      service_id: serviceId,
+      should_poll: false,
+      features: [
+        {
+          category: DEVICE_FEATURE_CATEGORIES.SWITCH,
+          type: DEVICE_FEATURE_TYPES.SWITCH.DIMMER,
+          external_id: 'tasmota:tasmota-device-topic:Dimmer',
+          selector: 'tasmota-tasmota-device-topic-dimmer',
+          name: 'Dimmer',
+          read_only: false,
+          has_feedback: true,
+          min: 0,
+          max: 100,
+          last_value: 14,
+        },
+      ],
+    };
+    expect(tasmotaHandler.discoveredDevices).to.deep.eq({
+      'tasmota-device-topic': expectedDevice,
+    });
+    expect(tasmotaHandler.pendingDevices).to.deep.eq({});
+
+    assert.notCalled(mqttService.device.publish);
+    assert.calledWith(gladys.stateManager.get, 'deviceByExternalId', 'tasmota:tasmota-device-topic');
+    assert.calledWith(gladys.event.emit, EVENTS.WEBSOCKET.SEND_ALL, {
+      type: WEBSOCKET_MESSAGE_TYPES.TASMOTA.NEW_MQTT_DEVICE,
+      payload: expectedDevice,
+    });
+  });
+});
