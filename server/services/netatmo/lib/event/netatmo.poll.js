143d26e60ba7335c870e601bef78ed342088118d
damalgos <thibaut.mery@gmail.com>
2020-12-08 13:09:47 +0100

    Update station data

--- server/services/netatmo/lib/event/netatmo.poll.js
+++ server/services/netatmo/lib/event/netatmo.poll.js
@@ -17,19 +17,39 @@ const MAX_VOLT = 3300;
 async function poll(device) {
   sid = device.external_id.split('netatmo:')[1];
   this.getDevices()
-  sensor = this.devices[sid]
-  this.gladys.event.emit(EVENTS.DEVICE.NEW_STATE, {
-    device_feature_external_id: `netatmo:${sid}:battery`,
-    state: this.devices[sid].battery_percent
-  });
-  this.gladys.event.emit(EVENTS.DEVICE.NEW_STATE, {
-    device_feature_external_id: `netatmo:${sid}:temperature`,
-    state: this.devices[sid].measured.temperature
-  });
-  this.gladys.event.emit(EVENTS.DEVICE.NEW_STATE, {
-    device_feature_external_id: `netatmo:${sid}:setpoint`,
-    state: this.devices[sid].measured.setpoint_temp
-  });
+  if (this.devices[sid].type == 'NATherm1') {
+    this.gladys.event.emit(EVENTS.DEVICE.NEW_STATE, {
+      device_feature_external_id: `netatmo:${sid}:battery`,
+      state: this.devices[sid].battery_percent
+    });
+    this.gladys.event.emit(EVENTS.DEVICE.NEW_STATE, {
+      device_feature_external_id: `netatmo:${sid}:temperature`,
+      state: this.devices[sid].measured.temperature
+    });
+    this.gladys.event.emit(EVENTS.DEVICE.NEW_STATE, {
+      device_feature_external_id: `netatmo:${sid}:setpoint`,
+      state: this.devices[sid].measured.setpoint_temp
+    });
+  }
+  if (this.devices[sid].type == 'NAMain') {
+    this.gladys.event.emit(EVENTS.DEVICE.NEW_STATE, {
+      device_feature_external_id: `netatmo:${sid}:temperature`,
+      state: this.devices[sid].dashboard_data.Temperature
+    });
+    this.gladys.event.emit(EVENTS.DEVICE.NEW_STATE, {
+      device_feature_external_id: `netatmo:${sid}:humidity`,
+      state: this.devices[sid].dashboard_data.Humidity
+    });
+    this.gladys.event.emit(EVENTS.DEVICE.NEW_STATE, {
+      device_feature_external_id: `netatmo:${sid}:co2`,
+      state: this.devices[sid].dashboard_data.CO2
+    });
+    this.gladys.event.emit(EVENTS.DEVICE.NEW_STATE, {
+      device_feature_external_id: `netatmo:${sid}:pressure`,
+      state: this.devices[sid].dashboard_data.Pressure
+    });
+  }
+
 }
 
 module.exports = {
