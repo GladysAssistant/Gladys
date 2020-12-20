9a11b36f03d58141dd8cd2f603391d01c7f9c460
atrovato <1839717+atrovato@users.noreply.github.com>
2020-12-20 11:43:24 +0100

    Stopped service only on user request

--- server/migrations/20201220100508-service-remove-not-configured-status.js
+++ server/migrations/20201220100508-service-remove-not-configured-status.js
@@ -1,6 +1,8 @@
 module.exports = {
   up: async (queryInterface) => {
-    await queryInterface.sequelize.query("UPDATE t_service SET status='RUNNING' WHERE status='NOT_CONFIGURED'");
+    await queryInterface.sequelize.query(
+      "UPDATE t_service SET status='RUNNING' WHERE status='NOT_CONFIGURED' OR status='STOPPED'",
+    );
   },
   down: async () => {},
 };
