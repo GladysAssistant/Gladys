8c2f563b2856165d5fb14bdda1c82ade80c739d7
atrovato <1839717+atrovato@users.noreply.github.com>
2020-12-20 11:08:08 +0100

    Remove NOT_CONFIGURED service status

---
+++ server/migrations/20201220100508-service-remove-not-configured-status.js
@@ -0,0 +1,6 @@
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.sequelize.query("UPDATE t_service SET status='RUNNING' WHERE status='NOT_CONFIGURED'");
+  },
+  down: async () => {},
+};
