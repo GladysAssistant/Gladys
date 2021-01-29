4f7cc87b2e78624f6df173e7e3e1b934295a424d
Terdious <thomas.lemaistre76@gmail.com>
2021-01-29 13:39:54 +0100

    Forgot modification of config file co file

--- front/config.js
+++ front/config.js
@@ -1,7 +1,7 @@
 const local = {
   gladysGatewayApiUrl: process.env.GLADYS_GATEWAY_API_URL || 'https://api.gladysgateway.com',
-  localApiUrl: process.env.LOCAL_API_URL || 'http://172.23.178.137:1443',
-  webSocketUrl: process.env.WEBSOCKET_URL || 'ws://172.23.178.137:1443',
+  localApiUrl: process.env.LOCAL_API_URL || 'http://localhost:1443',
+  webSocketUrl: process.env.WEBSOCKET_URL || 'ws://localhost:1443',
   gatewayMode: process.env.GATEWAY_MODE === 'true',
   demoMode: process.env.DEMO_MODE === 'true'
 };
