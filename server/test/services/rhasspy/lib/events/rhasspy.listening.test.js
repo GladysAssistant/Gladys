e99185198dde232295939c567e027a29164b35be
damalgos <thibaut.mery@gmail.com>
2021-02-08 17:46:19 +0100

    Update

---
+++ server/test/services/rhasspy/lib/events/rhasspy.listening.test.js
@@ -0,0 +1,21 @@
+// const nock = require('nock');
+// const { fake, stub } = require('sinon');
+// const proxyquire = require('proxyquire').noCallThru();
+// const sinon = require('sinon');
+// const chai = require('chai');
+//
+// const RhasspyManager = proxyquire('../../../../../services/rhasspy/lib/index', {
+//   'ws':
+// });
+//
+// const gladys = {
+//   event: {
+//     emit: fake.returns(null),
+//   },
+// };
+//
+// describe('rhasspyManager listening events', () => {
+//   it('Should receive message', async () => {
+//     Shou
+//   });
+// })
