d159785809cbda30183c600cf4d843559a1cb301
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2021-02-25 10:23:15 +0100

    Use upnp search in case nupnp search fail

--- server/services/philips-hue/lib/light/light.getBridges.js
+++ server/services/philips-hue/lib/light/light.getBridges.js
@@ -1,12 +1,21 @@
 const logger = require('../../../../utils/logger');
 
+const TIMEOUT = 5000;
+
 /**
  * @description Return Philips hue bridges
  * @example
  * getBridges();
  */
 async function getBridges() {
-  this.bridges = await this.hueClient.discovery.nupnpSearch();
+  // first, we try a fast nupnpsearch
+  const bridges = await this.hueClient.discovery.nupnpSearch();
+  // if we don't find any bridges, we try the other way
+  if (bridges.length === 0) {
+    this.bridges = await this.hueClient.discovery.upnpSearch(TIMEOUT);
+  } else {
+    this.bridges = bridges;
+  }
   logger.info(`PhilipsHueService: Found ${this.bridges.length} bridges`);
   this.bridges.forEach((bridge) => {
     this.bridgesBySerialNumber.set(bridge.model.serial, bridge);
--- server/test/services/philips-hue/light/light.getBridges.test.js
+++ server/test/services/philips-hue/light/light.getBridges.test.js
@@ -1,4 +1,5 @@
 const { expect } = require('chai');
+const { fake, assert } = require('sinon');
 const proxyquire = require('proxyquire').noCallThru();
 const { MockedPhilipsHueClient } = require('../mocks.test');
 
@@ -19,5 +20,30 @@ describe('PhilipsHueService', () => {
         },
       },
     ]);
+  });
+  it('getBridges should return bridges with upnp search', async () => {
+    const philipsHueService = PhilipsHueService();
+    philipsHueService.device.hueClient.discovery.nupnpSearch = fake.resolves([]);
+    philipsHueService.device.hueClient.discovery.upnpSearch = fake.resolves([
+      {
+        name: 'Philips Hue Bridge',
+        ipaddress: '192.168.2.245',
+        model: {
+          serial: 'FOUND_BY_UPNP_SEARCH',
+        },
+      },
+    ]);
+    const bridges = await philipsHueService.device.getBridges();
+    expect(bridges).to.deep.equal([
+      {
+        name: 'Philips Hue Bridge',
+        ipaddress: '192.168.2.245',
+        model: {
+          serial: 'FOUND_BY_UPNP_SEARCH',
+        },
+      },
+    ]);
+    assert.calledOnce(philipsHueService.device.hueClient.discovery.nupnpSearch);
+    assert.calledOnce(philipsHueService.device.hueClient.discovery.upnpSearch);
   });
 });
