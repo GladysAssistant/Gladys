43a70ddb59e5946e8db560de30f1e838911ecde7
Corentin Allemand <corentin.allemand@rakuten.com>
2021-03-16 20:17:50 +0100

    add assert on test

--- server/test/lib/scene/scene.execute.test.js
+++ server/test/lib/scene/scene.execute.test.js
@@ -1,9 +1,11 @@
 const { assert, fake } = require('sinon');
 const { expect } = require('chai');
+
 const EventEmitter = require('events');
 const { ACTIONS } = require('../../../utils/constants');
 const SceneManager = require('../../../lib/scene');
 const StateManager = require('../../../lib/state');
+
 
 const event = new EventEmitter();
 
@@ -77,6 +79,7 @@ describe('SceneManager', () => {
     const device = {
       setValue: fake.resolves(null),
     };
+
     const sceneManager = new SceneManager(stateManager, event, device);
     const scene = {
       selector: 'my-bad-scene',
@@ -93,5 +96,16 @@ describe('SceneManager', () => {
     };
     sceneManager.addScene(scene);
     await sceneManager.execute('my-bad-scene');
+    expect(sceneManager.queue.length).to.equal(1);
+    return new Promise((resolve, reject) => {
+      sceneManager.queue.start(() => {
+        try {
+          assert.callCount(device.setValue, 0);
+          resolve();
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
   });
 });
