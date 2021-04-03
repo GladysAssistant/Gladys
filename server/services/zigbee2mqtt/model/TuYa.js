38014752ff94a2d4bcb9b98e10e82c2b668846bc
VonOx <vkulak62@gmail.com>
2021-04-04 00:07:46 +0200

    feat(zigbee2mqtt): Handle TuYa TT001ZAV20

--- server/services/zigbee2mqtt/model/TuYa.js
+++ server/services/zigbee2mqtt/model/TuYa.js
@@ -8,6 +8,7 @@ const TuYa = {
   models: {
     TS0121_plug: [features.switch, features.power, features.current, features.voltage, features.energy],
     TS0011: [features.switch],
+    TT001ZAV20: [features.temperature, features.humidity],
   },
 };
 
