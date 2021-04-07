5720b6ac1300ecab41873197b07b07f9b1a13642
VonOx <vkulak62@gmail.com>
2021-04-07 15:10:29 +0200

    chore(pihole): Remove unused import

--- server/test/services/pihole/index.test.js
+++ server/test/services/pihole/index.test.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 
-const { assert, fake } = sinon;
+const { fake } = sinon;
 const PiholeService = require('../../../services/pihole/index');
 
 const FakePiholeSummary = require('./piholeSummary.json');
