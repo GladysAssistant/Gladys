ff7919c3321e23d4c7ddccf6eb8e19933bf350d8
bulolo <82607314@qq.com>
2021-04-16 15:11:34 +0800

    Update index.js

--- front/src/config/i18n/index.js
+++ front/src/config/i18n/index.js
@@ -1,5 +1,6 @@
 import en from './en.json';
 import fr from './fr.json';
+import cn from './cn.json';
 import { AVAILABLE_LANGUAGES } from '../../../../server/utils/constants';
 
-export default { [AVAILABLE_LANGUAGES.FR]: fr, [AVAILABLE_LANGUAGES.EN]: en };
+export default { [AVAILABLE_LANGUAGES.FR]: fr, [AVAILABLE_LANGUAGES.EN]: en, [AVAILABLE_LANGUAGES.CN]: cn };
