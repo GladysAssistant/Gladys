889eabfb640c63239d719cc8dafa414a329e964e
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2021-04-22 11:35:59 +0800

    Add more tests to scene.execute

--- server/test/lib/scene/scene.execute.test.js
+++ server/test/lib/scene/scene.execute.test.js
@@ -1,5 +1,6 @@
 const { assert, fake } = require('sinon');
 const EventEmitter = require('events');
+const { expect } = require('chai');
 const { ACTIONS } = require('../../../utils/constants');
 const SceneManager = require('../../../lib/scene');
 const StateManager = require('../../../lib/state');
@@ -39,6 +40,88 @@ describe('SceneManager', () => {
         } catch (e) {
           reject(e);
         }
+      });
+    });
+  });
+  it('should execute one scene and abort scene', async () => {
+    const stateManager = new StateManager(event);
+    stateManager.setState('deviceFeature', 'my-device-feature', {
+      category: 'light',
+      type: 'binary',
+      last_value: 15,
+    });
+    const device = {
+      setValue: fake.resolves(null),
+    };
+    const sceneManager = new SceneManager(stateManager, event, device);
+    const scene = {
+      selector: 'my-scene',
+      triggers: [],
+      actions: [
+        [
+          {
+            type: ACTIONS.DEVICE.GET_VALUE,
+            device_feature: 'my-device-feature',
+          },
+        ],
+        [
+          {
+            type: ACTIONS.CONDITION.ONLY_CONTINUE_IF,
+            conditions: [
+              {
+                variable: '0.0.last_value',
+                operator: '=',
+                value: 20,
+              },
+            ],
+          },
+        ],
+      ],
+    };
+    sceneManager.addScene(scene);
+    const scope = {};
+    await sceneManager.execute('my-scene', scope);
+    return new Promise((resolve, reject) => {
+      sceneManager.queue.start(() => {
+        try {
+          assert.notCalled(device.setValue);
+          expect(scope).to.deep.equal({ '0': { '0': { category: 'light', type: 'binary', last_value: 15 } } });
+          resolve();
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  });
+  it('should execute one scene, crash but not crash here', async () => {
+    const stateManager = new StateManager(event);
+    stateManager.setState('deviceFeature', 'my-device-feature', {
+      category: 'light',
+      type: 'binary',
+      last_value: 15,
+    });
+    const device = {
+      setValue: fake.resolves(null),
+    };
+    const sceneManager = new SceneManager(stateManager, event, device);
+    const scene = {
+      selector: 'my-scene',
+      triggers: [],
+      actions: [
+        [
+          {
+            type: 'this type does not exist',
+            device_feature: 'my-device-feature',
+          },
+        ],
+      ],
+    };
+    sceneManager.addScene(scene);
+    const scope = {};
+    await sceneManager.execute('my-scene', scope);
+    return new Promise((resolve, reject) => {
+      sceneManager.queue.start(() => {
+        resolve();
       });
     });
   });
