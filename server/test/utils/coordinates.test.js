b7b42b8e57db45a8a1b2e86dd0979a6264a7a367
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2021-06-11 09:50:00 +0800

    Area distance calculation should be in meter

--- server/test/utils/coordinates.test.js
+++ server/test/utils/coordinates.test.js
@@ -9,7 +9,7 @@ describe('calculateDistanceBetweenTwoPoints', () => {
       48.10743118848039,
       4.203229745748899,
     );
-    expect(value).to.equal(563);
+    expect(value).to.equal(562918);
   });
   it('should calculate distance between 2 points', () => {
     const value = calculateDistanceBetweenTwoPoints(
@@ -18,7 +18,7 @@ describe('calculateDistanceBetweenTwoPoints', () => {
       -41.66581108420415,
       -95.65893074555942,
     );
-    expect(value).to.equal(17371);
+    expect(value).to.equal(17370593);
   });
   it('should calculate distance between same point', () => {
     const value = calculateDistanceBetweenTwoPoints(
