5f8af19ca2d24d16dbf2a13726e2d0e882a362e5
atrovato <1839717+atrovato@users.noreply.github.com>
2021-07-26 22:45:41 +0200

    Cypress coverage

--- front/cypress/plugins/index.js
+++ front/cypress/plugins/index.js
@@ -19,6 +19,10 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 
+  // Code coverag
+  require('@cypress/code-coverage/task')(on, config);
+  on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'));
+
   // Load NODE env variables
   const language = process.env.LANGUAGE || 'en';
   config.env.language = language;
--- front/cypress/support/index.js
+++ front/cypress/support/index.js
@@ -16,5 +16,8 @@
 // Import commands.js using ES2015 syntax:
 import './commands';
 
+// Cypress code coverage
+import '@cypress/code-coverage/support';
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
