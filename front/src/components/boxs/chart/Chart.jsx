4b4723fbad961dca2c6620a54af287b0c243812e
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2021-07-30 16:33:13 +0800

    Improve loading & empty state

--- front/src/components/boxs/chart/Chart.jsx
+++ front/src/components/boxs/chart/Chart.jsx
@@ -116,153 +116,155 @@ class Chartbox extends Component {
     const smallBox = interval === ONE_HOUR_IN_MINUTES;
     return (
       <div class="card">
+        <div class="card-body">
+          <div class="d-flex align-items-center">
+            <div
+              class={cx({
+                [style.subheader]: smallBox,
+                [style.subheaderBig]: !smallBox
+              })}
+            >
+              {props.box.title}
+            </div>
+            <div class={cx(style.msAuto, style.lh1)}>
+              <div class="dropdown">
+                <a class="dropdown-toggle text-muted" onClick={this.toggleDropdown}>
+                  {interval === ONE_HOUR_IN_MINUTES && <Text id="dashboard.boxes.chart.lastHour" />}
+                  {interval === ONE_DAY_IN_MINUTES && <Text id="dashboard.boxes.chart.lastDay" />}
+                  {interval === SEVEN_DAYS_IN_MINUTES && <Text id="dashboard.boxes.chart.lastSevenDays" />}
+                  {interval === THIRTY_DAYS_IN_MINUTES && <Text id="dashboard.boxes.chart.lastThirtyDays" />}
+                  {interval === THREE_MONTHS_IN_MINUTES && <Text id="dashboard.boxes.chart.lastThreeMonths" />}
+                </a>
+                <div
+                  class={cx(style.dropdownMenuChart, {
+                    [style.show]: dropdown
+                  })}
+                >
+                  <a
+                    class={cx(style.dropdownItemChart, {
+                      [style.active]: interval === ONE_HOUR_IN_MINUTES
+                    })}
+                    onClick={this.switchToLastHourView}
+                  >
+                    <Text id="dashboard.boxes.chart.lastHour" />
+                  </a>
+                  <a
+                    class={cx(style.dropdownItemChart, {
+                      [style.active]: interval === ONE_DAY_IN_MINUTES
+                    })}
+                    onClick={this.switchToOneDayView}
+                  >
+                    <Text id="dashboard.boxes.chart.lastDay" />
+                  </a>
+                  <a
+                    class={cx(style.dropdownItemChart, {
+                      [style.active]: interval === SEVEN_DAYS_IN_MINUTES
+                    })}
+                    onClick={this.switchTo7DaysView}
+                  >
+                    <Text id="dashboard.boxes.chart.lastSevenDays" />
+                  </a>
+                  <a
+                    class={cx(style.dropdownItemChart, {
+                      [style.active]: interval === THIRTY_DAYS_IN_MINUTES
+                    })}
+                    onClick={this.switchTo30DaysView}
+                  >
+                    <Text id="dashboard.boxes.chart.lastThirtyDays" />
+                  </a>
+                  <a
+                    class={cx(style.dropdownItemChart, {
+                      [style.active]: interval === THREE_MONTHS_IN_MINUTES
+                    })}
+                    onClick={this.switchTo3monthsView}
+                  >
+                    <Text id="dashboard.boxes.chart.lastThreeMonths" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          {smallBox && (
+            <div class="d-flex align-items-baseline">
+              {lastValueRounded && (
+                <div class="h1 mb-0 mr-2">
+                  {lastValueRounded}
+                  {props.box.unit !== undefined && <Text id={`deviceFeatureUnitShort.${props.box.unit}`} />}
+                </div>
+              )}
+              <div
+                class={cx(style.meAuto, {
+                  [style.textGreen]: variation > 0,
+                  [style.textYellow]: variation === 0,
+                  [style.textRed]: variation < 0
+                })}
+              >
+                {labels && labels.length > 0 && (
+                  <span class="text-green d-inline-flex align-items-center lh-1">
+                    {variation}%
+                    {variation > 0 && (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class={cx(style.variationIcon)}
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        stroke-width="2"
+                        stroke="currentColor"
+                        fill="none"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <polyline points="3 17 9 11 13 15 21 7" />
+                        <polyline points="14 7 21 7 21 14" />
+                      </svg>
+                    )}
+                    {variation === 0 && (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class={cx(style.variationIcon)}
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        stroke-width="2"
+                        stroke="currentColor"
+                        fill="none"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <line x1="5" y1="12" x2="19" y2="12" />
+                      </svg>
+                    )}
+                    {variation < 0 && (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class={cx(style.variationIcon)}
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        stroke-width="2"
+                        stroke="currentColor"
+                        fill="none"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <polyline points="3 7 9 13 13 9 21 17" />
+                        <polyline points="21 10 21 17 14 17" />
+                      </svg>
+                    )}
+                  </span>
+                )}
+              </div>
+            </div>
+          )}
+        </div>
+
         <div class={loading ? 'dimmer active' : 'dimmer'}>
           <div class="loader" />
           <div class="dimmer-content">
-            <div class="card-body">
-              <div class="d-flex align-items-center">
-                <div
-                  class={cx({
-                    [style.subheader]: smallBox,
-                    [style.subheaderBig]: !smallBox
-                  })}
-                >
-                  {props.box.title}
-                </div>
-                <div class={cx(style.msAuto, style.lh1)}>
-                  <div class="dropdown">
-                    <a class="dropdown-toggle text-muted" onClick={this.toggleDropdown}>
-                      {interval === ONE_HOUR_IN_MINUTES && <Text id="dashboard.boxes.chart.lastHour" />}
-                      {interval === ONE_DAY_IN_MINUTES && <Text id="dashboard.boxes.chart.lastDay" />}
-                      {interval === SEVEN_DAYS_IN_MINUTES && <Text id="dashboard.boxes.chart.lastSevenDays" />}
-                      {interval === THIRTY_DAYS_IN_MINUTES && <Text id="dashboard.boxes.chart.lastThirtyDays" />}
-                      {interval === THREE_MONTHS_IN_MINUTES && <Text id="dashboard.boxes.chart.lastThreeMonths" />}
-                    </a>
-                    <div
-                      class={cx(style.dropdownMenuChart, {
-                        [style.show]: dropdown
-                      })}
-                    >
-                      <a
-                        class={cx(style.dropdownItemChart, {
-                          [style.active]: interval === ONE_HOUR_IN_MINUTES
-                        })}
-                        onClick={this.switchToLastHourView}
-                      >
-                        <Text id="dashboard.boxes.chart.lastHour" />
-                      </a>
-                      <a
-                        class={cx(style.dropdownItemChart, {
-                          [style.active]: interval === ONE_DAY_IN_MINUTES
-                        })}
-                        onClick={this.switchToOneDayView}
-                      >
-                        <Text id="dashboard.boxes.chart.lastDay" />
-                      </a>
-                      <a
-                        class={cx(style.dropdownItemChart, {
-                          [style.active]: interval === SEVEN_DAYS_IN_MINUTES
-                        })}
-                        onClick={this.switchTo7DaysView}
-                      >
-                        <Text id="dashboard.boxes.chart.lastSevenDays" />
-                      </a>
-                      <a
-                        class={cx(style.dropdownItemChart, {
-                          [style.active]: interval === THIRTY_DAYS_IN_MINUTES
-                        })}
-                        onClick={this.switchTo30DaysView}
-                      >
-                        <Text id="dashboard.boxes.chart.lastThirtyDays" />
-                      </a>
-                      <a
-                        class={cx(style.dropdownItemChart, {
-                          [style.active]: interval === THREE_MONTHS_IN_MINUTES
-                        })}
-                        onClick={this.switchTo3monthsView}
-                      >
-                        <Text id="dashboard.boxes.chart.lastThreeMonths" />
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              {smallBox && (
-                <div class="d-flex align-items-baseline">
-                  <div class="h1 mb-0 mr-2">
-                    {lastValueRounded}
-                    {props.box.unit !== undefined && <Text id={`deviceFeatureUnitShort.${props.box.unit}`} />}
-                  </div>
-                  <div
-                    class={cx(style.meAuto, {
-                      [style.textGreen]: variation > 0,
-                      [style.textYellow]: variation === 0,
-                      [style.textRed]: variation < 0
-                    })}
-                  >
-                    {labels && labels.length > 0 && (
-                      <span class="text-green d-inline-flex align-items-center lh-1">
-                        {variation}%
-                        {variation > 0 && (
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class={cx(style.variationIcon)}
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            stroke-width="2"
-                            stroke="currentColor"
-                            fill="none"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                          >
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <polyline points="3 17 9 11 13 15 21 7" />
-                            <polyline points="14 7 21 7 21 14" />
-                          </svg>
-                        )}
-                        {variation === 0 && (
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class={cx(style.variationIcon)}
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            stroke-width="2"
-                            stroke="currentColor"
-                            fill="none"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                          >
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <line x1="5" y1="12" x2="19" y2="12" />
-                          </svg>
-                        )}
-                        {variation < 0 && (
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class={cx(style.variationIcon)}
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            stroke-width="2"
-                            stroke="currentColor"
-                            fill="none"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                          >
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <polyline points="3 7 9 13 13 9 21 17" />
-                            <polyline points="21 10 21 17 14 17" />
-                          </svg>
-                        )}
-                      </span>
-                    )}
-                  </div>
-                </div>
-              )}
-            </div>
-
             {labels && labels.length === 0 && (
               <div
                 class={cx('text-center', {
