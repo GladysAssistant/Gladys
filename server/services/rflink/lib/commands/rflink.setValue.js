69aee96c0d9e6ea07ac63c8d975d6c192522f5b0
Nicolas Geissel <nicolas@geissel.fr>
2021-08-04 19:18:57 +0200

    Correct eslint

--- server/services/rflink/lib/commands/rflink.setValue.js
+++ server/services/rflink/lib/commands/rflink.setValue.js
@@ -1,59 +1,69 @@
+const c2xterm = require('color2xterm');
 const ObjToRF = require('../../api/rflink.parse.ObjToRF');
 const { DEVICE_FEATURE_CATEGORIES } = require('../../../../utils/constants');
-var namer = require('color-namer');
 const logger = require('../../../../utils/logger');
 const { intToHex } = require('../../../../utils/colors');
-const c2xterm = require('color2xterm');
+
 
 /**
- * @param {number}
- *          n - Number between 0 and 360 (not set if undefined)
+ * @description Convert an integer to an 8 bits hexa code.
+ * @param {string} value - A number.
+ * @returns {string} - The converted hexa code.
+ * @example
+ * to8bitsHex(15022);
  */
-const tohex = function(n) {
-	// really sleazy...
-	var s = ""
-	for ( ; n >= 0; n /= 16) {
-		rem = n % 16
-		n -= rem
-		if (rem == 15)
-			s = "F" + s
-		else if (rem == 14)
-			s = "E" + s
-		else if (rem == 13)
-			s = "D" + s
-		else if (rem == 12)
-			s = "C" + s
-		else if (rem == 11)
-			s = "B" + s
-		else if (rem == 10)
-			s = "A" + s
-		else
-			s = rem + s
-		if (n == 0)
-			break
-	}
-	return s
+function to8bitsHex(value) {
+  // really sleazy...
+  let s = '';
+  let n = value;
+  for (; n >= 0; n /= 16) {
+    const rem = n % 16;
+    n -= rem;
+    if (rem === 15) {
+      s = `F${s}`;
+    } else if (rem === 14) {
+      s = `E${s}`;
+    } else if (rem === 13) {
+      s = `D${s}`;
+    } else if (rem === 12) {
+      s = `C${s}`;
+    } else if (rem === 11) {
+      s = `B${s}`;
+    } else if (rem === 10) {
+      s = `A${s}`;
+    } else {
+      s = `${rem}${s}`;
+    }
+    if (n === 0) {
+      break;
+    }
+  }
+  return s;
 };
 
 /**
- * @param {number}
- *          value - Value of the color picker
+ * @description Convert an integer from color picker to an 8 bits hexa color code.
+ * @param {string} value - A number.
+ * @returns {string} - The converted hexa code.
+ * @example
+ * intTo8bitsColorHex(15022);
  */
-const intTo8bitsColorHex = function(value) {
+function intTo8bitsColorHex(value) {
   const hex = intToHex(value);
   // Find closest 8bit color code
   const colorCode = c2xterm.hex2xterm(hex);
-  logger.debug(`Nearest 8bits color code ${colorCode}`);
-  return tohex(colorCode);
+  return to8bitsHex(colorCode);
 };
 
+
 /**
- * @description send a message to change a device's value
+ * @description Send a message to change a device's value.
  * @param {Object} device - The device to control.
  * @param {Object} deviceFeature - The name of feature to control.
  * @param {string} state - The new state.
+ * @returns {string} - The message send to RFLink gateway.
  * @example
- * rflink.setValue();
+ * rflink.setValue(device, deviceFeature, state);
  */
 function setValue(device, deviceFeature, state) {
   let msg;
@@ -70,7 +80,6 @@ function setValue(device, deviceFeature, state) {
         } else if (deviceFeature.category === DEVICE_FEATURE_CATEGORIES.BUTTON) {
           value = 'DOWN';
         }
-
         break;
       case 1:
       case true:
@@ -79,7 +88,6 @@ function setValue(device, deviceFeature, state) {
         } else if (deviceFeature.category === DEVICE_FEATURE_CATEGORIES.BUTTON) {
           value = 'UP';
         }
-
         break;
       default:
         value = state;
@@ -97,9 +105,9 @@ function setValue(device, deviceFeature, state) {
       msg = `10;MiLightv1;${id};${channel};${color}98;COLOR;\n`;
     } else if (feature === 'brightness') {
       const featureIndex = device.features.findIndex((f) => f.type === 'color');
-      const last_color_value = intTo8bitsColorHex(device.features[featureIndex].last_value);
-      const hex = tohex(Math.round( (value*232)/100) );
-      msg = `10;MiLightv1;${id};${channel};${last_color_value}${hex};BRIGHT;\n`;
+      const lastColorValue = intTo8bitsColorHex(device.features[featureIndex].last_value);
+      const hex = to8bitsHex(Math.round( (value*232)/100) );
+      msg = `10;MiLightv1;${id};${channel};${lastColorValue}${hex};BRIGHT;\n`;
     } else if (feature === 'power') {
       switch (state) {
         case 0:
@@ -122,6 +130,7 @@ function setValue(device, deviceFeature, state) {
   }
   logger.debug(`Message send to USB : "${msg}"`);
   this.sendUsb.write(msg);
+  return msg;
 }
 
 module.exports = {
