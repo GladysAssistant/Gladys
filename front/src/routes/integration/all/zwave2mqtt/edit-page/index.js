9555139cdab3858e30a745a67bf786d3cda72694
POCHET Romuald <pochet.romuald@gmail.com>
2021-08-24 15:07:35 +0200

    Edit category

--- front/src/routes/integration/all/zwave2mqtt/edit-page/index.js
+++ front/src/routes/integration/all/zwave2mqtt/edit-page/index.js
@@ -5,11 +5,17 @@ import UpdateDevice from '../../../../../components/device';
 
 @connect('user,session,httpClient,currentIntegration,houses', {})
 class EditZwave2mqttDevice extends Component {
+
+  canEditCategory = (device, feature) => {
+    return true;
+  };
+
   render(props, {}) {
     return (
       <Zwave2mqttPage user={props.user}>
         <UpdateDevice
           {...props}
+          canEditCategory={this.canEditCategory}
           integrationName="zwave2mqtt"
           allowModifyFeatures={false}
           previousPage="/dashboard/integration/device/zwave2mqtt"
