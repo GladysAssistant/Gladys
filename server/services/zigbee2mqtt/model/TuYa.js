6256be7dbada71b1b90610fcc9fc6beabe9837af
Cyril Beslay <cicoub13@gmail.com>
2021-09-10 11:50:03 +0200

    fix(zigbee2mqtt): Add Tuya TS0201

--- server/services/zigbee2mqtt/model/TuYa.js
+++ server/services/zigbee2mqtt/model/TuYa.js
@@ -7,6 +7,7 @@ const TuYa = {
   brand: 'TuYa',
   models: {
     TS0121_plug: [features.switch, features.power, features.current, features.voltage, features.energy],
+    TS0201: [features.temperature, features.humidity, features.voltage],
     TS0011: [features.switch],
     TT001ZAV20: [features.temperature, features.humidity],
     SNTZ007: [features.door],
