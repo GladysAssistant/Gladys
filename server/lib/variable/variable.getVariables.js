8e94de8d851c2d150c7096dcd81ba9f970864363
bertrandda <bertrandda@users.noreply.github.com>
2021-12-09 09:11:11 +0100

    Improve code coverage

--- server/lib/variable/variable.getVariables.js
+++ server/lib/variable/variable.getVariables.js
@@ -4,15 +4,13 @@ const db = require('../../models');
  * @description Get all variables matching with key
  * @param {string} key - The unique key of the variable.
  * @param {string} [serviceId] - The unique ID of a service, or null.
- * @param {string} [userId] - The unique ID of a user, or null.
  * @example
  * gladys.user.getVariables();
  */
-async function getVariables(key, serviceId = null, userId = null) {
+async function getVariables(key, serviceId = null) {
   const where = {
     name: key,
     service_id: serviceId,
-    ...(userId && { user_id: userId }),
   };
 
   const variables = await db.Variable.findAll({
--- server/test/services/nextcloud-talk/index.test.js
+++ server/test/services/nextcloud-talk/index.test.js
@@ -1,17 +1,32 @@
+const { expect } = require('chai');
 const NextcloudTalkService = require('../../../services/nextcloud-talk');
 
-const gladys = {
-  variable: {
-    getVariables: () => Promise.resolve([{ user_id: 'f0de00a8-8ba7-4a4e-8f5a-7a21e94f36a8', value: 'testToken' }]),
-  },
-};
-
 describe('nextcloud-talk', () => {
-  const nextcloudTalkService = NextcloudTalkService(gladys, 'a4a59a7d-0001-4958-a794-37e38790142f');
   it('should start service', async () => {
+    const gladys = {
+      variable: {
+        getVariables: () => Promise.resolve([{ user_id: 'f0de00a8-8ba7-4a4e-8f5a-7a21e94f36a8', value: 'testToken' }]),
+      },
+    };
+    const nextcloudTalkService = NextcloudTalkService(gladys, 'a4a59a7d-0001-4958-a794-37e38790142f');
     await nextcloudTalkService.start();
   });
+  it('should failed to start service', async () => {
+    const gladys = {
+      variable: {
+        getVariables: () => Promise.resolve([]),
+      },
+    };
+    const nextcloudTalkService = NextcloudTalkService(gladys, 'a4a59a7d-0001-4958-a794-37e38790142f');
+    await expect(nextcloudTalkService.start()).to.be.rejectedWith(Error);
+  });
   it('should stop service', async () => {
+    const gladys = {
+      variable: {
+        getVariables: () => Promise.resolve([{ user_id: 'f0de00a8-8ba7-4a4e-8f5a-7a21e94f36a8', value: 'testToken' }]),
+      },
+    };
+    const nextcloudTalkService = NextcloudTalkService(gladys, 'a4a59a7d-0001-4958-a794-37e38790142f');
     await nextcloudTalkService.stop();
   });
 });
