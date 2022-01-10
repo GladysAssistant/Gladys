af375ce5b936356b2eb2b9181d8ac382e6f8bea8
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2022-01-10 13:21:56 +0100

    add alexa message handler

---
+++ server/lib/gateway/gateway.handleAlexaMessage.js
@@ -0,0 +1,53 @@
+const get = require('get-value');
+const logger = require('../../utils/logger');
+const { SYSTEM_VARIABLE_NAMES } = require('../../utils/constants');
+
+/**
+ * @description Handle a new Gladys Alexa Gateway message.
+ * @param {Object} data - Gateway message.
+ * @param {Object} rawMessage - Message with metadata.
+ * @param {Function} cb - Callback.
+ * @returns {Promise} Resolve when finished.
+ * @example
+ * handleNewMessage({
+ *  type: 'gladys-api-call',
+ * });
+ */
+async function handleAlexaMessage(data, rawMessage, cb) {
+  const service = this.serviceManager.getService('alexa');
+  try {
+    const body = {
+      ...data.data,
+      user: {
+        id: rawMessage.sender_id,
+        local_user_id: rawMessage.local_user_id,
+      },
+    };
+    // save that the user is connected to gateway alexa
+    if (!this.alexaConnected) {
+      await this.variable.setValue(
+        SYSTEM_VARIABLE_NAMES.GLADYS_GATEWAY_ALEXA_USER_IS_CONNECTED_WITH_GATEWAY,
+        rawMessage.sender_id,
+      );
+      this.alexaConnected = true;
+    }
+    const directiveNamespace = get(body, 'directive.header.namespace');
+    let response;
+    if (directiveNamespace === 'Alexa.Discovery') {
+      response = service.alexaHandler.onDiscovery();
+    } else {
+      response = {
+        status: 400,
+      };
+    }
+
+    cb(response);
+  } catch (e) {
+    logger.error(e);
+    cb({ status: 400 });
+  }
+}
+
+module.exports = {
+  handleAlexaMessage,
+};
