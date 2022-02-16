c36c5c1fc86216a12c5b7bd5cd0beb9924916ff6
Vincent Kulak <vkulak62@gmail.com>
2022-02-16 17:39:57 +0100

    cgroupv1 & v2 compatible

--- server/lib/system/system.getNetworkMode.js
+++ server/lib/system/system.getNetworkMode.js
@@ -1,6 +1,7 @@
 const get = require('get-value');
 const { PlatformNotCompatible } = require('../../utils/coreErrors');
 const { exec } = require('../../utils/childProcess');
+const logger = require('../../utils/logger');
 
 /**
  * @description Get Gladys network into Docker environment.
@@ -15,7 +16,16 @@ async function getNetworkMode() {
 
   if (!this.networkMode) {
     const cmdResult = await exec('head -1 /proc/self/cgroup | cut -d/ -f3');
-    const [containerId] = cmdResult.split('\n');
+    let [cleanResult];
+    if (cmdResult.indexOf('docker') > -1) {
+      cleanResult = cmdResult.split(/[-/.]+/)[1];
+      logger.debug(`Cgroups V2 detected ${cleanResult}`);
+    } else {
+      cleanResult = cmdResult;
+      logger.debug(`Cgroups V1 detected ${cleanResult}`);
+    }
+
+const [containerId] = cleanResult.split('\n');
     const gladysContainer = this.dockerode.getContainer(containerId);
     const gladysContainerInspect = await gladysContainer.inspect();
     this.networkMode = get(gladysContainerInspect, 'HostConfig.NetworkMode', { default: 'unknown' });
