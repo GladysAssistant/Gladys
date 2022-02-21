d1f8c3872d4b9fd09a3dc3b0d702800c58ae8990
VonOx <vkulak62@gmail.com>
2022-02-21 11:54:28 +0100

    Add cidfile check for container Id

--- server/lib/system/system.getNetworkMode.js
+++ server/lib/system/system.getNetworkMode.js
@@ -1,6 +1,7 @@
 const get = require('get-value');
 const { PlatformNotCompatible } = require('../../utils/coreErrors');
 const { exec } = require('../../utils/childProcess');
+const fs = require('fs');
 
 /**
  * @description Get Gladys network into Docker environment.
@@ -14,10 +15,17 @@ async function getNetworkMode() {
   }
 
   if (!this.networkMode) {
-    // eslint-disable-next-line no-useless-escape
-    const cmdResult = await exec(
-      'cat /proc/self/cgroup | grep -o -e "docker.*" | head -n 1 | sed -e "s/.scope//g;s/docker-//g;s!docker/!!g"',
-    );
+    let cmdResult;
+    fs.access('/var/lib/gladysassistant/containerId', fs.constants.F_OK, (err) => {
+      if (err) {
+        // eslint-disable-next-line no-useless-escape
+        cmdResult = await exec(
+          'cat /proc/self/cgroup | grep -o -e "docker.*" | head -n 1 | sed -e "s/.scope//g;s/docker-//g;s!docker/!!g"',
+        );
+      } else {
+        cmdResult = fs.readFileSync('/var/lib/gladysassistant/containerId', 'utf8');
+      }
+    });
     const [containerId] = cmdResult.split('\n');
     const gladysContainer = this.dockerode.getContainer(containerId);
     const gladysContainerInspect = await gladysContainer.inspect();
