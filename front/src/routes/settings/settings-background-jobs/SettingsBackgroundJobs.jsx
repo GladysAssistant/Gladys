aa625ebebe412f9928e978c85253c925ab7b3701
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2022-03-07 15:41:56 +0000

    Fix #1397 : Add pagination on background task list (#1453)

--- front/src/routes/settings/settings-background-jobs/SettingsBackgroundJobs.jsx
+++ front/src/routes/settings/settings-background-jobs/SettingsBackgroundJobs.jsx
@@ -1,9 +1,16 @@
 import SettingsLayout from '../SettingsLayout';
 import JobList from './JobList';
 
-const SettingsBackgroubJobsPage = ({ jobs, user }) => (
+const SettingsBackgroubJobsPage = ({ jobs, user, loadNextPage, loadPreviousPage, isFirstPage, isLastPage }) => (
   <SettingsLayout>
-    <JobList jobs={jobs} user={user} />
+    <JobList
+      jobs={jobs}
+      user={user}
+      loadNextPage={loadNextPage}
+      loadPreviousPage={loadPreviousPage}
+      isFirstPage={isFirstPage}
+      isLastPage={isLastPage}
+    />
   </SettingsLayout>
 );
 
