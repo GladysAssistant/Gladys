13bedf07c3df922effb276543337d456bec24afc
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2022-03-08 12:26:19 +0000

    Improve comments

--- server/lib/system/system.getGladysContainerId.js
+++ server/lib/system/system.getGladysContainerId.js
@@ -27,8 +27,6 @@ async function getGladysContainerId() {
     // 0::/system.slice/docker-2bb2c94b0c395fc8fdff9fa4ce364a3be0dd05792145ffc93ce8d665d06521f1.scope
     // Or this in cgroup v1 (Debian 10)
     // 12:cpuset:/docker/357e73ad015211a5acd76a8973b9287d4de75922e9802d94ba46b756f2bb5350
-    // First, we get the first line:
-
     let firstPart;
     let containerId;
     // If we are on cgroup v1 (debian 10)
@@ -37,6 +35,7 @@ async function getGladysContainerId() {
       const lineWithDocker = allLines.find((line) => line.indexOf('/docker/') !== -1);
       [, containerId] = lineWithDocker.split('/docker/');
     } else if (cgroup.indexOf('docker-') !== -1) {
+      // if we are on cgroupv2 (debian 11)
       const allLines = cgroup.split('\n');
       const lineWithDocker = allLines.find((line) => line.indexOf('docker-') !== -1);
       [, firstPart] = lineWithDocker.split('docker-');
