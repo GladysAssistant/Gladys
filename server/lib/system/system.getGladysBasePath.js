f4a9d2e2222e75c2f98da54c0a9ecebd2162e3c9
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2022-03-15 14:33:50 +0000

    MQTT : Fix installation on custom setup

--- server/lib/system/system.getGladysBasePath.js
+++ server/lib/system/system.getGladysBasePath.js
@@ -2,9 +2,9 @@
  * @description Compute basePath in host and container from mounted point or give default ones.
  * @returns {Promise} Base path in host/container to store files.
  * @example
- * basePath();
+ * const { basePathOnContainer, basePathOnHost } = await getGladysBasePath();
  */
-async function basePath() {
+async function getGladysBasePath() {
   let basePathOnContainer = '/var/lib/gladysassistant';
   // Fetch container path mount
   if (process.env.SQLITE_FILE_PATH) {
@@ -12,8 +12,8 @@ async function basePath() {
     basePathOnContainer = base.substring(0, base.lastIndexOf('/'));
   }
   // Find mount linked to this path to fetch host path
-  const currentContainerId = await this.gladys.system.getGladysContainerId();
-  const gladysMounts = await this.gladys.system.getContainerMounts(currentContainerId);
+  const currentContainerId = await this.getGladysContainerId();
+  const gladysMounts = await this.getContainerMounts(currentContainerId);
   if (gladysMounts) {
     const baseMount = gladysMounts.find((mount) => {
       return mount.Destination === basePathOnContainer;
@@ -26,5 +26,5 @@ async function basePath() {
 }
 
 module.exports = {
-  basePath,
+  getGladysBasePath,
 };
