58aeb3709c706e09df9849b6b490391b21e63999
Alexandre Trovato <1839717+atrovato@users.noreply.github.com>
2022-03-22 10:10:54 +0100

    Co2 sensor with colored badge green/yellow/red (#1472)
    * Dashboard badge value
    
    * Fix open icon

---
+++ front/src/components/boxs/device-in-room/device-features/sensor-value/IconBinaryDeviceValue.jsx
@@ -0,0 +1,23 @@
+import get from 'get-value';
+
+import { DEVICE_FEATURE_CATEGORIES } from '../../../../../../../server/utils/constants';
+
+const ICON_MAP = {
+  [DEVICE_FEATURE_CATEGORIES.OPENING_SENSOR]: {
+    0: 'shield-off',
+    1: 'shield'
+  }
+};
+
+const IconBinaryDeviceValue = ({ deviceFeature }) => {
+  const { category, last_value: lastValue = null } = deviceFeature;
+  const icon = get(ICON_MAP, `${category}.${lastValue}`);
+
+  if (icon) {
+    return <i class={`fe fe-${icon}`} />;
+  }
+
+  return null;
+};
+
+export default IconBinaryDeviceValue;
