e3a1bbb9c82b8994da015405e9ea423d7929c2c0
atrovato <1839717+atrovato@users.noreply.github.com>
2022-04-16 07:43:40 +0200

    Fix click button compat

--- server/services/zigbee2mqtt/exposes/enumType.js
+++ server/services/zigbee2mqtt/exposes/enumType.js
@@ -1,12 +1,43 @@
-const { DEVICE_FEATURE_CATEGORIES, DEVICE_FEATURE_TYPES } = require('../../../utils/constants');
+const { DEVICE_FEATURE_CATEGORIES, DEVICE_FEATURE_TYPES, BUTTON_STATUS } = require('../../../utils/constants');
 
 module.exports = {
   type: 'enum',
   writeValue: (expose, value) => {
-    return expose.values[value];
+    let relatedValue;
+
+    // Mapping is only for button click
+    switch (value) {
+      case BUTTON_STATUS.CLICK:
+        relatedValue = 'single';
+        break;
+      case BUTTON_STATUS.DOUBLE_CLICK:
+        relatedValue = 'double';
+        break;
+      case BUTTON_STATUS.LONG_CLICK:
+        relatedValue = 'hold';
+        break;
+      default:
+        relatedValue = undefined;
+    }
+
+    if (relatedValue && expose.values.includes(relatedValue)) {
+      return relatedValue;
+    }
+
+    return undefined;
   },
   readValue: (expose, value) => {
-    return expose.values.indexOf(value);
+    // Mapping is only for button click
+    switch (value) {
+      case 'single':
+        return BUTTON_STATUS.CLICK;
+      case 'double':
+        return BUTTON_STATUS.DOUBLE_CLICK;
+      case 'hold':
+        return BUTTON_STATUS.LONG_CLICK;
+      default:
+        return undefined;
+    }
   },
   feature: {
     min: 0,
