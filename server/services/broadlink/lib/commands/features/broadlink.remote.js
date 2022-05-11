5d9277d41070d19499d77ff83fe0f84d426d7996
atrovato <1839717+atrovato@users.noreply.github.com>
2022-05-11 22:13:07 +0200

    Remove deice features on remote

--- server/services/broadlink/lib/commands/features/broadlink.remote.js
+++ server/services/broadlink/lib/commands/features/broadlink.remote.js
@@ -1,63 +1,17 @@
 const deviceClasses = require('node-broadlink/dist/remote');
 
-const {
-  DEVICE_FEATURE_CATEGORIES,
-  DEVICE_FEATURE_TYPES,
-  DEVICE_FEATURE_UNITS,
-} = require('../../../../../utils/constants');
 const { NoValuesFoundError } = require('../../../../../utils/coreErrors');
 const logger = require('../../../../../utils/logger');
 const { PARAMS } = require('../../utils/broadlink.constants');
 
 /**
  * @description Builds light Broadlink features.
- * @param {string} deviceName - Device name.
- * @param {string} deviceExternalId - Device external ID.
- * @param {Object} broadlinkDevice - Broadlink device.
- * @returns {Array} Gladys features.
+ * @returns {Array} Empty array.
  * @example
- * buildFeatures(a1Device);
+ * buildFeatures();
  */
-function buildFeatures(deviceName, deviceExternalId, broadlinkDevice) {
-  const { checkTemperature, checkHumidity, TYPE } = broadlinkDevice;
-
-  const features = [];
-
-  // check for temperature sensor
-  if (typeof checkTemperature === 'function' && TYPE !== 'RMPRO') {
-    const featureExternalId = `${deviceExternalId}:temperature-sensor`;
-    features.push({
-      name: `${deviceName} temperature`,
-      category: DEVICE_FEATURE_CATEGORIES.TEMPERATURE_SENSOR,
-      type: DEVICE_FEATURE_TYPES.SENSOR.DECIMAL,
-      external_id: featureExternalId,
-      selector: featureExternalId,
-      min: -50,
-      max: 100,
-      unit: DEVICE_FEATURE_UNITS.CELSIUS,
-      read_only: true,
-      has_feedback: false,
-    });
-  }
-
-  // check for humidity sensor
-  if (typeof checkHumidity === 'function') {
-    const featureExternalId = `${deviceExternalId}:humidity-sensor`;
-    features.push({
-      name: `${deviceName} humidity`,
-      category: DEVICE_FEATURE_CATEGORIES.HUMIDITY_SENSOR,
-      type: DEVICE_FEATURE_TYPES.SENSOR.DECIMAL,
-      external_id: featureExternalId,
-      selector: featureExternalId,
-      min: 0,
-      max: 100,
-      unit: DEVICE_FEATURE_UNITS.PERCENT,
-      read_only: true,
-      has_feedback: false,
-    });
-  }
-
-  return features;
+function buildFeatures() {
+  return [];
 }
 
 /**
@@ -106,45 +60,9 @@ async function setValue(broadlinkDevice, gladysDevice, deviceFeature, value) {
   return null;
 }
 
-/**
- * @description Polling Broadlink device values.
- * @param {Object} broadlinkDevice - Broadlink device.
- * @param {Object} gladysDevice - Gladys device.
- * @returns {Promise} Messages to emit.
- * @example
- * await poll(broadlinkDevice, device);
- */
-async function poll(broadlinkDevice, gladysDevice) {
-  const { features } = gladysDevice;
-  const messages = [];
-
-  // temperature
-  const tempFeature = features.find((feature) => feature.category === DEVICE_FEATURE_CATEGORIES.TEMPERATURE_SENSOR);
-  if (tempFeature) {
-    const state = await broadlinkDevice.checkTemperature();
-    messages.push({
-      device_feature_external_id: tempFeature.external_id,
-      state,
-    });
-  }
-
-  // humidity
-  const humidityFeature = features.find((feature) => feature.category === DEVICE_FEATURE_CATEGORIES.HUMIDITY_SENSOR);
-  if (humidityFeature) {
-    const state = await broadlinkDevice.checkHumidity();
-    messages.push({
-      device_feature_external_id: humidityFeature.external_id,
-      state,
-    });
-  }
-
-  return messages;
-}
-
 module.exports = {
   deviceClasses,
   buildFeatures,
   setValue,
-  poll,
   canLearn: true,
 };
--- server/test/services/broadlink/lib/commands/features/broadlink.remote.test.js
+++ server/test/services/broadlink/lib/commands/features/broadlink.remote.test.js
@@ -18,82 +18,8 @@ describe('broadlink.remote', () => {
 
   describe('buildFeatures', () => {
     it('should have no features', () => {
-      const deviceName = 'deviceName';
-      const deviceExternalId = 'deviceExternalId';
-      const broadlinkDevice = {};
-
-      const features = remote.buildFeatures(deviceName, deviceExternalId, broadlinkDevice);
-
+      const features = remote.buildFeatures();
       expect(features).to.deep.eq([]);
-    });
-
-    it('should have temperture feature', () => {
-      const deviceName = 'deviceName';
-      const deviceExternalId = 'deviceExternalId';
-      const broadlinkDevice = {
-        checkTemperature: fake.resolves(null),
-      };
-
-      const features = remote.buildFeatures(deviceName, deviceExternalId, broadlinkDevice);
-
-      expect(features).to.deep.eq([
-        {
-          name: `deviceName temperature`,
-          category: 'temperature-sensor',
-          type: 'decimal',
-          external_id: 'deviceExternalId:temperature-sensor',
-          selector: 'deviceExternalId:temperature-sensor',
-          min: -50,
-          max: 100,
-          unit: 'celsius',
-          read_only: true,
-          has_feedback: false,
-        },
-      ]);
-
-      assert.notCalled(broadlinkDevice.checkTemperature);
-    });
-
-    it('should not have temperture feature on RMPRO', () => {
-      const deviceName = 'deviceName';
-      const deviceExternalId = 'deviceExternalId';
-      const broadlinkDevice = {
-        checkTemperature: fake.resolves(null),
-        TYPE: 'RMPRO',
-      };
-
-      const features = remote.buildFeatures(deviceName, deviceExternalId, broadlinkDevice);
-
-      expect(features).to.deep.eq([]);
-
-      assert.notCalled(broadlinkDevice.checkTemperature);
-    });
-
-    it('should have humidity feature', () => {
-      const deviceName = 'deviceName';
-      const deviceExternalId = 'deviceExternalId';
-      const broadlinkDevice = {
-        checkHumidity: fake.resolves(null),
-      };
-
-      const features = remote.buildFeatures(deviceName, deviceExternalId, broadlinkDevice);
-
-      expect(features).to.deep.eq([
-        {
-          name: `deviceName humidity`,
-          category: 'humidity-sensor',
-          type: 'decimal',
-          external_id: 'deviceExternalId:humidity-sensor',
-          selector: 'deviceExternalId:humidity-sensor',
-          min: 0,
-          max: 100,
-          unit: 'percent',
-          read_only: true,
-          has_feedback: false,
-        },
-      ]);
-
-      assert.notCalled(broadlinkDevice.checkHumidity);
     });
   });
 
@@ -183,67 +109,6 @@ describe('broadlink.remote', () => {
       }
 
       assert.notCalled(peripheral.sendData);
-    });
-  });
-
-  describe('poll', () => {
-    it('should do nothing on no feature', async () => {
-      const broadlinkDevice = {};
-      const gladysDevice = { features: [] };
-
-      const messages = await remote.poll(broadlinkDevice, gladysDevice);
-
-      expect(messages).to.deep.eq([]);
-    });
-
-    it('should do prepare temperature event', async () => {
-      const broadlinkDevice = {
-        checkTemperature: fake.resolves(33),
-      };
-      const gladysDevice = {
-        features: [
-          {
-            external_id: 'externalId',
-            category: 'temperature-sensor',
-          },
-        ],
-      };
-
-      const messages = await remote.poll(broadlinkDevice, gladysDevice);
-
-      expect(messages).to.deep.eq([
-        {
-          device_feature_external_id: 'externalId',
-          state: 33,
-        },
-      ]);
-
-      assert.calledOnceWithExactly(broadlinkDevice.checkTemperature);
-    });
-
-    it('should do prepare humidity event', async () => {
-      const broadlinkDevice = {
-        checkHumidity: fake.resolves(33),
-      };
-      const gladysDevice = {
-        features: [
-          {
-            external_id: 'externalId',
-            category: 'humidity-sensor',
-          },
-        ],
-      };
-
-      const messages = await remote.poll(broadlinkDevice, gladysDevice);
-
-      expect(messages).to.deep.eq([
-        {
-          device_feature_external_id: 'externalId',
-          state: 33,
-        },
-      ]);
-
-      assert.calledOnceWithExactly(broadlinkDevice.checkHumidity);
     });
   });
 });
