d2d95163f9a008aa782ba2bcfd206098334ecfdd
Euguuu <stephane.martin03@gmail.com>
2022-05-15 19:18:55 +0200

    fix cypress

--- front/cypress/integration/routes/integration/withings/settings/WithingsSettingsWithConfig.js
+++ front/cypress/integration/routes/integration/withings/settings/WithingsSettingsWithConfig.js
@@ -2,29 +2,6 @@ describe('Withings settings page', () => {
   const serverUrl = Cypress.env('serverUrl');
 
   before(() => {
-    /*
-    let interceptCount = 0;
-    cy.intercept(
-      {
-        method: 'GET',
-        url: `${serverUrl}/api/v1/service/oauth2/client`
-      },
-      req => {
-        req.reply(res => {
-          if (interceptCount === 0) {
-            interceptCount += 1;
-            console.log('yop 0');
-            res.send({});
-          } else {
-            console.log('yop A');
-            console.log(res);
-            res = { client_id: 'FakeClientId' };
-            return res;
-            //res.send({ client_id: 'FakeClientId' });
-          }
-        });
-      }
-    );*/
     cy.intercept(
       {
         method: 'POST',
@@ -76,27 +53,6 @@ describe('Withings settings page', () => {
         }
       }
     );
-
-    cy.login();
-
-    cy.visit('/dashboard/integration/health/withings/settings');
-  });
-
-  it('Check setting page and connect and unconnect', () => {
-    cy.get('.markup').i18n('oauth2.instructions');
-
-    cy.get('.form-label').i18n('oauth2.apiKeyLabel');
-
-    cy.get('input')
-      .first()
-      .clear()
-      .type('FakeClientId');
-
-    cy.get('input')
-      .last()
-      .clear()
-      .type('FakeSecret');
-
     cy.intercept(
       {
         method: 'GET',
@@ -105,10 +61,12 @@ describe('Withings settings page', () => {
       { client_id: 'FakeClientId' }
     );
 
-    cy.contains('button', 'oauth2.buttonConnect').click();
+    cy.login();
 
-    cy.wait('@authorizationUriAction');
+    cy.visit('/dashboard/integration/health/withings/settings');
+  });
 
+  it('Check setting page with existing', () => {
     cy.location('pathname').should('eq', '/dashboard/integration/health/withings/settings');
 
     cy.get('.alert.alert-info').i18n('integration.withings.settings.oauth2.complete');
---
+++ front/cypress/integration/routes/integration/withings/settings/WithingsSettingsWithoutConfig.js
@@ -0,0 +1,81 @@
+describe('Withings settings page', () => {
+  const serverUrl = Cypress.env('serverUrl');
+
+  before(() => {
+    cy.intercept(
+      {
+        method: 'POST',
+        url: `${serverUrl}/api/v1/service/oauth2/client/authorization-uri`
+      },
+      {
+        authorizationUri: '/dashboard/integration/health/withings/settings'
+      }
+    ).as('authorizationUriAction');
+    cy.intercept(
+      {
+        method: 'POST',
+        url: `${serverUrl}/api/v1/service/withings/init_devices`
+      },
+      {
+        body: []
+      }
+    );
+    cy.intercept(
+      {
+        method: 'POST',
+        url: `${serverUrl}/api/v1/service/withings/reset`
+      },
+      {
+        body: {
+          success: true
+        }
+      }
+    );
+    cy.intercept(
+      {
+        method: 'POST',
+        url: `${serverUrl}/api/v1/service/withings/variable/OAUTH2_CLIENT_ID`
+      },
+      {
+        body: {
+          success: true
+        }
+      }
+    );
+    cy.intercept(
+      {
+        method: 'POST',
+        url: `${serverUrl}/api/v1/service/withings/variable/OAUTH2_CLIENT_SECRET`
+      },
+      {
+        body: {
+          success: true
+        }
+      }
+    );
+
+    cy.login();
+
+    cy.visit('/dashboard/integration/health/withings/settings');
+  });
+
+  it('Check setting page and connect', () => {
+    cy.get('.markup').i18n('oauth2.instructions');
+
+    cy.get('.form-label').i18n('oauth2.apiKeyLabel');
+
+    cy.get('input')
+      .first()
+      .clear()
+      .type('FakeClientId');
+
+    cy.get('input')
+      .last()
+      .clear()
+      .type('FakeSecret');
+
+    cy.contains('button', 'oauth2.buttonConnect').click();
+
+    cy.wait('@authorizationUriAction');
+  });
+});
