e0ed0a65da165bf659c0c95836dc87b7b0034f44
Euguuu <stephane.martin03@gmail.com>
2022-05-15 19:43:09 +0200

    fix cypress

--- front/cypress/integration/routes/integration/withings/settings/WithingsSettingsWithConfig.js
+++ front/cypress/integration/routes/integration/withings/settings/WithingsSettingsWithConfig.js
@@ -10,7 +10,7 @@ describe('Withings settings page', () => {
       {
         authorizationUri: '/dashboard/integration/health/withings/settings'
       }
-    ).as('authorizationUriAction');
+    );
     cy.intercept(
       {
         method: 'POST',
@@ -59,7 +59,7 @@ describe('Withings settings page', () => {
         url: `${serverUrl}/api/v1/service/oauth2/client`
       },
       { client_id: 'FakeClientId' }
-    );
+    ).as('getConfig');
 
     cy.login();
 
@@ -67,7 +67,7 @@ describe('Withings settings page', () => {
   });
 
   it('Check setting page with existing', () => {
-    cy.location('pathname').should('eq', '/dashboard/integration/health/withings/settings');
+    cy.wait('@getConfig');
 
     cy.get('.alert.alert-info').i18n('integration.withings.settings.oauth2.complete');
 
