1feac098b11081c394f9f14f1b294e558f077f06
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2022-05-20 11:19:22 +0100

    Add Alexa integration with Gladys Plus (#1396)

---
+++ server/services/alexa/index.js
@@ -0,0 +1,32 @@
+const logger = require('../../utils/logger');
+const AlexaHandler = require('./lib');
+
+module.exports = function AlexaService(gladys, serviceId) {
+  const alexaHandler = new AlexaHandler(gladys, serviceId);
+
+  /**
+   * @public
+   * @description This function starts service
+   * @example
+   * gladys.services['alexa'].start();
+   */
+  async function start() {
+    logger.info('starting Alexa service');
+  }
+
+  /**
+   * @public
+   * @description This function stops the service
+   * @example
+   *  gladys.services['alexa'].stop();
+   */
+  async function stop() {
+    logger.info('stopping Alexa service');
+  }
+
+  return Object.freeze({
+    start,
+    stop,
+    alexaHandler,
+  });
+};
