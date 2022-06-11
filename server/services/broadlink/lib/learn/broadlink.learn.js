7ec9576df218f9dc65a52c0992765dd7a4bc38c6
atrovato <1839717+atrovato@users.noreply.github.com>
2022-06-11 15:05:45 +0200

    Add missing await

--- server/services/broadlink/lib/learn/broadlink.learn.js
+++ server/services/broadlink/lib/learn/broadlink.learn.js
@@ -28,7 +28,7 @@ async function learn(peripheralIdentifier) {
 
       // async check data
       logger.debug(`Broadlink entering ${peripheralIdentifier} in learn mode...`);
-      this.checkData(peripheral, peripheralIdentifier);
+      await this.checkData(peripheral, peripheralIdentifier);
     } catch (e) {
       logger.error(`Broadlink fails to enter in learning mode on ${peripheralIdentifier} device`, e);
       this.gladys.event.emit(EVENTS.WEBSOCKET.SEND_ALL, {
