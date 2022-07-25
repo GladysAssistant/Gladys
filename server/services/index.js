5c4580104604df1d2f9a8320a9d5459edf4ac90c
Nicolas Geissel <nicolas@geissel.fr>
2022-07-25 19:50:10 +0200

    add ecovacs service

--- server/services/index.js
+++ server/services/index.js
@@ -15,3 +15,5 @@ module.exports.ewelink = require('./ewelink');
 module.exports['tp-link'] = require('./tp-link');
 module.exports.zigbee2mqtt = require('./zigbee2mqtt');
 module.exports['google-actions'] = require('./google-actions');
+module.exports['ecovacs'] = require('./ecovacs');
+
