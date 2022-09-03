dbd58b6ce73841e6cc8e40125572e6b78b3511b6
bertrandda <bertrandda@users.noreply.github.com>
2022-09-03 10:47:53 +0200

    Use true uuid in test

--- server/test/services/homekit/lib/createBridge.test.js
+++ server/test/services/homekit/lib/createBridge.test.js
@@ -7,7 +7,7 @@ describe('Create bridge', () => {
   const homekitHandler = {
     serviceId: '7056e3d4-31cc-4d2a-bbdd-128cd49755e6',
     createBridge,
-    buildAccessory: stub().returns({ UUID: 'fake-accessory' }),
+    buildAccessory: stub().returns({ UUID: '78a7b724-18e8-4c15-ab30-c8486c253f36' }),
     gladys: {
       variable: {
         getValue: stub().resolves(null),
@@ -43,7 +43,7 @@ describe('Create bridge', () => {
 
     expect(homekitHandler.hap.Bridge.args[0][0]).to.equal('Gladys');
     expect(homekitHandler.hap.Bridge.args[0][1]).not.equal(null);
-    expect(addBridgedAccessories.args[0][0]).to.deep.members([{ UUID: 'fake-accessory' }]);
+    expect(addBridgedAccessories.args[0][0]).to.deep.members([{ UUID: '78a7b724-18e8-4c15-ab30-c8486c253f36' }]);
     expect(publish.args[0][0]).to.eql({
       username: 'C4:D0:AB:12:BC:51',
       pincode: '123-45-678',
