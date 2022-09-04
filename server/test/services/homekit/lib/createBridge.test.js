116f85f08ad8af22593abf915e9338679478d86f
bertrandda <bertrandda@users.noreply.github.com>
2022-09-04 20:23:21 +0200

    Fix tests

--- server/test/services/homekit/lib/createBridge.test.js
+++ server/test/services/homekit/lib/createBridge.test.js
@@ -4,39 +4,38 @@ const { createBridge } = require('../../../../services/homekit/lib/createBridge'
 const { DEVICE_FEATURE_CATEGORIES } = require('../../../../utils/constants');
 
 describe('Create bridge', () => {
-  const homekitHandler = {
-    serviceId: '7056e3d4-31cc-4d2a-bbdd-128cd49755e6',
-    createBridge,
-    buildAccessory: stub().returns({ UUID: '78a7b724-18e8-4c15-ab30-c8486c253f36' }),
-    gladys: {
-      variable: {
-        getValue: stub().resolves(null),
-        setValue: stub().resolves(),
-      },
-      device: {
-        get: stub().resolves([
-          {
-            id: '07f16117-8556-4b50-b9f0-e190d08f8d92',
-            name: 'Lampe bureau',
-            features: [{ category: DEVICE_FEATURE_CATEGORIES.LIGHT }],
-          },
-        ]),
-      },
-    },
-    newUsername: stub().resolves('C4:D0:AB:12:BC:51'),
-    newPinCode: stub().resolves('123-45-678'),
-  };
-
   it('should create a bridge', async () => {
     const addBridgedAccessories = stub().returns();
     const publish = stub().resolves();
-    homekitHandler.hap = {
-      Categories: { BRIDGE: 'BRIDGE' },
-      Bridge: stub().returns({
-        addBridgedAccessories,
-        publish,
-        setupURI: stub().returns(),
-      }),
+    const homekitHandler = {
+      serviceId: '7056e3d4-31cc-4d2a-bbdd-128cd49755e6',
+      createBridge,
+      buildAccessory: stub().returns({ UUID: '78a7b724-18e8-4c15-ab30-c8486c253f36' }),
+      gladys: {
+        variable: {
+          getValue: stub().resolves(null),
+          setValue: stub().resolves(),
+        },
+        device: {
+          get: stub().resolves([
+            {
+              id: '07f16117-8556-4b50-b9f0-e190d08f8d92',
+              name: 'Lampe bureau',
+              features: [{ category: DEVICE_FEATURE_CATEGORIES.LIGHT }],
+            },
+          ]),
+        },
+      },
+      newUsername: stub().resolves('C4:D0:AB:12:BC:51'),
+      newPinCode: stub().resolves('123-45-678'),
+      hap: {
+        Categories: { BRIDGE: 'BRIDGE' },
+        Bridge: stub().returns({
+          addBridgedAccessories,
+          publish,
+          setupURI: stub().returns(),
+        }),
+      },
     };
 
     await homekitHandler.createBridge();
