3e0ad13e6aa49c4be92632857f0de4458879c9ce
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2022-09-09 16:25:16 +0200

    Rework Enedis UI and add i18n

--- front/src/routes/integration/all/enedis-gateway/index.js
+++ front/src/routes/integration/all/enedis-gateway/index.js
@@ -5,7 +5,7 @@ import cx from 'classnames';
 import EnedisButton from './enedis-button.png';
 import { route } from 'preact-router';
 
-const EnedisWelcomePage = ({ redirectUri, errored, loading }) => (
+const EnedisWelcomePage = ({ redirectUri, errored, loading, usagePointsIds, notOnGladysGateway }) => (
   <div class="page">
     <div class="page-main">
       <div class="my-3 my-md-5">
@@ -31,17 +31,36 @@ const EnedisWelcomePage = ({ redirectUri, errored, loading }) => (
                           <Text id="integration.enedis.error" />
                         </p>
                       )}
+                      {notOnGladysGateway && (
+                        <p class="alert alert-info">
+                          <Text id="integration.enedis.notOnGladysGateway" />
+                        </p>
+                      )}
+                      {usagePointsIds && (
+                        <p class="alert alert-success">
+                          <Text
+                            id="integration.enedis.connectedToUsagePointds"
+                            plural={usagePointsIds.length}
+                            fields={{ count: usagePointsIds.length }}
+                          />{' '}
+                          <Text id="integration.enedis.reconnectInfo" />
+                        </p>
+                      )}
                       <p>
                         <Text id="integration.enedis.longDescription" />
                       </p>
-                      <p>
-                        <Text id="integration.enedis.buttonDescription" />
-                      </p>
-                      <a href={redirectUri} target="_blank" rel="noreferrer noopener">
-                        <Localizer>
-                          <img src={EnedisButton} alt={<Text id="integration.enedis.buttonDescription" />} />
-                        </Localizer>
-                      </a>
+                      {!notOnGladysGateway && (
+                        <div>
+                          <p>
+                            <Text id="integration.enedis.buttonDescription" />
+                          </p>
+                          <a href={redirectUri} target="_blank" rel="noreferrer noopener">
+                            <Localizer>
+                              <img src={EnedisButton} alt={<Text id="integration.enedis.buttonDescription" />} />
+                            </Localizer>
+                          </a>
+                        </div>
+                      )}
                     </div>
                   </div>
                 </div>
@@ -56,18 +75,36 @@ const EnedisWelcomePage = ({ redirectUri, errored, loading }) => (
 
 class EnedisWelcomePageComponent extends Component {
   getRedirectUri = async () => {
-    const response = await this.props.session.gatewayClient.initializeEnedis();
-    this.setState({
-      redirectUri: response.redirect_uri
-    });
+    try {
+      if (!this.props.session.gatewayClient) {
+        this.setState({
+          notOnGladysGateway: true
+        });
+        return;
+      }
+      const response = await this.props.session.gatewayClient.initializeEnedis();
+      this.setState({
+        redirectUri: response.redirect_uri
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  getCurrentEnedisUsagePoints = async () => {
+    try {
+      const data = await this.props.httpClient.get('/api/v1/service/zigbee2mqtt/variable/ENEDIS_USAGE_POINTS_ID');
+      const usagePointsIds = JSON.parse(data.value);
+      this.setState({ usagePointsIds });
+    } catch (e) {
+      console.error(e);
+    }
   };
   detectCode = async () => {
     if (this.props.code) {
       try {
-        await this.setState({ loading: true, errored: false });
+        await this.setState({ errored: false });
         const response = await this.props.session.gatewayClient.finalizeEnedis(this.props.code);
         this.setState({
-          loading: false,
           usagePointsIds: response.usage_points_id
         });
         await this.props.httpClient.post('/api/v1/service/zigbee2mqtt/variable/ENEDIS_USAGE_POINTS_ID', {
@@ -75,16 +112,28 @@ class EnedisWelcomePageComponent extends Component {
         });
         route('/dashboard/integration/device/enedis');
       } catch (e) {
-        await this.setState({ loading: false, errored: true });
+        await this.setState({ errored: true });
       }
     }
   };
+  init = async () => {
+    await this.setState({ loading: true });
+    await Promise.all([this.getRedirectUri(), this.detectCode(), this.getCurrentEnedisUsagePoints()]);
+    await this.setState({ loading: false });
+  };
   componentDidMount() {
-    this.getRedirectUri();
-    this.detectCode();
+    this.init();
   }
-  render({}, { redirectUri, loading, errored }) {
-    return <EnedisWelcomePage redirectUri={redirectUri} loading={loading} errored={errored} />;
+  render({}, { redirectUri, loading, errored, usagePointsIds, notOnGladysGateway }) {
+    return (
+      <EnedisWelcomePage
+        redirectUri={redirectUri}
+        loading={loading}
+        errored={errored}
+        usagePointsIds={usagePointsIds}
+        notOnGladysGateway={notOnGladysGateway}
+      />
+    );
   }
 }
 
