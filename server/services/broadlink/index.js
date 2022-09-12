f2ae7ba82197155ac24ba84337900da49b3ab20f
Alexandre Trovato <1839717+atrovato@users.noreply.github.com>
2022-09-12 11:48:22 +0200

    Add Broadlink integration (#1516)

---
+++ server/services/broadlink/index.js
@@ -0,0 +1,37 @@
+const logger = require('../../utils/logger');
+const BroadlinkHandler = require('./lib');
+const BroadlinkController = require('./api/broadlink.controller');
+
+module.exports = function BroadlinkService(gladys, serviceId) {
+  const broadlink = require('node-broadlink');
+  const broadlinkHandler = new BroadlinkHandler(gladys, broadlink, serviceId);
+
+  /**
+   * @public
+   * @description This function starts service
+   * @example
+   * gladys.services.broadlink.start();
+   */
+  async function start() {
+    logger.info('starting Broadlink service');
+    await broadlinkHandler.init();
+  }
+
+  /**
+   * @public
+   * @description This function stops the service
+   * @example
+   *  gladys.services.broadlink.stop();
+   */
+  async function stop() {
+    logger.info('stopping Broadlink service');
+    broadlinkHandler.stop();
+  }
+
+  return Object.freeze({
+    start,
+    stop,
+    device: broadlinkHandler,
+    controllers: BroadlinkController(broadlinkHandler),
+  });
+};
