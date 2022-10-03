3d27d12a7f2a30df0cc213503313c028e7ea83cd
Alexandre Trovato <1839717+atrovato@users.noreply.github.com>
2022-10-03 05:56:30 +0200

    Tasmota: Add more temp devices (#1570)
    * Add BL09XX temp devices
    
    * Add ESP32 temp devices
    
    * Add DS18x20 temp device
    
    * Generic temp/humidity discover

--- server/services/tasmota/lib/features/device_temperature.js
+++ server/services/tasmota/lib/features/device_temperature.js
@@ -6,22 +6,22 @@ const {
 
 module.exports = {
   // Tasmota matcher
-  keyMatcher: /^StatusSNS\.[^.]+\.Humidity$/,
+  keyMatcher: /^StatusSNS\.ESP32\.Temperature$/,
   // Gladys feature
   generateFeature: () => {
     return {
-      category: DEVICE_FEATURE_CATEGORIES.HUMIDITY_SENSOR,
+      category: DEVICE_FEATURE_CATEGORIES.DEVICE_TEMPERATURE_SENSOR,
       type: DEVICE_FEATURE_TYPES.SENSOR.DECIMAL,
-      name: 'Humidity',
+      name: 'Device temperature',
       read_only: true,
       has_feedback: false,
-      min: 0,
-      max: 100,
-      unit: DEVICE_FEATURE_UNITS.PERCENT,
+      min: -100,
+      max: 200,
+      unit: DEVICE_FEATURE_UNITS.CELSIUS,
     };
   },
   generateExternalId: (key, fullKey) => {
-    const parts = fullKey.split('.');
-    return `${parts[1]}:Humidity`;
+    const parts = fullKey.split('.').slice(1, -1);
+    return `${parts.join(':')}:DeviceTemperature`;
   },
 };
