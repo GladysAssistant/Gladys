323752491815571aff78f62c27b33f9487778660
VonOx <vkulak62@gmail.com>
2022-10-03 10:37:46 +0200

    Use addMapping function

--- server/services/zigbee2mqtt/exposes/enumType.js
+++ server/services/zigbee2mqtt/exposes/enumType.js
@@ -2,9 +2,8 @@ const {
   DEVICE_FEATURE_CATEGORIES,
   DEVICE_FEATURE_TYPES,
   BUTTON_STATUS,
-  TIMER_STATUS,
-} = require('../../../utils/constants');
   COVER_STATE,
+  TIMER_STATUS,
 } = require('../../../utils/constants');
 
 const WRITE_VALUE_MAPPING = {};
@@ -27,38 +26,13 @@ addMapping('action', BUTTON_STATUS.LONG_CLICK, 'long');
 addMapping('state', COVER_STATE.OPEN, 'OPEN');
 addMapping('state', COVER_STATE.CLOSE, 'CLOSE');
 addMapping('state', COVER_STATE.STOP, 'STOP');
+addMapping('timer_state', TIMER_STATUS.ACTIVE, 'active');
+addMapping('timer_state', TIMER_STATUS.DISABLED, 'disabled');
+addMapping('timer_state', TIMER_STATUS.ENABLED, 'enabled');
 
 module.exports = {
   type: 'enum',
   writeValue: (expose, value) => {
-    let relatedValue;
-
-    // Mapping is only for button click and timer status
-    switch (value) {
-      case BUTTON_STATUS.CLICK:
-        relatedValue = 'single';
-        break;
-      case BUTTON_STATUS.DOUBLE_CLICK:
-        relatedValue = 'double';
-        break;
-      case BUTTON_STATUS.HOLD_CLICK:
-        relatedValue = 'hold';
-        break;
-      case BUTTON_STATUS.LONG_CLICK:
-        relatedValue = 'long';
-        break;
-      case TIMER_STATUS.DISABLED:
-        relatedValue = 'disabled';
-        break;
-      case TIMER_STATUS.ENABLED:
-        relatedValue = 'enabled';
-        break;
-      case TIMER_STATUS.ACTIVE:
-        relatedValue = 'active';
-        break;
-      default:
-        relatedValue = undefined;
-    }
     const relatedValue = (WRITE_VALUE_MAPPING[expose.name] || {})[value];
 
     if (relatedValue && expose.values.includes(relatedValue)) {
@@ -68,25 +42,6 @@ module.exports = {
     return undefined;
   },
   readValue: (expose, value) => {
-    // Mapping is only for button click
-    switch (value) {
-      case 'single':
-        return BUTTON_STATUS.CLICK;
-      case 'double':
-        return BUTTON_STATUS.DOUBLE_CLICK;
-      case 'hold':
-        return BUTTON_STATUS.HOLD_CLICK;
-      case 'long':
-        return BUTTON_STATUS.LONG_CLICK;
-      case 'disabled':
-        return TIMER_STATUS.DISABLED;
-      case 'enabled':
-        return TIMER_STATUS.ENABLED;
-      case 'active':
-        return TIMER_STATUS.ACTIVE;
-      default:
-        return undefined;
-    }
     return (READ_VALUE_MAPPING[expose.name] || {})[value];
   },
   feature: {
