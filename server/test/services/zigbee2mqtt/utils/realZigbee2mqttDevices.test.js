ce240d44d95d9ec2086ff1ae4bf4afd000b30fe5
Alexandre Trovato <1839717+atrovato@users.noreply.github.com>
2022-10-03 05:52:12 +0200

    Handle Zigbee2mqtt cover devices (#1577)

---
+++ server/test/services/zigbee2mqtt/utils/realZigbee2mqttDevices.test.js
@@ -0,0 +1,35 @@
+const { expect } = require('chai');
+const Zigbee2mqttManager = require('../../../../services/zigbee2mqtt/lib');
+
+const { convertDevice } = require('../../../../services/zigbee2mqtt/utils/convertDevice');
+const payloads = require('./payloads');
+
+const serviceId = 'a4c859f0-32d2-46b7-8f5a-3285960f498a';
+
+describe('Decoding real devices', () => {
+  payloads.forEach((payload) => {
+    const { name, mqttDevice, gladysDevice, values } = payload;
+
+    let service;
+    beforeEach(() => {
+      service = new Zigbee2mqttManager(null, null, null);
+      service.discoveredDevices = {
+        [name]: mqttDevice,
+      };
+    });
+
+    it(`[${name}] check Gladys device`, async () => {
+      const device = await convertDevice(mqttDevice, serviceId);
+
+      expect(device).to.deep.eq(gladysDevice);
+    });
+
+    values.forEach(({ internal, external, property }) => {
+      it(`[${name}] read value ${external}`, () => {
+        const result = service.readValue(name, property, external);
+
+        expect(result).to.deep.eq(internal);
+      });
+    });
+  });
+});
