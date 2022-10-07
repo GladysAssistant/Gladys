9ebaa3259493b0769403862860ac7b0ec370f57a
bertrandda <bertrandda@users.noreply.github.com>
2022-10-07 20:37:32 +0200

    Fix tests

--- server/test/services/homekit/lib/buildAccessory.test.js
+++ server/test/services/homekit/lib/buildAccessory.test.js
@@ -6,14 +6,14 @@ describe('Build accessory', () => {
   const homekitHandler = {
     serviceId: '7056e3d4-31cc-4d2a-bbdd-128cd49755e6',
     buildAccessory,
-    buildService: sinon.stub(),
+    buildService: sinon.stub().returns('builded-service'),
     gladys: {},
   };
 
   it('should build an accessory', async () => {
     const addService = sinon.stub();
     homekitHandler.hap = {
-      Accessory: sinon.stub().returns({ addService }),
+      Accessory: sinon.stub().returns({ addService, services: ['service1', 'service2'] }),
     };
 
     const device = {
@@ -23,14 +23,17 @@ describe('Build accessory', () => {
         {
           name: 'Luminosité',
           category: 'light',
+          type: 'brightness',
         },
         {
           name: 'onoff',
           category: 'light',
+          type: 'binary',
         },
         {
           name: 'Température',
           category: 'temperature-sensor',
+          type: 'decimal',
         },
       ],
     };
@@ -39,11 +42,11 @@ describe('Build accessory', () => {
 
     expect(homekitHandler.buildService.callCount).to.equal(2);
     expect(homekitHandler.buildService.args[0][1]).to.have.deep.members([
-      { name: 'Luminosité', category: 'light' },
-      { name: 'onoff', category: 'light' },
+      { name: 'Luminosité', category: 'light', type: 'brightness' },
+      { name: 'onoff', category: 'light', type: 'binary' },
     ]);
     expect(homekitHandler.buildService.args[1][1]).to.have.deep.members([
-      { name: 'Température', category: 'temperature-sensor' },
+      { name: 'Température', category: 'temperature-sensor', type: 'decimal' },
     ]);
     expect(addService.callCount).to.equal(2);
   });
