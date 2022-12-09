778ad9abbdce16a59fec804b74e5c6c7b0d4f490
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2022-12-09 13:16:18 +0700

    Fix edit box button

--- front/src/components/boxs/ecowatt/EditEcowatt.jsx
+++ front/src/components/boxs/ecowatt/EditEcowatt.jsx
@@ -1,24 +1,15 @@
 import { Text } from 'preact-i18n';
+import BaseEditBox from '../baseEditBox';
 
-const EditEcowatt = ({}) => (
-  <div class="card">
-    <div class="card-header">
-      <h3 class="card-title">
-        <i class="fe fe-zap" />
-        <span class="m-1">
-          <Text id="dashboard.boxes.ecowatt.title" />
-        </span>
-      </h3>
-    </div>
-    <div class="card-body">
-      <Text id="dashboard.boxes.ecowatt.description" />{' '}
-      <small>
-        <a href="https://www.monecowatt.fr/" target="_blank" rel="noopener noreferrer">
-          <Text id="editScene.actionsCard.ecowattCondition.knowMore" />
-        </a>
-      </small>
-    </div>
-  </div>
+const EditEcowatt = ({ ...props }) => (
+  <BaseEditBox {...props} titleKey="dashboard.boxTitle.ecowatt">
+    <Text id="dashboard.boxes.ecowatt.description" />{' '}
+    <small>
+      <a href="https://www.monecowatt.fr/" target="_blank" rel="noopener noreferrer">
+        <Text id="editScene.actionsCard.ecowattCondition.knowMore" />
+      </a>
+    </small>
+  </BaseEditBox>
 );
 
 export default EditEcowatt;
