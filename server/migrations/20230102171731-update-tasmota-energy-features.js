e824f432234f7506ca5d593d51f00b5434c1926d
Vincent Kulak <vkulak62@gmail.com>
2023-01-02 23:15:35 +0100

    Fix migration file

--- server/migrations/20230102171731-update-tasmota-energy-features.js
+++ server/migrations/20230102171731-update-tasmota-energy-features.js
@@ -11,84 +11,85 @@ module.exports = {
         name: 'tasmota',
       },
     });
-
-    // Load impacted features
-    const features = await db.DeviceFeature.findAll({
-      where: {
-        category: 'switch',
-        type: ['energy', 'power', 'voltage'],
-      },
-      include: [
-        {
-          model: db.Device,
-          as: 'device',
-          where: {
-            service_id: service.id,
-          },
+    if (service != null) {
+      // Load impacted features
+      const features = await db.DeviceFeature.findAll({
+        where: {
+          category: 'switch',
+          type: ['energy', 'power', 'voltage'],
         },
-      ],
-    });
+        include: [
+          {
+            model: db.Device,
+            as: 'device',
+            where: {
+              service_id: service.id,
+            },
+          },
+        ],
+      });
 
-    await Promise.mapSeries(features, async (feature) => {
-      const { id, type, name, last_value: value } = feature;
+      await Promise.mapSeries(features, async (feature) => {
+        const { id, type, name, last_value: value } = feature;
 
-      // Modify energy current
-      if (type === 'energy') {
-        const currentFields = {
-          category: 'energy-sensor',
-          type: 'current',
-        };
+        // Modify energy current
+        if (type === 'energy') {
+          const currentFields = {
+            category: 'energy-sensor',
+            type: 'current',
+          };
 
-        if (name === 'Energy') {
-          currentFields.name = 'Intensity';
+          if (name === 'Energy') {
+            currentFields.name = 'Intensity';
+          }
+
+          await feature.update(currentFields);
         }
 
-        await feature.update(currentFields);
-      }
+        // Modify energy power
+        if (type === 'power') {
+          const currentFields = {
+            category: 'energy-sensor',
+            unit: 'watt',
+          };
 
-      // Modify energy power
-      if (type === 'power') {
-        const currentFields = {
-          category: 'energy-sensor',
-          unit: 'watt',
-        };
+          await feature.update(currentFields);
+        }
 
-        await feature.update(currentFields);
-      }
+        // Modify energy voltage
+        if (type === 'voltage') {
+          const currentFields = {
+            category: 'energy-sensor',
+            unit: 'watt',
+            last_value: value * 1000,
+          };
 
-      // Modify energy voltage
-      if (type === 'voltage') {
-        const currentFields = {
-          category: 'energy-sensor',
-          unit: 'watt',
-          last_value: value * 1000,
-        };
-
-        await feature.update(currentFields);
-        // Update feature states
-        await db.DeviceFeatureState.update(
-          {
-            value: sequelize.literal('value * 1000'),
-          },
-          {
-            where: {
-              device_feature_id: id,
+          await feature.update(currentFields);
+          // Update feature states
+          await db.DeviceFeatureState.update(
+            {
+              value: sequelize.literal('value * 1000'),
             },
-          },
-        );
-        // Update feature aggregate
-        await db.DeviceFeatureStateAggregate.update(
-          {
-            value: sequelize.literal('value * 1000'),
-          },
-          {
-            where: {
-              device_feature_id: id,
+            {
+              where: {
+                device_feature_id: id,
+              },
             },
-          },
-        );
-      }
-    });
+          );
+          // Update feature aggregate
+          await db.DeviceFeatureStateAggregate.update(
+            {
+              value: sequelize.literal('value * 1000'),
+            },
+            {
+              where: {
+                device_feature_id: id,
+              },
+            },
+          );
+        }
+      });
+    }
   },
   down: async () => {},
 };
