96460224e9ba8a3b5dc80067317ca121c9c81bcd
Nicolas Geissel <nicolas@geissel.fr>
2023-04-03 17:47:30 +0200

    Update Vacbot box template

--- front/src/components/boxs/vacbot/Vacbot.jsx
+++ front/src/components/boxs/vacbot/Vacbot.jsx
@@ -21,77 +21,66 @@ const VacbotBox = ({ children, ...props }) => (
         </p>
       </div>
     )}
-    <div class="card-body d-flex flex-column">
-      <h4>{props.name}</h4>
-    </div>
-    <div class="row" style={{fontSize: '1.5em'}}>
-          <div class="col-9" >
-          {props.cleanReport == 'idle' && <i class={`fe fe-disc`} />}
-          {props.chargeStatus == 'returning' && <i class={`list-separated-item fe fe-dowload`} />}
-          {props.cleanReport == 'auto' && <i class={`list-separated-item fe fe-play-circle`} />}
-          {props.cleanReport}
-          </div>
-          <div class="col-3">
-            {props.chargeStatus == 'charging' && <i class={`fe fe-battery-charging`} style={{fontSize: '20px'}} >{props.batteryLevel}% </i>}
-            {props.chargeStatus != 'charging' && <i class={`fe fe-battery`} style={{fontSize: '20px'}}>{props.batteryLevel}%</i> }
-          </div>
-    </div>
- 
     
-        <div class="position-relative" style={{fontSize: '20px'}}>
-          
-          <img src={props.imageUrl}  />
-  
-  
-          <span class="position-absolute top-0 start-100  badge rounded-pill bg-secondary">
-          
+    <div class="card-body ">
+      <div class="d-flex justify-content-center">
+            <div class="col" >
+            {props.cleanReport == 'idle' && <i class={`fe fe-disc`} />}
+            {props.chargeStatus == 'returning' && <i class={`list-separated-item fe fe-dowload`} />}
+            {props.cleanReport == 'auto' && <i class={`list-separated-item fe fe-play-circle`} />}
+            {props.cleanReport}
+            </div>
+            <div class="col-md-auto">
+              {props.chargeStatus == 'charging' && <i class={`fe fe-battery-charging`} style={{fontSize: '20px'}} >{props.batteryLevel}% </i>}
+              {props.chargeStatus != 'charging' && <i class={`fe fe-battery`} style={{fontSize: '20px'}}>{props.batteryLevel}%</i> }
+            </div>
+      </div>
+      <h4 class="mx-auto">{props.name}</h4>
+      
+      <div class="position-relative" style={{fontSize: '20px'}}>
+          <img src={props.imageUrl} class="img-fluid" />
+          <span class="position-absolute top-0 start-100">
           {props.hasMappingCapabilities && <button class={`btn btn-sm fe fe-map`} />}
           {props.hasCustomAreaCleaningMode && <button class={`btn btn-sm fe fe-codepen`} />}
-       
-          </span>
-      
-        
-      
-        
-
-      
-    </div>
-    
-      <div class="d-flex justify-content-center">
-        <div class="btn-group" role="group">
-          <button
-            class={cx('btn btn-sm btn-secondary', 'fe', 'fe-play', {
-              active: 1
-            })}
-            onClick={props.clean}
-          >
-            
-          </button>
-          <button
-            class={cx('btn btn-sm btn-secondary', 'fe', 'fe-pause', {
-              active: 1
-            })}
-            onClick={props.pause}
-          />
-          <button
-            class={cx('btn btn-sm btn-secondary', 'fe', 'fe-square', {
-              active: 1
-            })}
-            onClick={props.stop}
-          >
-          </button>
-          <button
-            class={cx('btn btn-sm btn-secondary', 'fe', 'fe-home', {
-              active: 1
-            })}
-            onClick={props.home}
-          ></button>
+          </span>  
         </div>
+    </div>
+ 
+    <div class="d-flex justify-content-center">
+      <div class="btn-group" role="group">
+        <button
+          class={cx('btn btn-sm btn-secondary', 'fe', 'fe-play', {
+            active: 1
+          })}
+          onClick={props.clean}
+        >
+          
+        </button>
+        <button
+          class={cx('btn btn-sm btn-secondary', 'fe', 'fe-pause', {
+            active: 1
+          })}
+          onClick={props.pause}
+        />
+        <button
+          class={cx('btn btn-sm btn-secondary', 'fe', 'fe-square', {
+            active: 1
+          })}
+          onClick={props.stop}
+        >
+        </button>
+        <button
+          class={cx('btn btn-sm btn-secondary', 'fe', 'fe-home', {
+            active: 1
+          })}
+          onClick={props.home}
+        ></button>
       </div>
+    </div>
     
    
     
-    <div class="mt-3">
+    <div class="mt-3 position-relative">
         hasMoppingSystem : {props.hasMoppingSystem}, chargeStatus : {props.chargeStatus},  cleanReport : {props.cleanReport}
     </div>
   </div>
