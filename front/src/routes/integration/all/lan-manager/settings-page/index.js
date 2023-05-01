51d6738db74de61a7288c36c4b2dce00fd52d71d
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2023-05-01 14:49:06 +0700

    Remove @connect to connect()

--- front/src/routes/integration/all/lan-manager/settings-page/index.js
+++ front/src/routes/integration/all/lan-manager/settings-page/index.js
@@ -4,7 +4,6 @@ import { connect } from 'unistore/preact';
 import LANManagerPage from '../LANManagerPage';
 import LANManagerSettingsTab from './LANManagerSettingsTab';
 
-@connect('user,httpClient')
 class LANManagerSettingsPage extends Component {
   render(props) {
     return (
@@ -15,4 +14,4 @@ class LANManagerSettingsPage extends Component {
   }
 }
 
-export default LANManagerSettingsPage;
+export default connect('user,httpClient')(LANManagerSettingsPage);
