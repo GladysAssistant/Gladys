106904bafe64d6a2994b86b1813af26952c7d049
Bertrand d'Aure <bertrandda@users.noreply.github.com>
2023-05-05 06:42:16 +0200

    New Nextcloud Talk integration (#1388)

--- front/src/components/app.jsx
+++ front/src/components/app.jsx
@@ -84,6 +84,7 @@ import TPLinkDevicePage from '../routes/integration/all/tp-link/device-page';
 import RtspCameraPage from '../routes/integration/all/rtsp-camera';
 import XiaomiPage from '../routes/integration/all/xiaomi';
 import EditXiaomiPage from '../routes/integration/all/xiaomi/edit-page';
+import NextcloudTalkPage from '../routes/integration/all/nextcloud-talk';
 
 // Deprecated integration
 import ZwaveNodePage from '../routes/integration/all/zwave/node-page';
@@ -208,6 +209,11 @@ const AppRouter = connect(
         <IntegrationPage path="/dashboard/integration/navigation" category="navigation" />
 
         <TelegramPage path="/dashboard/integration/communication/telegram" />
+        <Redirect
+          path="/dashboard/integration/communication/nextcloudtalk"
+          to="/dashboard/integration/communication/nextcloud-talk"
+        />
+        <NextcloudTalkPage path="/dashboard/integration/communication/nextcloud-talk" />
         <Redirect path="/dashboard/integration/calendar/caldav" to="/dashboard/integration/calendar/caldav/account" />
         <CalDAVAccountPage path="/dashboard/integration/calendar/caldav/account" />
         <CalDAVSyncPage path="/dashboard/integration/calendar/caldav/sync" />
