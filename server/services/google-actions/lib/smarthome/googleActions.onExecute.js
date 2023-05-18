b8f501b4e13dfc4d144cce518b9fde4ec5089227
atrovato <1839717+atrovato@users.noreply.github.com>
2023-05-18 21:18:29 +0200

    Optimize event emit and command management

--- server/services/google-actions/lib/smarthome/googleActions.onExecute.js
+++ server/services/google-actions/lib/smarthome/googleActions.onExecute.js
@@ -1,77 +1,18 @@
 const logger = require('../../../../utils/logger');
-const { EVENTS, ACTIONS, ACTIONS_STATUS } = require('../../../../utils/constants');
+const { ACTIONS, ACTIONS_STATUS, EVENTS } = require('../../../../utils/constants');
+
 const { TRAIT_BY_COMMAND } = require('../traits');
-
-const execSubCommand = (
-  command,
-  params,
-  commands,
-  requestedDeviceIds,
-  errorneousDevices,
-  pendingDevices,
-  gladys,
-  parentParam = '',
-) => {
-  Object.keys(params).forEach((param) => {
-    const paramValue = params[param];
-
-    if (typeof paramValue === 'object') {
-      execSubCommand(
-        command,
-        paramValue,
-        commands,
-        requestedDeviceIds,
-        errorneousDevices,
-        pendingDevices,
-        gladys,
-        `${parentParam}${param}.`,
-      );
-    } else {
-      const valueFunc = commands[parentParam + param];
-      if (param === 'name') {
-        logger.debug(`Google Actions: "name" is not a command, doing nothing`);
-      } else if (!valueFunc) {
-        // Param command key not found
-        logger.error(`GoogleActions "${command} -> ${parentParam}${param}" command is not managed.`);
-        // All devices are failure
-        requestedDeviceIds.forEach((requestedDevice) => {
-          errorneousDevices.push(requestedDevice);
-        });
-      } else {
-        // If value function exists, update Gladys device
-        const value = valueFunc.writeValue(paramValue, param);
-        requestedDeviceIds.forEach((requestedDevice) => {
-          valueFunc.features.forEach((f) => {
-            const { category, type } = f;
-            // Send execute command to Gladys
-            const action = {
-              type: ACTIONS.DEVICE.SET_VALUE,
-              status: ACTIONS_STATUS.PENDING,
-              value,
-              device: requestedDevice,
-              feature_category: category,
-              feature_type: type,
-            };
-            gladys.event.emit(EVENTS.ACTION.TRIGGERED, action);
-          });
-          pendingDevices.push(requestedDevice);
-        });
-      }
-    }
-  });
-};
 
 /**
  * @description The function that will run for an EXECUTE request.
  * It should return a valid response or a Promise that resolves to valid response.
  * @param {object} body - Request body.
- * @param {object} headers - Request headers.
- * @returns {Promise} A valid response.
+ * @returns {object} A valid response.
  * @example
- * googleActions.onExecute({}, {});
+ * googleActions.onExecute({});
  * @see https://actions-on-google.github.io/actions-on-google-nodejs/interfaces/smarthome.smarthomeapp.html#onexecute
  */
-async function onExecute(body, headers) {
+function onExecute(body) {
   // Get requested Gladys devices
   const pendingDevices = [];
   const errorneousDevices = [];
@@ -85,33 +26,52 @@ async function onExecute(body, headers) {
         })
         .forEach((command) => {
           const { devices, execution } = command;
-          const requestedDeviceIds = devices.map((d) => d.id);
+          const requestedDevices = devices
+            .map((device) => {
+              // Load related device
+              const gladysDevice = this.gladys.stateManager.get('device', device.id);
 
-          // Each execution triggered
-          execution.forEach((exec) => {
-            const trait = TRAIT_BY_COMMAND[exec.command];
+              if (!gladysDevice) {
+                errorneousDevices.push(device.id);
+              }
 
-            if (!trait || !trait.commands[exec.command]) {
-              // Command key not found
-              logger.error(`GoogleActions "${exec.command}" command is not managed.`);
-              // All devices are failure
-              requestedDeviceIds.forEach((requestedDevice) => {
-                errorneousDevices.push(requestedDevice);
-              });
-            } else {
-              const commands = trait.commands[exec.command];
+              return gladysDevice;
+            })
+            .filter(Boolean);
 
-              // Each sub-commands
-              execSubCommand(
-                exec.command,
-                exec.params,
-                commands,
-                requestedDeviceIds,
-                errorneousDevices,
-                pendingDevices,
-                this.gladys,
-              );
-            }
+          requestedDevices.forEach((device) => {
+            // Each execution triggered
+            execution.forEach((exec) => {
+              const trait = TRAIT_BY_COMMAND[exec.command];
+
+              if (!trait || !trait.commands[exec.command]) {
+                // Command key not found
+                logger.error(`GoogleActions "${exec.command}" command is not managed.`);
+                // All devices are failure
+                errorneousDevices.push(device.selector);
+              } else {
+                const commandExecutor = trait.commands[exec.command];
+
+                // Build related feature events according incomping attributes
+                const eventMessages = commandExecutor(device, exec.params);
+
+                if (!eventMessages || !eventMessages.length) {
+                  errorneousDevices.push(device.selector);
+                } else {
+                  eventMessages.forEach((eventMessage) => {
+                    const action = {
+                      type: ACTIONS.DEVICE.SET_VALUE,
+                      status: ACTIONS_STATUS.PENDING,
+                      device: device.selector,
+                      ...eventMessage,
+                    };
+                    this.gladys.event.emit(EVENTS.ACTION.TRIGGERED, action);
+                  });
+
+                  pendingDevices.push(device.selector);
+                }
+              }
+            });
           });
         });
     });
