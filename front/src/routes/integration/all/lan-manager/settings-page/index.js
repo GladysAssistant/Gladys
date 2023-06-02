c38904032cbf8038f20acd62e141271bd86d6a16
Alexandre Trovato <1839717+atrovato@users.noreply.github.com>
2023-06-02 07:30:04 +0200

    Front: Fix eslint "components as pure function" warning (#1798)

--- front/src/routes/integration/all/lan-manager/settings-page/index.js
+++ front/src/routes/integration/all/lan-manager/settings-page/index.js
@@ -1,17 +1,12 @@
-import { Component } from 'preact';
 import { connect } from 'unistore/preact';
 
 import LANManagerPage from '../LANManagerPage';
 import LANManagerSettingsTab from './LANManagerSettingsTab';
 
-class LANManagerSettingsPage extends Component {
-  render(props) {
-    return (
-      <LANManagerPage>
-        <LANManagerSettingsTab {...props} />
-      </LANManagerPage>
-    );
-  }
-}
+const LANManagerSettingsPage = props => (
+  <LANManagerPage>
+    <LANManagerSettingsTab {...props} />
+  </LANManagerPage>
+);
 
 export default connect('user,httpClient')(LANManagerSettingsPage);
