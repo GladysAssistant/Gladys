b715195e9842270f3e254218218aefaaf8e4bf51
callemand <corentin.allemand@gmail.com>
2023-07-04 10:38:29 +0200

    Prettier

--- server/services/tuya/lib/device/tuya.convertDevice.js
+++ server/services/tuya/lib/device/tuya.convertDevice.js
@@ -14,7 +14,6 @@ function convertDevice(tuyaDevice) {
   const externalId = `tuya:${id}`;
   const { functions = [] } = specifications;
 
-
   logger.warn(`Tuya convert device"${name}, ${model}"`);
   // Groups functions and status on same code
   const groups = {};
