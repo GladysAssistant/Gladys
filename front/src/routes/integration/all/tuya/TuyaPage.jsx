f01a082711ef26141e18288322843d6f972b54cd
callemand <corentin.allemand@gmail.com>
2023-07-06 10:25:09 +0200

    Add documentation tab

--- front/src/routes/integration/all/tuya/TuyaPage.jsx
+++ front/src/routes/integration/all/tuya/TuyaPage.jsx
@@ -1,7 +1,8 @@
 import { Text } from 'preact-i18n';
 import { Link } from 'preact-router/match';
+import DeviceConfigurationLink from '../../../../components/documentation/DeviceConfigurationLink';
 
-const TuyaPage = ({ children }) => (
+const TuyaPage = ({ children, user }) => (
   <div class="page">
     <div class="page-main">
       <div class="my-3 my-md-5">
@@ -45,6 +46,16 @@ const TuyaPage = ({ children }) => (
                     </span>
                     <Text id="integration.tuya.setupTab" />
                   </Link>
+                  <DeviceConfigurationLink
+                    user={user}
+                    documentKey="tuya"
+                    linkClass="list-group-item list-group-item-action d-flex align-items-center"
+                  >
+                    <span class="icon mr-3">
+                      <i class="fe fe-book-open" />
+                    </span>
+                    <Text id="integration.tuya.documentation" />
+                  </DeviceConfigurationLink>
                 </div>
               </div>
             </div>
