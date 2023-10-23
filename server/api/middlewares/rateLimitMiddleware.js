2b162f1b24c507d4bb6cf43ab3702f7e66e41bc8
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2023-10-23 12:01:53 +0200

    Reduce rate limit

--- server/api/middlewares/rateLimitMiddleware.js
+++ server/api/middlewares/rateLimitMiddleware.js
@@ -2,8 +2,8 @@ const rateLimit = require('express-rate-limit');
 
 // @ts-ignore
 const limiter = rateLimit({
-  windowMs: 30 * 60 * 1000, // 30 minutes
-  max: 20, // limit each IP to 20 requests per windowMs
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 100, // limit each IP to 100 requests
 });
 
 module.exports = limiter;
