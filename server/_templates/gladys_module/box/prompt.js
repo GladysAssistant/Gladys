af62936cbbfc60eb9d31c21af5561cb0de32d3d9
ngeissel <nicolas.geissel@gmail.com>
2023-12-06 13:29:12 +0100

    If params are given in CLI, do not ask for them in prompt. Thanks to this thread : https://github.com/jondot/hygen/issues/35

--- server/_templates/gladys_module/box/prompt.js
+++ server/_templates/gladys_module/box/prompt.js
@@ -1,37 +1,32 @@
 // see types of prompts:
 // https://github.com/enquirer/enquirer/tree/master/examples
-//
+// see advanced :
+// https://github.com/jondot/hygen/issues/35
+const promptArgs = require('../../promptArgsUtil');
+
+const questions = [
+  {
+    type: 'input',
+    name: 'module',
+    message: "What's your module name (ie:androidtv)? It will create the service folder (ie: services/androidtv)",
+  },
+  {
+    type: 'input',
+    name: 'className',
+    message: 'Used in Service, Handler and Controller name as prefix (ie: AndroidTv => AndroidTvService):',
+  },
+  {
+    type: 'input',
+    name: 'attributeName',
+    message: 'Used as prefix for attributes (ie: androidtv => androidtvManager):',
+  },
+  {
+    type: 'input',
+    name: 'constName',
+    message: 'Used as prefix for constants (ie: androidtv => ANDROIDTV):',
+  },
+];
+
 module.exports = {
-  params: ({ args }) => {
-    return {
-      module: args.module,
-      className: args.className,
-      attributeName: args.attributeName,
-      constName: args.constName,
-    };
-  },
-  prompt: ({ inquirer, args, inflection }) => {
-    return inquirer.prompt([
-      {
-        type: 'input',
-        name: 'module',
-        message: "What's your module name (ie:androidtv)? It will create the service folder (ie: services/androidtv)",
-      },
-      {
-        type: 'input',
-        name: 'className',
-        message: 'Used in Service, Handler and Controller name as prefix (ie: AndroidTv => AndroidTvService):',
-      },
-      {
-        type: 'input',
-        name: 'attributeName',
-        message: 'Used as prefix for attributes (ie: androidtv => androidtvManager):',
-      },
-      {
-        type: 'input',
-        name: 'constName',
-        message: 'Used as prefix for constants (ie: androidtv => ANDROIDTV):',
-      },
-    ]);
-  },
+  prompt: promptArgs(questions)
 };
---
+++ server/_templates/promptArgsUtil.js
@@ -0,0 +1,10 @@
+module.exports = questions => ({ prompter, args }) => {
+  const providedArgs = questions.reduce((selectedArgs, question) => {
+    if (args[question.name]) selectedArgs[question.name] = args[question.name];
+    return selectedArgs;
+  }, {});
+  return prompter
+    .prompt(questions.filter(({ name }) => !providedArgs[name]))
+    // See https://github.com/jondot/hygen/issues/35#issuecomment-594718170
+    // .then(answers => ({ ...answers, ...providedArgs }));
+};
