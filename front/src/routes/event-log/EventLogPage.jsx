8b41606e048780b21a76cf26a93ad815ec0b5f52
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2023-12-06 09:32:17 +0100

    add style.css file

--- front/src/routes/event-log/EventLogPage.jsx
+++ front/src/routes/event-log/EventLogPage.jsx
@@ -2,6 +2,8 @@ import { Text } from 'preact-i18n';
 import { Link } from 'preact-router/match';
 import cx from 'classnames';
 
+import style from './style.css';
+
 const EventLogPage = ({children,...props }) => (
     <div>coucou</div>);
 
