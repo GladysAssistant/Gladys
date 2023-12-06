d916b81365b4fce796a51972918ff1d295d6651e
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2023-12-06 09:07:30 +0100

    test eventlogpage

---
+++ front/src/routes/event-log/EventLogPage.jsx
@@ -0,0 +1,8 @@
+import { Text } from 'preact-i18n';
+import { Link } from 'preact-router/match';
+import cx from 'classnames';
+
+const EventLogPage = ({...props }) => (
+    <div>coucou</div>);
+
+export default EventLogPage;
