ff3fbd9e6c730da25d17ed959c624edb1fcded92
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2023-12-08 13:31:55 +0100

    Remove console.log

--- server/services/sonos/lib/sonos.setValue.js
+++ server/services/sonos/lib/sonos.setValue.js
@@ -27,12 +27,10 @@ async function setValue(device, deviceFeature, value) {
   }
 
   if (deviceFeature.type === DEVICE_FEATURE_TYPES.MUSIC.PLAY_NOTIFICATION) {
-    console.log(deviceFeature);
-    console.log(value);
     await sonosDevice.PlayNotification({
       trackUri: value,
       onlyWhenPlaying: false,
-      volume: 25, // Set the volume for the notification (and revert back afterwards)
+      volume: 30, // Set the volume for the notification (and revert back afterwards)
       timeout: 10,
     });
   }
