e74d9ed0dafe8155dfd71d3df744b3609691d508
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2023-12-12 11:09:54 +0100

    first logBox

--- front/src/components/log/logBox.jsx
+++ front/src/components/log/logBox.jsx
@@ -1,11 +1,28 @@
 import { Text } from 'preact-i18n';
 import { Component } from 'preact';
+import dayjs from 'dayjs';
 import style from './style.css';
 
-class logBox extends component {
+class logBox extends Component {
+    date = "2019-11-11";
+    time = "11:11:11";
+
+    type = "porte";
+
+    title = "ouverture de porte";
+    description = "qqn a ouvert la porte de la maison"
 
 
 
     render(props) {
-}
+        // Your render logic goes here
+        <div class = "card"> 
+            <div class= "card-header">
+                <h>bonjour</h>
+            </div>
+            <div class = "card-body">
+
+            </div>
+        </div>
+    }
 }
