ee5838e8377318359db32687629a4581939e0402
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2023-12-14 19:02:16 +0100

    date title incon settings for log

--- front/src/components/log/logBox.jsx
+++ front/src/components/log/logBox.jsx
@@ -7,22 +7,43 @@ class LogBox extends Component {
     date = "2019-11-11";
     time = "11:11:11";
 
-    type = "porte";
-
     title = "ouverture de porte";
     description = "qqn a ouvert la porte de la maison"
 
+    icon = "fe fe-home";
+
+    dico_icon = {
+        House : "fe fe-home",
+    }
     
 
+    //"2019-11-11 11:11:11.111 fuseau horaire"
+    setDateAndTime(date){
+        this.date = date.substring(0,10);
+        this.time = date.substring(11,19);
+    }
+
+    setIcon(service){
+        this.icon = this.dico_icon[type];
+    }
+
+    setTitle(service,type){
+        this.title = "EventLog.event."+service+"."+type+".title";
+    }
 
 
-    render(props, {date, type, action, object,eventProperty}) {
-        // Your render logic goes here
 
+
+
+    render(props, {date, service, type, sender_name,eventProperty}) {
+        // penser à decommanter 
+        //this.setDateAndTime(date);
+        //this.setIcon(service);
+        //this.setTitle(service,type);
         return(
        
         <div class = "card" > 
-            <div class= "card-header ">
+            <div class= "card-header " style = "background-color : #E0EBFF">
                 <div class='box'  style="flex-grow: 1; max-width:10%;">
                     <i class="fe fe-message-square" />
                 </div>
@@ -37,8 +58,14 @@ class LogBox extends Component {
                 </div>
             </div>
             <div class = "card-body">
-                {this.description}
+                <div class='box'  style="border-bottom : solid 1px grey; display : inline-block">
+                    {this.time}
+                
+                </div>
 
+                <div class='box'  style="flex-grow: 1; padding-top : 10px">
+                    {this.description}
+                </div>
             </div>
             
             
