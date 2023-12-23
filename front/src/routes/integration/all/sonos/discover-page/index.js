dd13a5d0217f7ce64b5944a5ba9d9acf8d00a7d6
atrovato <1839717+atrovato@users.noreply.github.com>
2023-12-23 19:51:22 +0100

    Fix react/prefer-stateless-function

--- front/src/routes/integration/all/sonos/discover-page/index.js
+++ front/src/routes/integration/all/sonos/discover-page/index.js
@@ -1,16 +1,11 @@
-import { Component } from 'preact';
 import { connect } from 'unistore/preact';
 import DiscoverTab from './DiscoverTab';
 import SonosPage from '../SonosPage';
 
-class SonosDiscoverPage extends Component {
-  render(props) {
-    return (
-      <SonosPage user={props.user}>
-        <DiscoverTab {...props} />
-      </SonosPage>
-    );
-  }
-}
+const SonosDiscoverPage = props => (
+  <SonosPage user={props.user}>
+    <DiscoverTab {...props} />
+  </SonosPage>
+);
 
 export default connect('user', {})(SonosDiscoverPage);
