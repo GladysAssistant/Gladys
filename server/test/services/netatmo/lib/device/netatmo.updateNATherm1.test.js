c562320509e6aada29ac9cb1c6c4f802ee069fcd
Terdious <thomas.lemaistre76@gmail.com>
2024-01-30 06:32:08 +0100

    fake: push with error to relaunch codecov

--- server/test/services/netatmo/lib/device/netatmo.updateNATherm1.test.js
+++ server/test/services/netatmo/lib/device/netatmo.updateNATherm1.test.js
@@ -21,7 +21,7 @@ const serviceId = 'serviceId';
 
 const netatmoHandler = new NetatmoHandler(gladys, serviceId);
 
-describe('Netatmo update NATherm1 features', () => {
+describe.only('Netatmo update NATherm1 features', () => {
   const deviceGladys = devicesGladys[1];
   const deviceNetatmoNATherm1 = JSON.parse(JSON.stringify(devicesNetatmo[1]));
   const externalIdNATherm1 = `netatmo:${devicesNetatmo[1].id}`;
