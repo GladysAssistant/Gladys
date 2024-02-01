617cd653d7e4e87cf9af6b4c9b02ec7c4f738d40
Terdious <thomas.lemaistre76@gmail.com>
2024-02-01 16:57:47 +0100

    refactor: Update Netatmo device rain feature and add tests

---
+++ server/test/services/netatmo/lib/device/netatmo.updateNAModule3.test.js
@@ -0,0 +1,96 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const { fake } = sinon;
+
+const devicesGladys = require('../../netatmo.convertDevices.mock.test.json');
+const devicesNetatmo = require('../../netatmo.loadDevices.mock.test.json');
+const { EVENTS } = require('../../../../../utils/constants');
+const NetatmoHandler = require('../../../../../services/netatmo/lib/index');
+const logger = require('../../../../../utils/logger');
+
+const gladys = {
+  event: {
+    emit: fake.resolves(null),
+  },
+  variable: {
+    setValue: fake.resolves(null),
+  },
+};
+const serviceId = 'serviceId';
+
+const netatmoHandler = new NetatmoHandler(gladys, serviceId);
+
+describe('Netatmo update NAModule3 features', () => {
+  const deviceGladysNAModule3 = devicesGladys[4];
+  const deviceNetatmoNAModule3 = JSON.parse(JSON.stringify(devicesNetatmo[4]));
+  const externalIdNAModule3 = `netatmo:${devicesNetatmo[4].id}`;
+  beforeEach(() => {
+    sinon.reset();
+
+    netatmoHandler.status = 'not_initialized';
+  });
+
+  afterEach(() => {
+    sinon.reset();
+  });
+
+  it('should save all values according to all cases', async () => {
+    await netatmoHandler.updateNAModule3(deviceGladysNAModule3, deviceNetatmoNAModule3, externalIdNAModule3);
+
+    expect(netatmoHandler.gladys.event.emit.callCount).to.equal(5);
+    sinon.assert.calledWith(netatmoHandler.gladys.event.emit, 'device.new-state', {
+      device_feature_external_id: `${deviceGladysNAModule3.external_id}:battery_percent`,
+      state: 52,
+    });
+    expect(
+      netatmoHandler.gladys.event.emit.getCall(0).calledWith(EVENTS.DEVICE.NEW_STATE, {
+        device_feature_external_id: 'netatmo:05:00:00:yy:yy:yy:battery_percent',
+        state: 52,
+      }),
+    ).to.equal(true);
+    expect(
+      netatmoHandler.gladys.event.emit.getCall(1).calledWith(EVENTS.DEVICE.NEW_STATE, {
+        device_feature_external_id: 'netatmo:05:00:00:yy:yy:yy:rain',
+        state: 0,
+      }),
+    ).to.equal(true);
+    expect(
+      netatmoHandler.gladys.event.emit.getCall(2).calledWith(EVENTS.DEVICE.NEW_STATE, {
+        device_feature_external_id: 'netatmo:05:00:00:yy:yy:yy:sum_rain_1',
+        state: 0,
+      }),
+    ).to.equal(true);
+    expect(
+      netatmoHandler.gladys.event.emit.getCall(3).calledWith(EVENTS.DEVICE.NEW_STATE, {
+        device_feature_external_id: 'netatmo:05:00:00:yy:yy:yy:sum_rain_24',
+        state: 0.1,
+      }),
+    ).to.equal(true);
+    expect(
+      netatmoHandler.gladys.event.emit.getCall(4).calledWith(EVENTS.DEVICE.NEW_STATE, {
+        device_feature_external_id: 'netatmo:05:00:00:yy:yy:yy:rf_strength',
+        state: 96,
+      }),
+    ).to.equal(true);
+  });
+  it('should handle errors correctly', async () => {
+    deviceNetatmoNAModule3.battery_percent = undefined;
+    const error = new Error('Test error');
+    netatmoHandler.gladys = {
+      event: {
+        emit: sinon.stub().throws(error),
+      },
+    };
+    sinon.stub(logger, 'error');
+
+    try {
+      await netatmoHandler.updateNAModule3(deviceGladysNAModule3, deviceNetatmoNAModule3, externalIdNAModule3);
+    } catch (e) {
+      expect(e).to.equal(error);
+      sinon.assert.calledOnce(logger.error);
+    }
+
+    logger.error.restore();
+  });
+});
