825ffb16136b35e74e89164551f2a982a7a40c7f
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2024-04-21 13:50:35 +0200

    Zigbee2mqtt : Remove duplicate features in getDiscoveredDevices to fix bug on Aqara motion sensor

--- server/test/services/zigbee2mqtt/lib/getDiscoveredDevices.test.js
+++ server/test/services/zigbee2mqtt/lib/getDiscoveredDevices.test.js
@@ -6,6 +6,8 @@ const Zigbee2MqttService = require('../../../../services/zigbee2mqtt');
 const discoveredDevices = require('./payloads/mqtt_devices_get.json');
 const expectedDevicesPayload = require('./payloads/event_device_result.json');
 
+const aqaraWithDuplicateFeatureDevice = require('./payloads/aqara_with_duplicate_feature_mqtt.json');
+
 const gladys = {
   job: {
     wrapper: (type, func) => {
@@ -55,6 +57,32 @@ describe('zigbee2mqtt getDiscoveredDevices', () => {
     const devices = zigbee2MqttService.device.getDiscoveredDevices();
     // ASSERT
     expect(devices).deep.eq(expectedDevicesPayload);
+  });
+
+  it('get discovered devices without duplicate features', async () => {
+    // PREPARE
+    gladys.stateManager.get
+      .onFirstCall()
+      .returns({ id: 'gladys-id', room_id: 'room_id', name: 'device-name' })
+      .onSecondCall()
+      .returns(expectedDevicesPayload[3])
+      .onThirdCall()
+      .returns(false);
+
+    aqaraWithDuplicateFeatureDevice
+      .filter((d) => d.supported)
+      .forEach((device) => {
+        zigbee2MqttService.device.discoveredDevices[device.friendly_name] = device;
+      });
+
+    // EXECUTE
+    const devices = zigbee2MqttService.device.getDiscoveredDevices();
+    const aqaraDevice = devices.find((d) => d.model === 'WSDCGQ11LM');
+    // ASSERT
+    expect(aqaraDevice.features.filter((f) => f.category === 'humidity-sensor')).to.have.lengthOf(
+      1,
+      'should not have duplicate humidity',
+    );
   });
 
   it('filter discovered devices', async () => {
