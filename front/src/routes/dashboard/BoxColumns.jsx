68a7eecad6dad62c8cd2c74a74c4214139bd0baa
Terdious <thomas.lemaistre76@gmail.com>
2024-06-13 13:43:11 +0200

    fix: review

--- front/src/routes/dashboard/BoxColumns.jsx
+++ front/src/routes/dashboard/BoxColumns.jsx
@@ -3,8 +3,8 @@ import cx from 'classnames';
 import style from './style.css';
 
 const BoxColumns = ({ children, ...props }) => {
-  const lengthBoxes = props.homeDashboard.boxes.length;
-  const columnClass = `col-lg-${12 / lengthBoxes}`;
+  const boxesLength = props.homeDashboard.boxes.length;
+  const columnClass = `col-lg-${12 / boxesLength}`;
 
   return (
     <div class="d-flex flex-row flex-wrap justify-content-center">
@@ -13,7 +13,7 @@ const BoxColumns = ({ children, ...props }) => {
           <div
             class={cx('d-flex flex-column', columnClass, style.removePadding, {
               [style.removePaddingFirstCol]: x === 0,
-              [style.removePaddingLastCol]: x === lengthBoxes - 1,
+              [style.removePaddingLastCol]: x === boxesLength - 1,
               [style.removePadding]: true // it will remove padding when in mobile view
             })}
           >
