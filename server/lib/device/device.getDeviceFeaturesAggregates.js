8343bfe19026bb2bd2b77745a2d7d95163b0508f
Terdious <thomas.lemaistre76@gmail.com>
2024-09-02 12:34:32 +0200

    feat: Add support for start_date and end_date in device feature aggregates

--- server/lib/device/device.getDeviceFeaturesAggregates.js
+++ server/lib/device/device.getDeviceFeaturesAggregates.js
@@ -6,11 +6,19 @@ const { NotFoundError } = require('../../utils/coreErrors');
  * @param {string} selector - Device selector.
  * @param {number} intervalInMinutes - Interval.
  * @param {number} maxStates - Number of elements to return max.
+ * @param {Date} startDate - Start date.
+ * @param {Date} endDate - End date.
  * @returns {Promise<object>} - Resolve with an array of data.
  * @example
- * device.getDeviceFeaturesAggregates('test-devivce');
+ * device.getDeviceFeaturesAggregates('test-device');
  */
-async function getDeviceFeaturesAggregates(selector, intervalInMinutes, maxStates = 100) {
+async function getDeviceFeaturesAggregates(
+  selector,
+  intervalInMinutes,
+  maxStates = 100,
+  startDate = null,
+  endDate = null,
+) {
   const deviceFeature = this.stateManager.get('deviceFeature', selector);
   if (deviceFeature === null) {
     throw new NotFoundError('DeviceFeature not found');
@@ -18,33 +26,55 @@ async function getDeviceFeaturesAggregates(selector, intervalInMinutes, maxState
   const device = this.stateManager.get('deviceById', deviceFeature.device_id);
 
   const now = new Date();
-  const intervalDate = new Date(now.getTime() - intervalInMinutes * 60 * 1000);
+  // const intervalDate = new Date(now.getTime() - intervalInMinutes * 60 * 1000);
+  let intervalDate;
+  let newStartDate = startDate;
+  let newEndDate = endDate;
+  if (startDate === null && endDate === null) {
+    intervalDate = new Date(now.getTime() - intervalInMinutes * 60 * 1000);
+  } else if (startDate !== null && endDate === null) {
+    intervalDate = new Date(new Date(startDate).getTime() + intervalInMinutes * 60 * 1000);
+    newEndDate = intervalDate;
+    intervalDate = new Date(startDate);
+  } else if (startDate === null && endDate !== null) {
+    intervalDate = new Date(new Date(endDate).getTime() - intervalInMinutes * 60 * 1000);
+    newStartDate = intervalDate;
+    intervalDate = new Date(endDate);
+  } else {
+    intervalDate = new Date(startDate);
+  }
 
   const values = await db.duckDbReadConnectionAllAsync(
     `
-    WITH intervals AS (
-          SELECT
-              created_at,
-              value,
-              NTILE(?) OVER (ORDER BY created_at) AS interval
-          FROM
-              t_device_feature_state
-          WHERE device_feature_id = ?
-          AND created_at > ?
-      )
-      SELECT
-          MIN(created_at) AS created_at,
-          AVG(value) AS value
-      FROM
-          intervals
-      GROUP BY
-          interval
-      ORDER BY
-          created_at;  
+  WITH intervals AS (
+        SELECT
+            created_at,
+            value,
+            NTILE(?) OVER (ORDER BY created_at) AS interval
+        FROM
+            t_device_feature_state
+        WHERE device_feature_id = ?
+        AND created_at > ?
+        ${startDate ? 'AND created_at >= ?' : ''}
+        ${endDate ? 'AND created_at <= ?' : ''}
+    )
+    SELECT
+        MIN(created_at) AS created_at,
+        AVG(value) AS value
+    FROM
+        intervals
+    GROUP BY
+        interval
+    ORDER BY
+        created_at;  
   `,
-    maxStates,
-    deviceFeature.id,
-    intervalDate,
+    ...[
+      maxStates,
+      deviceFeature.id,
+      intervalDate,
+      ...(startDate ? [new Date(startDate)] : []),
+      ...(endDate ? [new Date(endDate)] : [])
+    ]
   );
 
   return {
--- server/lib/device/device.getDeviceFeaturesAggregatesMulti.js
+++ server/lib/device/device.getDeviceFeaturesAggregatesMulti.js
@@ -4,15 +4,23 @@ const Promise = require('bluebird');
  * @param {Array} selectors - Array of device feature selectors.
  * @param {number} intervalInMinutes - Interval.
  * @param {number} maxStates - Number of elements to return max.
+ * @param {Date} startDate - Start date.
+ * @param {Date} endDate - End date.
  * @returns {Promise<Array>} - Resolve with an array of array of data.
  * @example
  * device.getDeviceFeaturesAggregates('test-devivce');
  */
-async function getDeviceFeaturesAggregatesMulti(selectors, intervalInMinutes, maxStates = 100) {
+async function getDeviceFeaturesAggregatesMulti(
+  selectors,
+  intervalInMinutes,
+  maxStates = 100,
+  startDate = null,
+  endDate = null,
+) {
   return Promise.map(
     selectors,
     async (selector) => {
-      return this.getDeviceFeaturesAggregates(selector, intervalInMinutes, maxStates);
+      return this.getDeviceFeaturesAggregates(selector, intervalInMinutes, maxStates, startDate, endDate);
     },
     { concurrency: 4 },
   );
