cc978bc17780cf80935ccd1d4642556152017909
William Deren <williamderen@gmail.com>
2024-10-24 23:49:17 +0200

    add path of docker command

--- server/lib/system/system.updateContainers.js
+++ server/lib/system/system.updateContainers.js
@@ -12,7 +12,7 @@ async function updateContainers() {
     throw new PlatformNotCompatible('SYSTEM_NOT_RUNNING_DOCKER');
   }
 
-  const command = `docker run --rm -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --cleanup --include-restarting --run-once`;
+  const command = `/usr/bin/docker run --rm -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --cleanup --include-restarting --run-once`;
 
   logger.info(`Updating all containers`);
   await exec(command);
