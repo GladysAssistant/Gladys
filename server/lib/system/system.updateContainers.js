d59d9d49f4f0ccc91655ce824e3161c8225be843
William Deren <williamderen@gmail.com>
2024-10-26 15:18:30 +0200

    use docker API instead of exec function

--- server/lib/system/system.updateContainers.js
+++ server/lib/system/system.updateContainers.js
@@ -1,6 +1,6 @@
 const { PlatformNotCompatible } = require('../../utils/coreErrors');
 const logger = require('../../utils/logger');
-const { exec } = require('../../utils/childProcess');
+// const { exec } = require('../../utils/childProcess');
 
 /**
  * @description Update containers.
@@ -12,10 +12,31 @@ async function updateContainers() {
     throw new PlatformNotCompatible('SYSTEM_NOT_RUNNING_DOCKER');
   }
 
-  const command = `/usr/bin/docker run --rm -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --cleanup --include-restarting --run-once`;
+  // const command = `/usr/bin/docker run --rm -v /var/run/docker.sock:/var/run/docker.sock 
+  // containrrr/watchtower --cleanup --include-restarting --run-once`;
 
-  logger.info(`Updating all containers`);
-  await exec(command);
+  // logger.info(`Updating all containers`);
+  // await exec(command);
+
+
+  const container = await this.dockerode.createContainer({
+    Image: 'containrrr/watchtower',
+    Cmd: ['--run-once', '--cleanup', '--include-restarting'],
+    HostConfig: {
+        AutoRemove: true,
+        Binds: [
+            '/var/run/docker.sock:/var/run/docker.sock' // Monté pour accéder à l'API Docker
+        ]
+    },
+    Tty: false
+  });
+
+  await container.start();
+  logger.info('Watchtower container is running.');
+
+  // Attendre que le conteneur se termine et le supprimer
+  await container.wait();
+  logger.info('Watchtower has completed its run.');
   
 }
 
