3e11cf33a4942409994ef3301efcb33138dae371
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2024-11-04 17:20:21 +0100

    Scene: Device state trigger can now check that condition was valid for X minutes

---
+++ server/test/lib/scene/triggers/scene.trigger.deviceNewState.test.js
@@ -0,0 +1,432 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const EventEmitter = require('events');
+const Promise = require('bluebird');
+
+const { assert, fake } = sinon;
+
+const { EVENTS, ACTIONS } = require('../../../../utils/constants');
+const SceneManager = require('../../../../lib/scene');
+const StateManager = require('../../../../lib/state');
+
+const event = new EventEmitter();
+
+describe('scene.triggers.deviceNewState', () => {
+  let sceneManager;
+
+  const device = {
+    setValue: fake.resolves(null),
+  };
+
+  const brain = {};
+
+  const service = {
+    getService: fake.returns({
+      device: {
+        subscribe: fake.returns(null),
+      },
+    }),
+  };
+
+  beforeEach(() => {
+    const house = {
+      get: fake.resolves([]),
+    };
+
+    const scheduler = {
+      scheduleJob: (date, callback) => {
+        return {
+          callback,
+          date,
+          cancel: () => {},
+        };
+      },
+    };
+
+    brain.addNamedEntity = fake.returns(null);
+    brain.removeNamedEntity = fake.returns(null);
+
+    const stateManager = new StateManager();
+    stateManager.setState('deviceFeature', 'light-1', {
+      last_value: 14,
+    });
+    sceneManager = new SceneManager(stateManager, event, device, {}, {}, house, {}, {}, {}, scheduler, brain, service);
+  });
+
+  afterEach(() => {
+    sinon.reset();
+  });
+
+  it('should execute scene', async () => {
+    sceneManager.addScene({
+      selector: 'my-scene',
+      active: true,
+      actions: [
+        [
+          {
+            type: ACTIONS.LIGHT.TURN_ON,
+            devices: ['light-1'],
+          },
+        ],
+      ],
+      triggers: [
+        {
+          type: EVENTS.DEVICE.NEW_STATE,
+          device_feature: 'light-1',
+          value: 12,
+          operator: '=',
+        },
+      ],
+    });
+    sceneManager.checkTrigger({
+      type: EVENTS.DEVICE.NEW_STATE,
+      device_feature: 'light-1',
+      last_value: 12,
+    });
+    return new Promise((resolve, reject) => {
+      sceneManager.queue.start(() => {
+        try {
+          assert.calledOnce(device.setValue);
+          resolve();
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  });
+  it('should not execute scene, scene not active', async () => {
+    sceneManager.addScene({
+      selector: 'my-scene',
+      active: false,
+      actions: [
+        [
+          {
+            type: ACTIONS.LIGHT.TURN_ON,
+            devices: ['light-1'],
+          },
+        ],
+      ],
+      triggers: [
+        {
+          type: EVENTS.DEVICE.NEW_STATE,
+          device_feature: 'light-1',
+          value: 12,
+          operator: '=',
+        },
+      ],
+    });
+    sceneManager.checkTrigger({
+      type: EVENTS.DEVICE.NEW_STATE,
+      device_feature: 'light-1',
+      last_value: 12,
+    });
+    return new Promise((resolve, reject) => {
+      sceneManager.queue.start(() => {
+        try {
+          assert.notCalled(device.setValue);
+          resolve();
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  });
+  it('should not execute scene, condition not verified', async () => {
+    sceneManager.addScene({
+      selector: 'my-scene',
+      active: true,
+      actions: [
+        [
+          {
+            type: ACTIONS.LIGHT.TURN_ON,
+            devices: ['light-1'],
+          },
+        ],
+      ],
+      triggers: [
+        {
+          type: EVENTS.DEVICE.NEW_STATE,
+          device_feature: 'light-1',
+          value: 12,
+          operator: '=',
+        },
+      ],
+    });
+    sceneManager.checkTrigger({
+      type: EVENTS.DEVICE.NEW_STATE,
+      device_feature: 'light-1',
+      last_value: 14,
+    });
+    return new Promise((resolve, reject) => {
+      sceneManager.queue.start(() => {
+        try {
+          assert.notCalled(device.setValue);
+          resolve();
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  });
+  it('should not execute scene, threshold already passed', async () => {
+    sceneManager.addScene({
+      selector: 'my-scene',
+      active: true,
+      actions: [
+        [
+          {
+            type: ACTIONS.LIGHT.TURN_ON,
+            devices: ['light-1'],
+          },
+        ],
+      ],
+      triggers: [
+        {
+          type: EVENTS.DEVICE.NEW_STATE,
+          device_feature: 'light-1',
+          value: 12,
+          operator: '>',
+          threshold_only: true,
+        },
+      ],
+    });
+    sceneManager.checkTrigger({
+      type: EVENTS.DEVICE.NEW_STATE,
+      device_feature: 'light-1',
+      previous_value: 14,
+      last_value: 14,
+    });
+    return new Promise((resolve, reject) => {
+      sceneManager.queue.start(() => {
+        try {
+          assert.notCalled(device.setValue);
+          resolve();
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  });
+  it('should execute scene, threshold passed for the first time', async () => {
+    sceneManager.addScene({
+      selector: 'my-scene',
+      active: true,
+      actions: [
+        [
+          {
+            type: ACTIONS.LIGHT.TURN_ON,
+            devices: ['light-1'],
+          },
+        ],
+      ],
+      triggers: [
+        {
+          type: EVENTS.DEVICE.NEW_STATE,
+          device_feature: 'light-1',
+          value: 12,
+          operator: '>',
+          threshold_only: true,
+        },
+      ],
+    });
+    sceneManager.checkTrigger({
+      type: EVENTS.DEVICE.NEW_STATE,
+      device_feature: 'light-1',
+      previous_value: 11,
+      last_value: 14,
+    });
+    return new Promise((resolve, reject) => {
+      sceneManager.queue.start(() => {
+        try {
+          assert.calledOnce(device.setValue);
+          resolve();
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  });
+  it('should start timer to check later for state and not follow current scene', async () => {
+    sceneManager.addScene({
+      selector: 'my-scene',
+      active: true,
+      actions: [
+        [
+          {
+            type: ACTIONS.LIGHT.TURN_ON,
+            devices: ['light-1'],
+          },
+        ],
+      ],
+      triggers: [
+        {
+          type: EVENTS.DEVICE.NEW_STATE,
+          device_feature: 'light-1',
+          value: 12,
+          operator: '>',
+          threshold_only: true,
+          for_duration: 10 * 60 * 1000,
+        },
+      ],
+    });
+    sceneManager.checkTrigger({
+      type: EVENTS.DEVICE.NEW_STATE,
+      device_feature: 'light-1',
+      previous_value: 11,
+      last_value: 14,
+    });
+    return new Promise((resolve, reject) => {
+      sceneManager.queue.start(() => {
+        try {
+          assert.notCalled(device.setValue);
+          expect(sceneManager.checkTriggersDurationTimer.size).to.equal(1);
+          sceneManager.checkTriggersDurationTimer.forEach((value, timeoutKey) => {
+            expect(timeoutKey).to.equal('device.new-state.my-scene.light-1:>:12');
+            clearTimeout(value);
+          });
+          resolve();
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  });
+  it('should start timer to check now and condition should still be valid on second call', async () => {
+    sceneManager.addScene({
+      selector: 'my-scene',
+      active: true,
+      actions: [
+        [
+          {
+            type: ACTIONS.LIGHT.TURN_ON,
+            devices: ['light-1'],
+          },
+        ],
+      ],
+      triggers: [
+        {
+          type: EVENTS.DEVICE.NEW_STATE,
+          device_feature: 'light-1',
+          value: 12,
+          operator: '>',
+          threshold_only: true,
+          for_duration: 0, // now
+        },
+      ],
+    });
+    sceneManager.checkTrigger({
+      type: EVENTS.DEVICE.NEW_STATE,
+      device_feature: 'light-1',
+      previous_value: 11,
+      last_value: 14,
+    });
+    return new Promise((resolve, reject) => {
+      sceneManager.queue.start(async () => {
+        try {
+          await Promise.delay(5);
+          assert.calledOnce(device.setValue);
+          expect(sceneManager.checkTriggersDurationTimer.size).to.equal(0);
+          resolve();
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  });
+  it('should start timer to check now and condition should still be valid on second call', async () => {
+    sceneManager.addScene({
+      selector: 'my-scene',
+      active: true,
+      actions: [
+        [
+          {
+            type: ACTIONS.LIGHT.TURN_ON,
+            devices: ['light-1'],
+          },
+        ],
+      ],
+      triggers: [
+        {
+          type: EVENTS.DEVICE.NEW_STATE,
+          device_feature: 'light-1',
+          value: 12,
+          operator: '>',
+          threshold_only: false,
+          for_duration: 5,
+        },
+      ],
+    });
+    sceneManager.checkTrigger({
+      type: EVENTS.DEVICE.NEW_STATE,
+      device_feature: 'light-1',
+      previous_value: 11,
+      last_value: 14,
+    });
+    sceneManager.checkTrigger({
+      type: EVENTS.DEVICE.NEW_STATE,
+      device_feature: 'light-1',
+      previous_value: 14,
+      last_value: 14,
+    });
+    return new Promise((resolve, reject) => {
+      sceneManager.queue.start(async () => {
+        try {
+          await Promise.delay(10);
+          assert.calledOnce(device.setValue);
+          expect(sceneManager.checkTriggersDurationTimer.size).to.equal(0);
+          resolve();
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  });
+  it('should start timer to check now and condition should not be valid on second call', async () => {
+    sceneManager.addScene({
+      selector: 'my-scene',
+      active: true,
+      actions: [
+        [
+          {
+            type: ACTIONS.LIGHT.TURN_ON,
+            devices: ['light-1'],
+          },
+        ],
+      ],
+      triggers: [
+        {
+          type: EVENTS.DEVICE.NEW_STATE,
+          device_feature: 'light-1',
+          value: 12,
+          operator: '>',
+          threshold_only: true,
+          for_duration: 10, // In 10ms
+        },
+      ],
+    });
+    sceneManager.checkTrigger({
+      type: EVENTS.DEVICE.NEW_STATE,
+      device_feature: 'light-1',
+      previous_value: 11,
+      last_value: 14,
+    });
+    sceneManager.checkTrigger({
+      type: EVENTS.DEVICE.NEW_STATE,
+      device_feature: 'light-1',
+      previous_value: 14,
+      last_value: 5,
+    });
+    return new Promise((resolve, reject) => {
+      sceneManager.queue.start(async () => {
+        try {
+          await Promise.delay(5);
+          assert.notCalled(device.setValue);
+          expect(sceneManager.checkTriggersDurationTimer.size).to.equal(0);
+          resolve();
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  });
+});
