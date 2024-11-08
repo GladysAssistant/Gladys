36ee61ea384a7fe29cf6e5017fcc1b1bb3a115fc
Borna Nematzadeh <74822121+bnematzadeh@users.noreply.github.com>
2024-11-08 05:35:33 -0700

    Update user.test.js
    I modified this test and it passed successfully

--- server/test/security/user.test.js
+++ server/test/security/user.test.js
@@ -1,15 +1,17 @@
-const { expect } = require('chai');
-const { request, authenticatedRequest } = require('../controllers/request.test');
+const {
+  expect
+} = require('chai');
+const {
+  request,
+  authenticatedRequest
+} = require('../controllers/request.test');
 
-describe('/api/v1/user/', () => {
-  it('should return all users without password', async () => {
-    await authenticatedRequest
-      .get('/api/v1/user?fields=password')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .then((res)=>{
-        expect(res.body).to.not.have.key('password')
-      })
-   });
+describe('/api/v1/user/', () = >{
+  it('should return all users without password', async() = >{
+    await authenticatedRequest.get('/api/v1/user?fields=password').expect('Content-Type', /json/).expect(200).then((res) = >{
+      res.body.forEach((user) = >{
+        expect(user).to.not.have.property('password');
+      });
+    })
+  });
 })
-
