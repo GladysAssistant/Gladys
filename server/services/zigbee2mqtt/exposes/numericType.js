22c256cd3f924b09d15dc2fd6fffb853a343c4fa
Cyril Beslay <cicoub13@proton.me>
2025-03-17 22:37:28 +0100

    feat(z2m): Fix default unit for illuminance

--- server/services/zigbee2mqtt/exposes/numericType.js
+++ server/services/zigbee2mqtt/exposes/numericType.js
@@ -194,6 +194,7 @@ module.exports = {
       feature: {
         category: DEVICE_FEATURE_CATEGORIES.LIGHT_SENSOR,
         type: DEVICE_FEATURE_TYPES.SENSOR.DECIMAL,
+        unit: DEVICE_FEATURE_UNITS.LUX,
         min: 0,
         max: 100000,
       },
