72e53a23ffb3bf2c172f7c606578fa98f1836c63
ngeissel <nicolas.geissel@gmail.com>
2025-05-04 20:28:45 +0200

    Add a link button to http discovering when no device

--- front/src/routes/integration/all/nuki/device-page/EmptyState.jsx
+++ front/src/routes/integration/all/nuki/device-page/EmptyState.jsx
@@ -9,11 +9,19 @@ const EmptyState = () => (
       <Text id="integration.nuki.device.noDeviceFound" />
 
       <div class="mt-5">
+      
         <Link href="/dashboard/integration/device/nuki/mqtt">
           <button class="btn btn-outline-primary ml-2">
             <Text id="integration.nuki.mqttDiscoverTab" /> <i class="fe fe-radio" />
           </button>
         </Link>
+
+        <Link href="/dashboard/integration/device/nuki/http">
+          <button class="btn btn-outline-primary ml-2">
+            <Text id="integration.nuki.httpDiscoverTab" /> <i class="fe fe-radio" />
+          </button>
+        </Link>
+      
       </div>
     </div>
   </div>
