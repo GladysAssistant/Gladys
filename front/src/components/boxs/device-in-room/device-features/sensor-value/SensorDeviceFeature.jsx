4d81e10d07427d5bf94aa2cd920bad8793350bc4
Pierre-Gilles Leymarie <7365207+Pierre-Gilles@users.noreply.github.com>
2025-05-09 08:44:09 +0200

    Zigbee2mqtt: Add level sensor Tuya ME201WZ (#2286)

--- front/src/components/boxs/device-in-room/device-features/sensor-value/SensorDeviceFeature.jsx
+++ front/src/components/boxs/device-in-room/device-features/sensor-value/SensorDeviceFeature.jsx
@@ -13,6 +13,7 @@ import SignalQualityDeviceValue from './SignalQualityDeviceValue';
 import ButtonClickDeviceValue from './ButtonClickDeviceValue';
 import TextDeviceValue from './TextDeviceValue';
 import NoRecentValueBadge from './NoRecentValueBadge';
+import LevelSensorDeviceValue from './LevelSensorDeviceValue';
 
 const DISPLAY_BY_FEATURE_CATEGORY = {
   [DEVICE_FEATURE_CATEGORIES.MOTION_SENSOR]: MotionSensorDeviceValue,
@@ -20,7 +21,8 @@ const DISPLAY_BY_FEATURE_CATEGORY = {
   [DEVICE_FEATURE_CATEGORIES.OPENING_SENSOR]: IconBinaryDeviceValue,
   [DEVICE_FEATURE_CATEGORIES.SIGNAL]: SignalQualityDeviceValue,
   [DEVICE_FEATURE_CATEGORIES.BUTTON]: ButtonClickDeviceValue,
-  [DEVICE_FEATURE_CATEGORIES.TEXT]: TextDeviceValue
+  [DEVICE_FEATURE_CATEGORIES.TEXT]: TextDeviceValue,
+  [DEVICE_FEATURE_CATEGORIES.LEVEL_SENSOR]: LevelSensorDeviceValue
 };
 
 const DISPLAY_BY_FEATURE_TYPE = {
