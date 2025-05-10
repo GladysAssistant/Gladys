779452a2ea1cf6696d22970c02c65c55338917a6
Pierre-Gilles Leymarie <7365207+Pierre-Gilles@users.noreply.github.com>
2025-05-10 11:02:06 +0200

    Dashboard: Add gauge widget (#2296)

--- front/src/routes/dashboard/edit-dashboard/EditBox.jsx
+++ front/src/routes/dashboard/edit-dashboard/EditBox.jsx
@@ -15,6 +15,7 @@ import EditSceneBox from '../../../components/boxs/scene/EditSceneBox';
 import EditAlarmBox from '../../../components/boxs/alarm/EditAlarm';
 
 import EditEdfTempoBox from '../../../components/boxs/edf-tempo/EditEdfTempo';
+import EditGaugeBox from '../../../components/boxs/gauge/EditGaugeBox';
 
 const Box = ({ children, ...props }) => {
   switch (props.box.type) {
@@ -46,6 +47,8 @@ const Box = ({ children, ...props }) => {
       return <EditMusicBox {...props} />;
     case 'edf-tempo':
       return <EditEdfTempoBox {...props} />;
+    case 'gauge':
+      return <EditGaugeBox {...props} />;
     default:
       return <SelectBoxType {...props} />;
   }
