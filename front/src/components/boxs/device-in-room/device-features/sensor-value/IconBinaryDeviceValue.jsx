630b2bbe11c7b0e225332706abbbd5d4b8a01901
Terdious <thomas.lemaistre76@gmail.com>
2025-05-25 17:16:52 +0200

    Refactor IconBinaryDeviceValue component to simplify icon mapping and remove unused electrical vehicle states

--- front/src/components/boxs/device-in-room/device-features/sensor-value/IconBinaryDeviceValue.jsx
+++ front/src/components/boxs/device-in-room/device-features/sensor-value/IconBinaryDeviceValue.jsx
@@ -1,31 +1,17 @@
 import get from 'get-value';
 
-import { DEVICE_FEATURE_CATEGORIES, DEVICE_FEATURE_TYPES } from '../../../../../../../server/utils/constants';
+import { DEVICE_FEATURE_CATEGORIES } from '../../../../../../../server/utils/constants';
 
 const ICON_MAP = {
   [DEVICE_FEATURE_CATEGORIES.OPENING_SENSOR]: {
     0: 'unlock',
     1: 'lock'
-  },
-  [DEVICE_FEATURE_CATEGORIES.ELECTRICAL_VEHICLE]: {
-    [DEVICE_FEATURE_TYPES.ELECTRICAL_VEHICLE_STATE.DOOR_OPEN]: {
-      0: 'lock',
-      1: 'unlock'
-    },
-    [DEVICE_FEATURE_TYPES.ELECTRICAL_VEHICLE_CHARGE.PLUGGED]: {
-      0: 'unlock',
-      1: 'lock'
-    },
-    [DEVICE_FEATURE_TYPES.ELECTRICAL_VEHICLE_STATE.WINDOW_OPEN]: {
-      0: 'lock',
-      1: 'unlock'
-    }
   }
 };
 
 const IconBinaryDeviceValue = ({ deviceFeature }) => {
-  const { category, type, last_value: lastValue = null } = deviceFeature;
-  const icon = get(ICON_MAP, `${category}.${lastValue}`) || get(ICON_MAP, `${category}.${type}.${lastValue}`);
+  const { category, last_value: lastValue = null } = deviceFeature;
+  const icon = get(ICON_MAP, `${category}.${lastValue}`);
 
   if (icon) {
     return <i class={`fe fe-${icon}`} />;
