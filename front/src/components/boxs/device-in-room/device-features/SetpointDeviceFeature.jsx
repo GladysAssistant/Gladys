05b78261bf5f0ebe15cb0fa1c86a7e815f0ddf3e
Terdious <thomas.lemaistre76@gmail.com>
2025-05-26 18:07:43 +0200

    Refactor device feature components to replace ThermostatDeviceFeature with SetpointDeviceFeature and update related logic

--- front/src/components/boxs/device-in-room/device-features/SetpointDeviceFeature.jsx
+++ front/src/components/boxs/device-in-room/device-features/SetpointDeviceFeature.jsx
@@ -8,10 +8,20 @@ import { DEVICE_FEATURE_CATEGORIES } from '../../../../../../server/utils/consta
 import style from './style.css';
 
 const isNullOrUndefined = val => val === null || val === undefined;
-const DEFAULT_CURRENT_IN_CASE_EMPTY = 0;
 
-const CurrentSetpointDeviceFeature = ({ children, ...props }) => {
-  const CURRENT_STEP = props.deviceFeature.category == DEVICE_FEATURE_CATEGORIES.ELECTRICAL_VEHICLE_CHARGE ? 1 : 0.5;
+const SETPOINT_STEP_BY_CATEGORY = {
+  [DEVICE_FEATURE_CATEGORIES.AIR_CONDITIONING]: 1,
+  [DEVICE_FEATURE_CATEGORIES.SWITCH]: 1
+};
+
+const DEFAULT_VALUE_BY_CATEGORY = {
+  [DEVICE_FEATURE_CATEGORIES.AIR_CONDITIONING]: 18,
+  [DEVICE_FEATURE_CATEGORIES.SWITCH]: 0
+};
+
+const SetpointDeviceFeature = ({ children, ...props }) => {
+  const SETPOINT_STEP = SETPOINT_STEP_BY_CATEGORY[props.deviceFeature.category] || 0.5;
+  const DEFAULT_VALUE_IN_CASE_EMPTY = DEFAULT_VALUE_BY_CATEGORY[props.deviceFeature.category] || 0;
 
   function updateValue(value) {
     props.updateValueWithDebounce(props.deviceFeature, value);
@@ -23,16 +33,16 @@ const CurrentSetpointDeviceFeature = ({ children, ...props }) => {
 
   function add() {
     const prevValue = isNullOrUndefined(props.deviceFeature.last_value)
-      ? DEFAULT_CURRENT_IN_CASE_EMPTY
+      ? DEFAULT_VALUE_IN_CASE_EMPTY
       : props.deviceFeature.last_value;
-    updateValue(prevValue + CURRENT_STEP);
+    updateValue(prevValue + SETPOINT_STEP);
   }
 
   function substract() {
     const prevValue = isNullOrUndefined(props.deviceFeature.last_value)
-      ? DEFAULT_CURRENT_IN_CASE_EMPTY
+      ? DEFAULT_VALUE_IN_CASE_EMPTY
       : props.deviceFeature.last_value;
-    updateValue(prevValue - CURRENT_STEP);
+    updateValue(prevValue - SETPOINT_STEP);
   }
 
   return (
@@ -51,7 +61,7 @@ const CurrentSetpointDeviceFeature = ({ children, ...props }) => {
       <td class="py-0">
         <div class="d-flex justify-content-end">
           <div class="d-flex">
-            <div class={cx('input-group', style.currentSetpointHorizontalControls)}>
+            <div class={cx('input-group', style.setpointHorizontalControls)}>
               <div class="input-group-prepend">
                 <button class="btn btn-outline-secondary" type="button" onClick={substract}>
                   <Text id="dashboard.boxes.devicesInRoom.substractButton" />
@@ -62,7 +72,7 @@ const CurrentSetpointDeviceFeature = ({ children, ...props }) => {
                 value={props.deviceFeature.last_value}
                 class={cx('form-control text-center', style.removeNumberArrow)}
                 onChange={updateValueEvent}
-                step={CURRENT_STEP}
+                step={SETPOINT_STEP}
                 min={props.deviceFeature.min}
                 max={props.deviceFeature.max}
               />
@@ -72,7 +82,7 @@ const CurrentSetpointDeviceFeature = ({ children, ...props }) => {
                 </button>
               </div>
             </div>
-            <div class={cx('input-group input-group-sm', style.currentSetpointVerticalControls)}>
+            <div class={cx('input-group input-group-sm', style.setpointVerticalControls)}>
               <div class="d-flex flex-column mt-2 mb-2">
                 <div class="mb-1">
                   <button class="btn btn-block btn-sm btn-outline-secondary" type="button" onClick={add}>
@@ -85,7 +95,7 @@ const CurrentSetpointDeviceFeature = ({ children, ...props }) => {
                     value={props.deviceFeature.last_value}
                     class={cx('form-control text-center input-sm', style.removeNumberArrow)}
                     onChange={updateValueEvent}
-                    step={CURRENT_STEP}
+                    step={SETPOINT_STEP}
                     min={props.deviceFeature.min}
                     max={props.deviceFeature.max}
                   />
@@ -104,4 +114,4 @@ const CurrentSetpointDeviceFeature = ({ children, ...props }) => {
   );
 };
 
-export default CurrentSetpointDeviceFeature;
+export default SetpointDeviceFeature;
