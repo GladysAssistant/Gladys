2cd4d6ee830995a7c47715bf26dc2c0b2d782a67
Pierre-Gilles Leymarie <7365207+Pierre-Gilles@users.noreply.github.com>
2025-06-30 17:51:38 +0200

    Fix integrations images for dark mode

--- front/src/routes/integration/IntegrationCategory.jsx
+++ front/src/routes/integration/IntegrationCategory.jsx
@@ -1,6 +1,20 @@
 import { Text, Localizer } from 'preact-i18n';
 import ScrollToTopLink from '../../components/router/ScrollToTopLink';
 
+/**
+ * Component to display an integration category.
+ *
+ * @param {Object} integration
+ * @prop {string} integration.key - The key of the integration.
+ * @prop {string} integration.url - The URL of the integration.
+ * @prop {string} integration.img - The URL of the image for the integration.
+ * @prop {boolean} integration.invertInDarkMode - If true, the image will be
+ *  inverted in dark mode.
+ * @prop {boolean} integration.whiteBackground - If true, the image will have a
+ *  white background.
+ *
+ * @returns {ReactElement} The integration category component.
+ */
 const IntegrationCategory = ({ integration }) => {
   return (
     <div class="col-sm-6 col-lg-4">
@@ -8,7 +22,9 @@ const IntegrationCategory = ({ integration }) => {
         <ScrollToTopLink href={integration.url}>
           <Localizer>
             <img
-              class="card-img-top"
+              class={`card-img-top ${integration.invertInDarkMode ? 'keep-dark' : ''} ${
+                integration.whiteBackground ? 'white-bg' : ''
+              }`}
               src={integration.img}
               alt={<Text id={`integration.${integration.key}.title`} />}
             />
