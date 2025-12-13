36082bd3b4df16b20b2ac190ef5a2c9d1ae24efa
Pierre-Gilles Leymarie <7365207+Pierre-Gilles@users.noreply.github.com>
2025-12-13 10:11:56 +0100

    MQTT : When unsuscribing a scene topic, check if device feature is not listening to it (#2394)

--- server/test/services/mqtt/lib/unsubscribe.test.js
+++ server/test/services/mqtt/lib/unsubscribe.test.js
@@ -37,4 +37,44 @@ describe('Mqtt handle message', () => {
     assert.calledWith(mqttApi.unsubscribe, 'UNKNOWN_TOPIC');
     expect(Object.keys(mqttHandler.topicBinds)).deep.eq([]);
   });
+
+  it('should not unsubscribe from broker if device feature is still listening to topic', async () => {
+    await mqttHandler.connect({ mqttUrl: 'url' });
+    mqttHandler.subscribe('SHARED_TOPIC', () => {});
+    // Simulate a device feature listening to the same topic
+    mqttHandler.deviceFeatureCustomMqttTopics.push({
+      topic: 'SHARED_TOPIC',
+      device_feature_id: 'device-feature-id',
+    });
+
+    mqttHandler.unsubscribe('SHARED_TOPIC');
+
+    // Should NOT call mqttClient.unsubscribe because device feature is still listening
+    assert.notCalled(mqttApi.unsubscribe);
+    // But should still remove from topicBinds (only UNKNOWN_TOPIC from beforeEach remains)
+    expect(mqttHandler.topicBinds).to.not.have.property('SHARED_TOPIC');
+
+    // Cleanup
+    mqttHandler.deviceFeatureCustomMqttTopics = [];
+  });
+
+  it('should unsubscribe from broker if no device feature is listening to topic', async () => {
+    await mqttHandler.connect({ mqttUrl: 'url' });
+    mqttHandler.subscribe('UNIQUE_TOPIC', () => {});
+    // Simulate a device feature listening to a DIFFERENT topic
+    mqttHandler.deviceFeatureCustomMqttTopics.push({
+      topic: 'OTHER_TOPIC',
+      device_feature_id: 'device-feature-id',
+    });
+
+    mqttHandler.unsubscribe('UNIQUE_TOPIC');
+
+    // Should call mqttClient.unsubscribe because no device feature is listening to this topic
+    assert.calledWith(mqttApi.unsubscribe, 'UNIQUE_TOPIC');
+    // Should remove from topicBinds (only UNKNOWN_TOPIC from beforeEach remains)
+    expect(mqttHandler.topicBinds).to.not.have.property('UNIQUE_TOPIC');
+
+    // Cleanup
+    mqttHandler.deviceFeatureCustomMqttTopics = [];
+  });
 });
