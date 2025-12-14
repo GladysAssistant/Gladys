dcad9ec3e660731c10f2f244d444285a946b7b11
Pierre-Gilles Leymarie <7365207+Pierre-Gilles@users.noreply.github.com>
2025-12-14 13:53:02 +0100

    Add more logs

--- server/lib/device/device.getDuckDbMigrationState.js
+++ server/lib/device/device.getDuckDbMigrationState.js
@@ -1,5 +1,6 @@
 const db = require('../../models');
 const { SYSTEM_VARIABLE_NAMES } = require('../../utils/constants');
+const logger = require('../../utils/logger');
 
 /**
  * @description GetDuckDbMigrationState.
@@ -7,12 +8,17 @@ const { SYSTEM_VARIABLE_NAMES } = require('../../utils/constants');
  * @returns {Promise} Resolve with current migration state.
  */
 async function getDuckDbMigrationState() {
+  logger.info('Device : getDuckDbMigrationState');
   const isDuckDbMigrated = await this.variable.getValue(SYSTEM_VARIABLE_NAMES.DUCKDB_MIGRATED);
   // Use estimated_size from DuckDB metadata for fast approximate count
   const [{ estimated_size: duckDbDeviceStateCount }] = await db.duckDbReadConnectionAllAsync(`
     SELECT estimated_size FROM duckdb_tables() WHERE table_name = 't_device_feature_state';
   `);
   const sqliteDeviceStateCount = await db.DeviceFeatureState.count();
+
+  logger.info(`Device : getDuckDbMigrationState : isDuckDbMigrated: ${isDuckDbMigrated}`);
+  logger.info(`Device : getDuckDbMigrationState : duckDbDeviceStateCount: ${duckDbDeviceStateCount}`);
+  logger.info(`Device : getDuckDbMigrationState : sqliteDeviceStateCount: ${sqliteDeviceStateCount}`);
 
   return {
     is_duck_db_migrated: isDuckDbMigrated === 'true',
