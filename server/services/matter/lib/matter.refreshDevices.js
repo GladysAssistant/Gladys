19faba388e1b022bdf3190bb0a3264efd0134380
Pierre-Gilles Leymarie <7365207+Pierre-Gilles@users.noreply.github.com>
2026-03-23 11:06:17 +0100

    refreshDevices should not fail completely if one device fail

--- server/services/matter/lib/matter.refreshDevices.js
+++ server/services/matter/lib/matter.refreshDevices.js
@@ -1,5 +1,7 @@
 const Promise = require('bluebird');
 
+const logger = require('../../../utils/logger');
+
 /**
  * @description Refresh the devices.
  * @example matter.refreshDevices();
@@ -10,7 +12,12 @@ async function refreshDevices() {
   const nodeDetails = this.commissioningController.getCommissionedNodesDetails();
 
   await Promise.each(nodeDetails, async (nodeDetail) => {
-    await this.handleNode(nodeDetail);
+    try {
+      await this.handleNode(nodeDetail);
+    } catch (err) {
+      // Log error but continue with other devices - one unreachable device shouldn't break the others
+      logger.warn(`Matter: Error handling node ${nodeDetail.nodeId}: ${err.message}`);
+    }
   });
 }
 
