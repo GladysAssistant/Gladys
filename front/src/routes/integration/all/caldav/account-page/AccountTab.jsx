4bdf6481525d5bbed3b2fc45f7b62973432f5e1d
bertrandda <bertrandda@users.noreply.github.com>
2026-03-23 20:09:40 +0100

    Hide button if not secure

--- front/src/routes/integration/all/caldav/account-page/AccountTab.jsx
+++ front/src/routes/integration/all/caldav/account-page/AccountTab.jsx
@@ -158,9 +158,11 @@ const AccountTab = ({ children, ...props }) => (
                   />
                   <Text id="integration.caldav.logTitle" />
                 </button>
-                <button type="button" class="btn p-0" onClick={() => navigator.clipboard.writeText(props.caldavLog)}>
-                  <i class="fe fe-copy me-1" />
-                </button>
+                {window.isSecureContext && (
+                  <button type="button" class="btn p-0" onClick={() => navigator.clipboard.writeText(props.caldavLog)}>
+                    <i class="fe fe-copy me-1" />
+                  </button>
+                )}
               </div>
               {!props.caldavLogVisibility && (
                 <pre id="caldav-log-content" class="mb-0 mt-2" style="white-space: pre-wrap; word-break: break-all;">
