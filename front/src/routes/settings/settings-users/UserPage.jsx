e1ac58415db8b47c5ac0baa52c3c8903b4c7650c
William Deren <williamderen@gmail.com>
2026-06-20 13:03:42 +0200

    fix(ui): fix users settings header layout on mobile

--- front/src/routes/settings/settings-users/UserPage.jsx
+++ front/src/routes/settings/settings-users/UserPage.jsx
@@ -5,21 +5,23 @@ import UserCard from './UserCard';
 
 const UserPage = ({ children, ...props }) => (
   <div>
-    <div class="page-header">
+    <div class="page-header d-flex flex-column flex-sm-row align-items-start align-items-sm-center justify-content-sm-between">
       <h1 class="page-title">
         <Text id="usersSettings.title" />
       </h1>
-      <div class="page-options d-flex">
-        <Localizer>
-          <CardFilter
-            changeOrderDir={props.changeOrderDir}
-            orderValue={props.getUsersOrderDir}
-            search={props.search}
-            searchValue={props.userSearchTerms}
-            searchPlaceHolder={<Text id="usersSettings.searchPlaceholder" />}
-          />
-        </Localizer>
-        <Link href="/dashboard/settings/user/new" class="btn btn-outline-primary ml-2">
+      <div class="d-flex flex-column flex-sm-row align-items-start align-items-sm-center mt-2 mt-sm-0">
+        <div class="d-flex">
+          <Localizer>
+            <CardFilter
+              changeOrderDir={props.changeOrderDir}
+              orderValue={props.getUsersOrderDir}
+              search={props.search}
+              searchValue={props.userSearchTerms}
+              searchPlaceHolder={<Text id="usersSettings.searchPlaceholder" />}
+            />
+          </Localizer>
+        </div>
+        <Link href="/dashboard/settings/user/new" class="btn btn-outline-primary mt-2 mt-sm-0 ml-sm-2 flex-shrink-0">
           <Text id="usersSettings.newUserButton" /> <i class="fe fe-plus" />
         </Link>
       </div>
