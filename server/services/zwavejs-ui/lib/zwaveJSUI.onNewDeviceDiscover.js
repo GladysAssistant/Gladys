85db184ebf8c25f9d916a421568080a428383c5e
Stéphane Escandell <stephane.escandell@gmail.com>
2026-07-05 14:27:42 +0200

    Let's filter virtual devices to avoid unexpected behaviors right at the root integration. Let's make them available once it makes sense.

--- server/services/zwavejs-ui/lib/zwaveJSUI.onNewDeviceDiscover.js
+++ server/services/zwavejs-ui/lib/zwaveJSUI.onNewDeviceDiscover.js
@@ -2,17 +2,22 @@ const { EVENTS, WEBSOCKET_MESSAGE_TYPES } = require('../../../utils/constants');
 const { convertToGladysDevice } = require('../utils/convertToGladysDevice');
 
 /**
- * @description This will be called when new Z-Wave devices are discovered.
+ * @description This will be called when new Z-Wave devices are discovered. It
+ * will convert them to Gladys devices and emit an event to notify the frontend.
+ * Virtual devices are filtered out and not added to the list of devices as
+ * they are not supported by Gladys.
  * @param {object} data - Data sent by ZWave JS UI.
  * @example zwaveJSUI.onNewDeviceDiscover();
  */
 async function onNewDeviceDiscover(data) {
   const devices = [];
   const zwaveDevices = [];
-  data.result.forEach((zwaveJSDevice) => {
-    zwaveDevices.push(zwaveJSDevice);
-    devices.push(convertToGladysDevice(this.serviceId, zwaveJSDevice));
-  });
+  data.result
+    .filter((zwaveJSDevice) => !zwaveJSDevice.virtual)
+    .forEach((zwaveJSDevice) => {
+      zwaveDevices.push(zwaveJSDevice);
+      devices.push(convertToGladysDevice(this.serviceId, zwaveJSDevice));
+    });
   this.devices = devices;
   this.zwaveJSDevices = zwaveDevices;
 
