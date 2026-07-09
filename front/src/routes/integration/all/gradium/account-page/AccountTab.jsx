d736efbb494aa6ac5da42fa686d2302cba2947fa
bertrandda <bertrandda@users.noreply.github.com>
2026-07-09 20:04:13 +0200

    Add test on tts

--- front/src/routes/integration/all/gradium/account-page/AccountTab.jsx
+++ front/src/routes/integration/all/gradium/account-page/AccountTab.jsx
@@ -20,56 +20,56 @@ const AccountTab = ({ children, ...props }) => (
       >
         <div class="loader" />
         {props.user && props.user.role === USER_ROLE.ADMIN && (
-        <div class="dimmer-content">
-          <p>
-            <Text id="integration.gradium.accountIntroduction" />
-          </p>
-          <div class="form-group">
-            <div class="form-label">
-              <Text id="integration.gradium.endpointLabel" />
-            </div>
-            <Text id="integration.gradium.endpointInfo" />
-            <select class="form-control" onChange={props.updateGradiumEndpoint} value={props.gradiumEndpoint}>
-              {['eu', 'us'].map(endpoint => (
-                <option value={endpoint}>
-                  <Text id={`integration.gradium.endpoint.${endpoint}`} />
-                </option>
-              ))}
-            </select>
-          </div>
-          <div class="form-group">
-            <div class="form-label">
-              <Text id={`integration.gradium.apiKey`} />
-            </div>
-            <MarkupText id={`integration.gradium.apiKeyInfo`} />
-            <Localizer>
-              <input
-                type="password"
-                class="form-control"
-                placeholder={<Text id={`integration.gradium.apiKey`} />}
-                onInput={props.updateGradiumApiKey}
-                value={props.gradiumApiKey}
-              />
-            </Localizer>
-          </div>
-          {props.gradiumSaveSettingsStatus === RequestStatus.Error && (
-            <div class="alert alert-danger">
-              <Text id="integration.gradium.configurationError" />
-            </div>
-          )}
-          {props.gradiumSaveSettingsStatus === RequestStatus.Success && (
-            <p class="alert alert-info">
-              <Text id="integration.gradium.configurationSuccess" />
+          <div class="dimmer-content">
+            <p>
+              <Text id="integration.gradium.accountIntroduction" />
             </p>
-          )}
-          <div class="form-group">
-            <span class="input-group-append">
-              <button className={cx('btn btn-primary', style.button)} onClick={props.saveGradiumSettings}>
-                <Text id={`integration.gradium.buttonSave`} />
-              </button>
-            </span>
+            <div class="form-group">
+              <div class="form-label">
+                <Text id="integration.gradium.endpointLabel" />
+              </div>
+              <Text id="integration.gradium.endpointInfo" />
+              <select class="form-control" onChange={props.updateGradiumEndpoint} value={props.gradiumEndpoint}>
+                {['eu', 'us'].map(endpoint => (
+                  <option value={endpoint}>
+                    <Text id={`integration.gradium.endpoint.${endpoint}`} />
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div class="form-group">
+              <div class="form-label">
+                <Text id={`integration.gradium.apiKey`} />
+              </div>
+              <MarkupText id={`integration.gradium.apiKeyInfo`} />
+              <Localizer>
+                <input
+                  type="password"
+                  class="form-control"
+                  placeholder={<Text id={`integration.gradium.apiKey`} />}
+                  onInput={props.updateGradiumApiKey}
+                  value={props.gradiumApiKey}
+                />
+              </Localizer>
+            </div>
+            {props.gradiumSaveSettingsStatus === RequestStatus.Error && (
+              <div class="alert alert-danger">
+                <Text id="integration.gradium.configurationError" />
+              </div>
+            )}
+            {props.gradiumSaveSettingsStatus === RequestStatus.Success && (
+              <p class="alert alert-info">
+                <Text id="integration.gradium.configurationSuccess" />
+              </p>
+            )}
+            <div class="form-group">
+              <span class="input-group-append">
+                <button className={cx('btn btn-primary', style.button)} onClick={props.saveGradiumSettings}>
+                  <Text id={`integration.gradium.buttonSave`} />
+                </button>
+              </span>
+            </div>
           </div>
-        </div>
         )}
       </div>
     </div>
