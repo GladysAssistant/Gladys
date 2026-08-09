f1ad35e646ccf3e966078ca68a15101b3a3dce01
bertrandda <bertrandda@users.noreply.github.com>
2026-08-09 22:48:04 +0200

    Add comment

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -849,7 +849,7 @@ const DEVICE_FEATURE_TYPES = {
     BINARY: 'binary',
     LMH_VOLUME: 'lmh_volume',
     MELODY: 'melody',
-    TEST_IN_PROGRESS: 'test-in-progress',
+    TEST_IN_PROGRESS: 'test-in-progress', // Alarm testing status (binary - sensor)
   },
   CHILD_LOCK: {
     BINARY: 'binary',
