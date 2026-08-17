1ab4bae21ee9daa9c23a7bfefee0d52c27ebd69f
callemand <corentin.allemand@gmail.com>
2026-08-17 08:16:19 +0000

    feat(scene): see and stop running scenes (#9015) (#2710)

--- front/src/config/demo.js
+++ front/src/config/demo.js
@@ -1269,6 +1269,7 @@ const data = {
       description: "Tony's wake up scene"
     }
   ],
+  'get /api/v1/scene/running': [],
   'get /api/v1/user': [
     {
       id: 'd84ced32-d937-4cf6-a32e-105ffb584226',
